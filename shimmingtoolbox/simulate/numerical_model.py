30fbf0cb7443f60eca7a26e1d7d7305fa8a3ab31
Mathieu Boudreau <emb6150@gmail.com>
2020-07-03 21:50:34 -0300

    Add default zeros volume

--- shimmingtoolbox/simulate/numerical_model.py
+++ shimmingtoolbox/simulate/numerical_model.py
@@ -1,8 +1,15 @@
 # coding: utf-8
 
 # Scientific modules imports
+import numpy as np
 
 class NumericalModel():
 
-    def __init__(self):
-        pass
+    num_vox = None
+    starting_volume = None
+
+    def __init__(self, num_vox=128):
+
+        self.num_vox = num_vox
+        self.starting_volume = np.zeros((num_vox, num_vox))
+
