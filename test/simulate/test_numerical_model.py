a16e33bb81009aa21966ce5d3ba9a2ca826b3405
Mathieu Boudreau <emb6150@gmail.com>
2020-07-03 21:50:34 -0300

    Add default zeros volume

--- test/simulate/test_numerical_model.py
+++ test/simulate/test_numerical_model.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 
 import pytest
+import numpy as np
 from shimmingtoolbox.simulate import *
 
 class TestCore(object):
@@ -11,9 +12,18 @@ class TestCore(object):
     def teardown_class(cls):
         pass
 
-    # --------------class tests-------------- #
-    def test_object_creation(self):
+    # --------------class instance tests-------------- #
+    def test_initiate_object_is_expected_class(self):
 
-        sim_obj = NumericalModel()
+        test_obj = NumericalModel()
 
-        assert isinstance(sim_obj, NumericalModel)
+        assert isinstance(test_obj, NumericalModel)
+
+    def test_empty_initialization_returns_expected_starting_volume(self):
+
+        test_obj = NumericalModel()
+
+        expected_volume = np.zeros((128, 128))
+        actual_volume = test_obj.starting_volume
+
+        np.testing.assert_array_equal(actual_volume, expected_volume)
