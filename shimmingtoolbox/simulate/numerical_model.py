a5d264a97b72d2c5564f49267ffb98f0e9fa3458
Mathieu Boudreau <emb6150@gmail.com>
2020-07-03 23:39:47 -0300

    Add customized phantom and tests

--- shimmingtoolbox/simulate/numerical_model.py
+++ shimmingtoolbox/simulate/numerical_model.py
@@ -2,12 +2,23 @@
 
 # Scientific modules imports
 import numpy as np
+from copy import deepcopy
 from phantominator import shepp_logan
 
 class NumericalModel():
 
     num_vox = None
     starting_volume = None
+    volume = {
+        'T2_star': None,
+        'proton_density': None
+        }
+
+    # Default brain T2* values in seconds @ 3T
+    T2_star = {'WM': 0.053, 'GM': 0.066, 'CSF': 0.10}
+        
+    # Default brain proton density in percentage
+    proton_density = {'WM': 70, 'GM': 82, 'CSF': 100}
 
     def __init__(self, model=None, num_vox=128):
 
@@ -16,5 +27,40 @@ class NumericalModel():
         if model is None:
             self.starting_volume = np.zeros((num_vox, num_vox))
         elif model=='shepp-logan':
-            self.starting_volume = shepp_logan(num_vox)
+            self.__shepp_logan_brain__(num_vox)
 
+    def __shepp_logan_brain__(self, numVox):
+        self.starting_volume = shepp_logan(numVox)
+
+        self.volume['proton_density'] = self.__customize_shepp_logan__(
+            self.starting_volume,
+            self.proton_density['WM'],
+            self.proton_density['GM'],
+            self.proton_density['CSF']
+            )
+        self.volume['T2_star'] = self.__customize_shepp_logan__(
+            self.starting_volume,
+            self.T2_star['WM'],
+            self.T2_star['GM'],
+            self.T2_star['CSF']
+            )
+
+    def __customize_shepp_logan__(
+        self,
+        volume,
+        class1,
+        class2,
+        class3
+        ):
+        
+        customVolume = deepcopy(volume)
+            
+        customVolume[abs(volume-0.2)<0.001] = class1
+        customVolume[abs(volume-0.3)<0.001] = class2
+        customVolume[abs(volume-1)<0.001] = class3
+
+        customVolume[np.logical_and((abs(volume)<0.0001), volume!=0)] = class1/2
+        customVolume[abs(volume-0.1)<0.001] = (class2 + class1)/2
+        customVolume[abs(volume-0.4)<0.001] = class2*1.5
+
+        return customVolume
--- test/simulate/test_numerical_model.py
+++ test/simulate/test_numerical_model.py
@@ -37,3 +37,34 @@ class TestCore(object):
         actual_volume = test_obj.starting_volume
 
         np.testing.assert_array_equal(actual_volume, expected_volume)
+
+    def test_shepplogan_dims_init_returns_expected_starting_volume(self):
+        dims = 256
+        test_obj = NumericalModel(model='shepp-logan', num_vox=dims)
+
+        expected_volume = shepp_logan(dims)
+        actual_volume = test_obj.starting_volume
+
+        np.testing.assert_array_equal(actual_volume, expected_volume)
+
+    def test_shepp_logan_defines_expected_t2star_values(self):
+        dims = 256
+        test_obj = NumericalModel(model='shepp-logan', num_vox=dims)
+
+        assert np.all(test_obj.volume['T2_star'][abs(test_obj.starting_volume-0.2)<0.001] == test_obj.T2_star['WM'])
+        assert np.all(test_obj.volume['T2_star'][abs(test_obj.starting_volume-0.3)<0.001] == test_obj.T2_star['GM'])
+        assert np.all(test_obj.volume['T2_star'][abs(test_obj.starting_volume-1)<0.001] == test_obj.T2_star['CSF'])
+        assert np.all(test_obj.volume['T2_star'][np.logical_and((abs(test_obj.starting_volume)<0.0001), test_obj.starting_volume!=0)] == test_obj.T2_star['WM']/2)
+        assert np.all(test_obj.volume['T2_star'][abs(test_obj.starting_volume-0.1)<0.001] == (test_obj.T2_star['GM']+test_obj.T2_star['WM'])/2)
+        assert np.all(test_obj.volume['T2_star'][abs(test_obj.starting_volume-0.4)<0.001] == test_obj.T2_star['GM'] * 1.5)
+
+    def test_shepp_logan_defines_expected_magnitude_values(self):
+        dims = 256
+        test_obj = NumericalModel(model='shepp-logan', num_vox=dims)
+
+        assert np.all(test_obj.volume['proton_density'][abs(test_obj.starting_volume-0.2)<0.001] == test_obj.proton_density['WM'])
+        assert np.all(test_obj.volume['proton_density'][abs(test_obj.starting_volume-0.3)<0.001] == test_obj.proton_density['GM'])
+        assert np.all(test_obj.volume['proton_density'][abs(test_obj.starting_volume-1)<0.001] == test_obj.proton_density['CSF'])
+        assert np.all(test_obj.volume['proton_density'][np.logical_and((abs(test_obj.starting_volume)<0.0001), test_obj.starting_volume!=0)] == test_obj.proton_density['WM']/2)
+        assert np.all(test_obj.volume['proton_density'][abs(test_obj.starting_volume-0.1)<0.001] == (test_obj.proton_density['GM']+test_obj.proton_density['WM'])/2)
+        assert np.all(test_obj.volume['proton_density'][abs(test_obj.starting_volume-0.4)<0.001] == test_obj.proton_density['GM'] * 1.5)
