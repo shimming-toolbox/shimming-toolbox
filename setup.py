ddfec292cac44ecf31215326e9ac465644d5f9d7
Nick Guenther <nguenthe@uwaterloo.ca>
2020-07-06 07:21:47 -0400

    Bump python version backwards
    Ubuntu Xenial still uses 3.6 and it's still alive for at least
    half a year more.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="shimmingtoolbox",
-    python_requires=">=3.7",
+    python_requires=">=3.6",
     version=shimmingtoolbox.__version__,
     description="Code for performing real-time shimming using external MRI shim coils",
     long_description=long_description,
