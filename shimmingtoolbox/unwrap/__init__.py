80a8ec85d1b045e5e0322a5511f0693dae85313c
Alexandre D'Astous <po09i@hotmail.com>
2020-07-07 14:56:44 -0400

    NEW : Added unwrap folder structure

---
+++ shimmingtoolbox/unwrap/__init__.py
@@ -0,0 +1,3 @@
+__version__ = "0.1"
+
+from .unwrap_phase import unwrap_phase
---
+++ shimmingtoolbox/unwrap/unwrap_phase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Description
+
+"""
+
+
+def unwrap_phase(complexArray, unwrapFunction = ""):
+    print("Not implemented")
+
