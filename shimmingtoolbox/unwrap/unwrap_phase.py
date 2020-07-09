83d1aae12c6542ef96ea34e9cd143fd2e91c37ca
Alexandre D'Astous <po09i@hotmail.com>
2020-07-09 13:10:27 -0400

    Initial function for unwrap phase

--- shimmingtoolbox/unwrap/unwrap_phase.py
+++ shimmingtoolbox/unwrap/unwrap_phase.py
@@ -1,9 +1,22 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
 """ Description
 
 """
 
 
-def unwrap_phase(complexArray, unwrapFunction = ""):
-    print("Not implemented")
+import numpy as np
+from shimmingtoolbox.unwrap.prelude import prelude
 
+
+def unwrap_phase(complexArray, affine, unwrapFunction="prelude"):
+
+    # TODO: create mask (here or in script?) (probably in script)
+    # Call SCT or user defined mask
+    mask = np.ones(complexArray.shape)
+    if unwrapFunction == "prelude":
+        unwrappedPhase = prelude(complexArray, affine, mask)
+        return unwrappedPhase
+    else:
+        print("The unwrap function ", unwrapFunction, " is not implemented")
+        return -1
