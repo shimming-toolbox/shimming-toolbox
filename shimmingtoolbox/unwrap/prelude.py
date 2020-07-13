45d2653aec53b46c004c2414dd18c4e3fb4b8228
Alexandre D'Astous <po09i@hotmail.com>
2020-07-13 17:01:23 -0400

    Updated according to comments

--- shimmingtoolbox/unwrap/prelude.py
+++ shimmingtoolbox/unwrap/prelude.py
@@ -15,8 +15,8 @@ def prelude(complex_array, affine, mask=np.array([-1]), path_2_unwrapped_phase="
     abs_path = os.path.abspath(path_2_unwrapped_phase)
     data_save_directory = os.path.dirname(abs_path)
 
-    # Save complex image
-    # TODO: Save voxelSize when saving nifti (Maybe affine does it) (if it does, might be easier to just pass entire nibabel object)
+    # Save complex image TODO: Save voxelSize when saving nifti (Maybe affine does it) (if it does, might be easier
+    #  to just pass entire nibabel object)
     phase_nii = nib.Nifti1Image(np.angle(complex_array), affine)
     mag_nii = nib.Nifti1Image(np.abs(complex_array), affine)
     nib.save(phase_nii, os.path.join(data_save_directory, "rawPhase.nii"))
@@ -26,29 +26,28 @@ def prelude(complex_array, affine, mask=np.array([-1]), path_2_unwrapped_phase="
     options = " "
 
     if is_unwrapping_in_2d:
-        options = options + "-s "
+        options += "-s "
 
-    # Create mask with all ones if mask is not provided
+    # Add mask data and options if there is a mask provided
     if not np.any(mask == -1):
         assert mask.shape == complex_array.shape, "Mask must be the same shape as the array"
-        maskNii = nib.Nifti1Image(mask, affine)
+        mask_nii = nib.Nifti1Image(mask, affine)
 
-        options = options + "-m "
-        options = options + os.path.join(data_save_directory, "mask.nii")
-        nib.save(maskNii, os.path.join(data_save_directory, "mask.nii"))
+        options += "-m "
+        options += os.path.join(data_save_directory, "mask.nii")
+        nib.save(mask_nii, os.path.join(data_save_directory, "mask.nii"))
 
     # Unwrap
-    unwrap_command = "prelude -p " + os.path.join(data_save_directory, "rawPhase") + " -a " + \
-                     os.path.join(data_save_directory, "mag") + " -o " + path_2_unwrapped_phase + options
-
-    print(unwrap_command)
+    unwrap_command = "prelude -p {} -a {} -o {} {}".format(os.path.join(data_save_directory, "rawPhase"),
+                                                           os.path.join(data_save_directory, "mag"),
+                                                           path_2_unwrapped_phase, options)
     os.system(unwrap_command)
 
     # Uncompress
     os.system("gunzip " + path_2_unwrapped_phase + ".gz -df")
-
     unwrapped_phase = nib.load(path_2_unwrapped_phase)
     unwrapped_phase = unwrapped_phase.get_fdata()
+
     # Delete temporary files according to options
     if not is_saving_nii:
         os.remove(os.path.join(data_save_directory, "mag.nii"))
