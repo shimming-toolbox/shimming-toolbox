cfe5ab3a6da9c9b9dc63b0e5a8ce867df40d1366
Alexandre D'Astous <po09i@hotmail.com>
2020-07-13 16:34:46 -0400

    Return 3d instead of 2d

--- shimmingtoolbox/unwrap/prelude.py
+++ shimmingtoolbox/unwrap/prelude.py
@@ -4,71 +4,57 @@
 
 """
 
-
 import numpy as np
 import os
 import nibabel as nib
 
 
-def prelude(complexArray, affine, mask=np.array([-1]), path2UnwrappedPhase="./unwrappedPhase.nii", isUnwrappingIn2D=True, isSavingNiftis=False):
-    """
-
-    Args:
-        complexArray:
-        affine:
-        mask:
-        path2UnwrappedPhase:
-        isUnwrappingIn2D:
-        isSavingNiftis:
-
-    Returns:
-
-    """
+def prelude(complex_array, affine, mask=np.array([-1]), path_2_unwrapped_phase="./unwrapped_phase.nii",
+            is_unwrapping_in_2d=True, is_saving_nii=False):
     # Get absolute path
-    absPath = os.path.abspath(path2UnwrappedPhase)
-    dataSaveDirectory = os.path.dirname(absPath)
+    abs_path = os.path.abspath(path_2_unwrapped_phase)
+    data_save_directory = os.path.dirname(abs_path)
 
     # Save complex image
     # TODO: Save voxelSize when saving nifti (Maybe affine does it) (if it does, might be easier to just pass entire nibabel object)
-    phaseNii = nib.Nifti1Image(np.angle(complexArray[:-1, :-1, 0, 0]), affine)
-    magNii = nib.Nifti1Image(np.abs(complexArray[:-1, :-1, 0, 0]), affine)
-    nib.save(phaseNii, os.path.join(dataSaveDirectory, "rawPhase.nii"))
-    nib.save(magNii, os.path.join(dataSaveDirectory, "mag.nii"))
+    phase_nii = nib.Nifti1Image(np.angle(complex_array), affine)
+    mag_nii = nib.Nifti1Image(np.abs(complex_array), affine)
+    nib.save(phase_nii, os.path.join(data_save_directory, "rawPhase.nii"))
+    nib.save(mag_nii, os.path.join(data_save_directory, "mag.nii"))
 
     # Fill options
     options = " "
 
-    if isUnwrappingIn2D:
+    if is_unwrapping_in_2d:
         options = options + "-s "
 
     # Create mask with all ones if mask is not provided
-    if not mask.all() == -1:
-        assert mask.shape == complexArray.shape, "Mask must be the same shape as the array"
+    if not np.any(mask == -1):
+        assert mask.shape == complex_array.shape, "Mask must be the same shape as the array"
         maskNii = nib.Nifti1Image(mask, affine)
 
         options = options + "-m "
-        options = options + os.path.join(dataSaveDirectory, "mask.nii")
-        nib.save(maskNii, os.path.join(dataSaveDirectory, "mask.nii"))
+        options = options + os.path.join(data_save_directory, "mask.nii")
+        nib.save(maskNii, os.path.join(data_save_directory, "mask.nii"))
 
     # Unwrap
-    unwrapCommand = "prelude -p " + os.path.join(dataSaveDirectory, "rawPhase") + " -a " + \
-                    os.path.join(dataSaveDirectory, "mag") + " -o " + path2UnwrappedPhase + options
+    unwrap_command = "prelude -p " + os.path.join(data_save_directory, "rawPhase") + " -a " + \
+                     os.path.join(data_save_directory, "mag") + " -o " + path_2_unwrapped_phase + options
 
-    print(unwrapCommand)
-    os.system(unwrapCommand)
+    print(unwrap_command)
+    os.system(unwrap_command)
 
     # Uncompress
-    os.system("gunzip " + path2UnwrappedPhase + ".gz -df")
+    os.system("gunzip " + path_2_unwrapped_phase + ".gz -df")
 
-    unwrappedPhase = nib.load(path2UnwrappedPhase)
-    unwrappedPhase = np.array(unwrappedPhase.dataobj)
+    unwrapped_phase = nib.load(path_2_unwrapped_phase)
+    unwrapped_phase = unwrapped_phase.get_fdata()
     # Delete temporary files according to options
-    if not isSavingNiftis:
-        os.remove(os.path.join(dataSaveDirectory, "mag.nii"))
-        os.remove(os.path.join(dataSaveDirectory, "rawPhase.nii"))
-        os.remove(os.path.join(dataSaveDirectory, "unwrappedPhase.nii"))
-        if not mask.all() == -1:
-            os.remove(os.path.join(dataSaveDirectory, "mask.nii"))
+    if not is_saving_nii:
+        os.remove(os.path.join(data_save_directory, "mag.nii"))
+        os.remove(os.path.join(data_save_directory, "rawPhase.nii"))
+        os.remove(os.path.join(data_save_directory, "unwrapped_phase.nii"))
+        if not np.any(mask == -1):
+            os.remove(os.path.join(data_save_directory, "mask.nii"))
 
-    return unwrappedPhase
-
+    return unwrapped_phase
