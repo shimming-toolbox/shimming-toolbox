63f13e4dd3d424f68c2cc23890e00a6eef5f995d
Gaspard Cereza <54723104+gaspardcereza@users.noreply.github.com>
2020-07-13 16:25:55 -0400

    Update volume_RT_shim_respiratory.py
    Converted variable names to `snake_case`

--- examples/volume_RT_shim_respiratory.py
+++ examples/volume_RT_shim_respiratory.py
@@ -2,42 +2,45 @@
 #
 # Sort unorganized images from DICOM socket transfer
 # dcm2bids is used to create folder structure and nifti conversion
-# unsortedDicomDir: path to the folder containing the dicoms
-# niftiPath: path where we want dcm2bids to store the nifti files
+# unsorted_dicom_dir: path to the folder containing the dicoms
+# nifti_path: path where we want dcm2bids to store the nifti files
 # 
-# > dicom_to_nifti( unsortedDicomDir, niftiPath )
+# > dicom_to_nifti( unsorted_dicom_dir, nifti_path )
 #
 # Load images from the path
-# Using niftiPath will prompt the user to select the appropriate acquisition. Alternatively, if the path to the acquisition is known, the path can be changed and the function won't prompt the user
-# > mag = load_niftis( niftiPath ) 5D array (x, y, z, nEcho, nAcq)
-# > phase = load_niftis( niftiPath ) 5D array (x, y, z, nEcho, nAcq)
+# Using nifti_path will prompt the user to select the appropriate acquisition. Alternatively, if the path to the acquisition is known, the path can be changed and the function won't prompt the user
+# > mag = load_niftis( nifti_path ) 
+# > phase = load_niftis( nifti_path )
+# 5D arrays (x, y, z, nEcho, nAcq)
 #
 # Convert to complex
-# > complexArray = mag.*exp( 1i.*phase ); 5D array (x, y, z, nEcho, nAcq)
+# > complex_array = mag.*exp( 1i.*phase ); 5D array (x, y, z, nEcho, nAcq)
 #
-# Compute B0 fieldmap
-# b0FieldMaps could be 2d, 3d or 4d. 4th dimension is the time in [s] (i.e. one B0 per timestamp).
+# Compute B0 fieldmaps
+# b0_field_maps could be 2d, 3d or 4d. 4th dimension is the time in [s] (i.e. one B0 per timestamp).
 # JCA: OK for s as unit?
 # Note: the function should accommodate multiple fieldmap data (one per time point, in case of realtime shimming scenario)
-# > unwrappedPhase = unwrap.unwrap_phase( complexArray, unwrapFunction )
-# > b0FieldMaps = mapping( unwrappedPhase, echoTimes, mappingFunction )
+# > unwrapped_phase = unwrap_phase( complex_array, unwrapping_function )
+# > b0_field_maps = mapping( unwrappedPhase, echoTimes, mapping_function )
 #
 # Load Siemens respiratory probe trace
 # JCA: it would be nice to have the file "acdc45/probe02.resp" copied in this folder automatically, so everything pertaining to this experiment is at the same location.
 # pmu is an object with useful properties.
-# > pmu = load_probe_data(fullfile(pathNifti, 'probe02.resp'));
+# > pmu = load_probe_data(fullfile(nifti_path, 'probe02.resp'));
 #
 # Interpolate pmu to the image times
 # JCA: Do we want the function below to output an object pmuInterp or do we want to keep the current behaviour: include pmu information inside "Field" object?
 # pmuTrace is a vector
-# > pmuTrace = match_probe_data_with_image(pmu, imgB0);
+# > pmu_trace = match_probe_data_with_image(pmu, imgB0);
 #
 # Load T2w magnitude image for segmentation
-# > imgAnat = load_niftis(fullfile(pathNifti, '04-T2w.nii'));
+# > imgAnat = load_niftis(fullfile(nifti_path, '04-T2w.nii'));
 #
 # Segment ROI
-# > paramSeg.betParamDummy = XX  # some params for FSL BET function
-# > paramSeg.sctDiameter = 30  # in mm
+# > param_seg.bet_param_dummy = XX
+# some params for FSL BET function
+# > param_seg.sct_diameter = 30
+# in mm
 # > roi = segment_image(imgAnat, method={'bet', 'sct', 'cylinder'}, paramSeg)
 #
 # Optimize shim currents
@@ -50,7 +53,7 @@
 # > coil = generate_coil(‘Nibardo’, [x y z])
 #
 # WIP
-# > shims = optimize_shim_algo(b0FieldMaps, coil, method=‘realtimeZShim’, roi (optional), PMU (optional), etc.)
+# > shims = optimize_shim_algo(b0_field_maps, coil, method=‘realtimeZShim’, roi (optional), PMU (optional), etc.)
 #
 # Set shims to scanner or coil WIP
-# > send_shim(shims, SyngoPath)
+# > send_shim(shims, syngo_path)
