b2b5dfbb8755c2040a1e1331f825dbc79727267d
Gaspard Cereza <54723104+gaspardcereza@users.noreply.github.com>
2020-07-13 15:25:20 -0400

    Update volume_RT_shim_respiratory.py
    Identified the function calls and added context

--- examples/volume_RT_shim_respiratory.py
+++ examples/volume_RT_shim_respiratory.py
@@ -1,18 +1,4 @@
-# Context: this is blablabla
-# Things to consider:
-# - with this large refactoring, we should try to accommodate Jason's current code
-# - log file should be created for each action.
-#   - should we use Matlab's diary()?
-#   - if so, is there a way to configure an output log file at the beginning of a process, and then pass the log handler across functions?
-# - when useful, plottings, etc. will appear.
-# - conventions:
-#   - variable: mixedCamelCase
-#     - images: start with "img", example: imgFieldmapMag
-#   - functions: snake_convention
-#     - use "action", then "what"
-# - use nifti-based software
-#   - ANTs: great for registration, interpolation (fast!)
-#
+# Context: That scripts covers a volume-wise realtime shimming with respiratory probe scenario 
 #
 # Sort unorganized images from DICOM socket transfer
 # dcm2bids is used to create folder structure and nifti conversion
@@ -27,32 +13,32 @@
 # > phase = load_niftis( niftiPath ) 5D array (x, y, z, nEcho, nAcq)
 #
 # Convert to complex
-# complexArray = mag.*exp( 1i.*phase ); 5D array (x, y, z, nEcho, nAcq)
+# > complexArray = mag.*exp( 1i.*phase ); 5D array (x, y, z, nEcho, nAcq)
 #
 # Compute B0 fieldmap
 # b0FieldMaps could be 2d, 3d or 4d. 4th dimension is the time in [s] (i.e. one B0 per timestamp).
 # JCA: OK for s as unit?
 # Note: the function should accommodate multiple fieldmap data (one per time point, in case of realtime shimming scenario)
-# unwrappedPhase = unwrap.unwrap_phase( complexArray, unwrapFunction )
-# b0FieldMaps = mapping( unwrappedPhase, echoTimes, mappingFunction )
+# > unwrappedPhase = unwrap.unwrap_phase( complexArray, unwrapFunction )
+# > b0FieldMaps = mapping( unwrappedPhase, echoTimes, mappingFunction )
 #
 # Load Siemens respiratory probe trace
 # JCA: it would be nice to have the file "acdc45/probe02.resp" copied in this folder automatically, so everything pertaining to this experiment is at the same location.
 # pmu is an object with useful properties.
-# pmu = load_probe_data(fullfile(pathNifti, 'probe02.resp'));
+# > pmu = load_probe_data(fullfile(pathNifti, 'probe02.resp'));
 #
 # Interpolate pmu to the image times
 # JCA: Do we want the function below to output an object pmuInterp or do we want to keep the current behaviour: include pmu information inside "Field" object?
 # pmuTrace is a vector
-# pmuTrace = match_probe_data_with_image(pmu, imgB0);
+# > pmuTrace = match_probe_data_with_image(pmu, imgB0);
 #
 # Load T2w magnitude image for segmentation
-# imgAnat = load_niftis(fullfile(pathNifti, '04-T2w.nii'));
+# > imgAnat = load_niftis(fullfile(pathNifti, '04-T2w.nii'));
 #
 # Segment ROI
-# paramSeg.betParamDummy = XX  some params for FSL BET function
-# paramSeg.sctDiameter = 30  in mm
-# roi = segment_image(imgAnat, method={'bet', 'sct', 'cylinder'}, paramSeg)
+# > paramSeg.betParamDummy = XX  # some params for FSL BET function
+# > paramSeg.sctDiameter = 30  # in mm
+# > roi = segment_image(imgAnat, method={'bet', 'sct', 'cylinder'}, paramSeg)
 #
 # Optimize shim currents
 # JCA: ShimOpt will use imgAnat as the destination target for regridding b0FieldMaps into the destination space. Also, if a segmentation exists in roi, it will use it to optimize shim currents.
@@ -61,10 +47,10 @@
 # another suggestion is to specify the algo type in the function itself
 # Coil sensitivity WIP
 # Output : (x y z channels)
-# coil = generate_coil(‘Nibardo’, [x y z])
+# > coil = generate_coil(‘Nibardo’, [x y z])
 #
 # WIP
-# shims = optimize_shim_algo(b0FieldMaps, coil, method=‘realtimeZShim’, roi (optional), PMU (optional), etc.)
+# > shims = optimize_shim_algo(b0FieldMaps, coil, method=‘realtimeZShim’, roi (optional), PMU (optional), etc.)
 #
 # Set shims to scanner or coil WIP
-# send_shim(shims, SyngoPath)
+# > send_shim(shims, SyngoPath)
