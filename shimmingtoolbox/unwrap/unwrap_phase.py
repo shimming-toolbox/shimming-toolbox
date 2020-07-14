2d68b26dc820b1ae844c8bbeb4310d9ebf551d92
Alexandre D'Astous <po09i@hotmail.com>
2020-07-14 01:50:44 -0400

    moved mask to script

--- shimmingtoolbox/unwrap/unwrap_phase.py
+++ shimmingtoolbox/unwrap/unwrap_phase.py
@@ -9,29 +9,25 @@ import numpy as np
 from shimmingtoolbox.unwrap.prelude import prelude
 
 
-def unwrap_phase(complex_array, affine, unwrap_function="prelude"):
-
-    # TODO: create mask (here or in script?) (probably in script)
-    # Call SCT or user defined mask
-    # mask = np.ones(complex_array.shape)
+def unwrap_phase(complex_array, affine, unwrap_function="prelude", mask=np.array([-1])):
 
     unwrapped_phase = np.empty(complex_array.shape, np.complex)
     if unwrap_function == "prelude":
 
         if complex_array.ndim == 2:
             complex_array = complex_array[..., np.newaxis]
-            unwrapped_phase = prelude(complex_array, affine)
+            unwrapped_phase = prelude(complex_array, affine, mask)
         elif complex_array.ndim == 3:
-            unwrapped_phase = prelude(complex_array, affine)
+            unwrapped_phase = prelude(complex_array, affine, mask)
         elif complex_array.ndim == 4:
             for i_Echo in range(complex_array.shape[3]):
-                unwrapped_phase[:, :, :, i_Echo] = prelude(complex_array[:, :, :, i_Echo], affine)
+                unwrapped_phase[:, :, :, i_Echo] = prelude(complex_array[:, :, :, i_Echo], affine, mask)
         elif complex_array.ndim == 5:
             for i_Echo in range(complex_array.shape[3]):
                 for i_acq in range(complex_array.shape[4]):
-                    unwrapped_phase[:, :, :, i_Echo, i_acq] = prelude(complex_array[:, :, :, i_Echo, i_acq], affine)
+                    unwrapped_phase[:, :, :, i_Echo, i_acq] = prelude(complex_array[:, :, :, i_Echo, i_acq], affine,
+                                                                      mask)
         else:
-            # TODO: Better error handling
             raise Exception('Number of dimensions not supported')
 
     else:
