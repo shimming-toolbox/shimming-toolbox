33f875f2c7e0d6460c4b0bf88095304b1af5db9e
Alexandre D'Astous <po09i@hotmail.com>
2020-07-14 02:07:00 -0400

    added docstring

--- shimmingtoolbox/unwrap/unwrap_phase.py
+++ shimmingtoolbox/unwrap/unwrap_phase.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-""" Description
+""" Wrapper to different unwrapping algorithms
 
 """
 
@@ -10,7 +10,24 @@ from shimmingtoolbox.unwrap.prelude import prelude
 
 
 def unwrap_phase(complex_array, affine, unwrap_function="prelude", mask=np.array([-1])):
+    """ Calls different unwrapping algorithms according to the specified `unwrap_function` parameter
 
+    Args:
+        complex_array (numpy.ndarray): 2D,3D,4D or 5D complex values numpy array to perform phase unwrapping
+        affine (numpy.ndarray): 2D array (4x4) containing the transformation coefficients. Can be acquired by :
+            nii = nib.load("nii_path")
+            affine = nii.affine
+        unwrap_function (string, optional): unwrapper algorithm name. Possible values : prelude
+        mask: numpy array of booleans with shape of `complex_array` to mask during phase unwrapping. Calling `mask` wth
+        np.array([-1]) is the same as specifying no mask.
+
+    Returns:
+        numpy.ndarray: 3D array with the shape of `complex_array` of the unwrapped phase output from prelude
+
+    Raises:
+        Exception: complex array does not have 2,3,4,5 dimensions
+        Exception: unwrapping algorithm in `unwrap_function` is not supported
+    """
     unwrapped_phase = np.empty(complex_array.shape, np.complex)
     if unwrap_function == "prelude":
 
