38f9bace4472dc0f42aab64b0488790d94b709fe
Gaspard Cereza <gaspard.cereza@gmail.com>
2020-07-14 18:54:43 -0400

    Changed some variable names, completed dicom_to_nifti

--- examples/script_nifti.py
+++ examples/script_nifti.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import os
 import pathlib
 import shutil
@@ -9,11 +11,11 @@ from zipfile import ZipFile
 
 
 def main():
-    scriptPath = pathlib.Path(__file__).parent.absolute()  # Folder where this script is located
-    dataPath = os.path.join(scriptPath, 'data_testing')  # Folder where the data will be downloaded
+    script_path = pathlib.Path(__file__).parent.absolute()  # Folder where this script is located
+    data_path = os.path.join(script_path, 'data-testing-r20200713')  # Folder where the data will be downloaded
 
     # Download data when not already present
-    if not os.path.isdir(dataPath):
+    if not os.path.isdir(data_path):
         url = 'https://github.com/shimming-toolbox/data-testing/archive/r20200713.zip'
         try:
             fname_archive = 'data-testing.zip'
@@ -21,19 +23,20 @@ def main():
                 shutil.copyfileobj(resp, out_file)
                 with ZipFile(fname_archive, 'r') as zipObj:
                     # Extract all the contents of zip file in current directory
-                    zipObj.extractall('.')
+                    zipObj.extractall(script_path)
+            print('Downloading test data from github')
         except IndexError:
             print("ERROR - {0}:{1}".format(sys.exc_info()[0], sys.exc_info()[1]))
 
     # TODO: use systematic name for data-testing (could be in metadata of shimmingtoolbox)
-    path_data = 'data-testing-r20200713'
 
+    unsorted_dicom_path = os.path.join(data_path, 'dicom_unsorted')
     # Create temporary folder for processing
     tmp = tempfile.TemporaryDirectory()
     # Path where the niftis will be temporarily stored
-    niftiPath = os.path.join(tmp.name, 'niftis')
+    nifti_path = os.path.join(tmp.name, 'niftis')
 
-    dicom_to_nifti(unsortedDicomDir, niftiPath)
+    dicom_to_nifti(unsorted_dicom_path, nifti_path)
     print('Conversion from dicom to nifti done')
 
 
