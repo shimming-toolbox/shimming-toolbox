cfb957ffdc763ce070bb97fb0f647a7f7c5aa890
Gaspard Cereza <gaspard.cereza@gmail.com>
2020-07-15 15:03:14 -0400

    updated variable names and error handling

--- shimmingtoolbox/read_nii.py
+++ shimmingtoolbox/read_nii.py
@@ -3,80 +3,76 @@ import math
 import nibabel as nib
 import os
 
-def read_nii(niiPath):
+def read_nii(nii_path):
     """
     Args:
-        niiPath (str): Path to the nifti file
+        nii_path (str): direct path to the .nii or .nii.gz file
 
     Returns:
-        image:
-        info:
-        jsonData:
+
     """
 
-    info = nib.load(niiPath)
+    info = nib.load(nii_path)
     image = info.get_fdata()
 
     # `extractBefore` should get the correct filename in both.nii and.nii.gz cases
-    jsonPath = niiPath.split('.nii')[0] + '.json'
+    json_path = nii_path.split('.nii')[0] + '.json'
 
-    if os.path.isfile(jsonPath):
-        jsonData = json.load(open(jsonPath))
+    if os.path.isfile(json_path):
+        json_data = json.load(open(json_path))
     else:
-        jsonData = []
-        print('Warning: No json file has been found')
+        raise ValueError('Ambiguous ImageType entry in json file: Indicates magnitude AND phase?')
 
     # NOTE: nib.load automatically scales the nifti and replaces scl_inter and scl_slope with 'nan'
-    # See the "Data scaling" section in https://nipy.org/nibabel/nifti_images.html
-    if ('Manufacturer' in jsonData) and (jsonData['Manufacturer'] == 'Siemens') \
-            and (check_json_image_type(jsonData) == 'phase'):
+    # More info in the "Data scaling" section in https://nipy.org/nibabel/nifti_images.html
+    if ('Manufacturer' in json_data) and (json_data['Manufacturer'] == 'Siemens') \
+            and (check_json_image_type(json_data) == 'phase'):
 
         image, info = convert_siemens_phase(image, info)
 
-    return image, info, jsonData
-
-# -----------------------------------------------------------------------------
-# Local functions
-# -----------------------------------------------------------------------------
+    return image, info, json_data
 
 
-def check_json_image_type(jsonData):
-    imgType = []
+def check_json_image_type(json_data):
+    img_type = []
 
-    if not jsonData:
-        print('Warning: The image type has not been found because the json file is empty')
-        return imgType
+    # Check that jsonData exists
+    if not json_data:
+        raise TypeError("json_data is empty")
 
-    assert isinstance(jsonData, dict)  # Check that jsonData is a dictionary
+    # Check that jsonData is a dictionary
+    if not isinstance(json_data, dict):
+        raise TypeError("json_data is not a dictionary")
 
-    if 'ImageType' in jsonData:
-        isPhase = "P" in jsonData['ImageType']
-        isMag = "M" in jsonData['ImageType']
+    if 'ImageType' in json_data:
+        is_phase = "P" in json_data['ImageType']
+        is_mag = "M" in json_data['ImageType']
 
-        if isPhase and isMag:
-            # Both true: json file and / or DICOM issue(hopefully this doesn't occur?)
-            print('Warning: Ambiguous ImageType entry in json file: Indicates magnitude AND phase?')
-        elif isPhase:
-            imgType = 'phase'
-        elif isMag:
-            imgType = 'magnitude'
+        if is_phase and is_mag:
+            # Both true: json file and/or DICOM issue
+            raise ValueError('Ambiguous ImageType entry in json file: Indicates magnitude AND phase')
+        elif is_phase:
+            img_type = 'phase'
+        elif is_mag:
+            img_type = 'magnitude'
         else:
-            imgType = 'unknown'
-            if ('Manufacturer' in jsonData) and (jsonData['Manufacturer'] != 'Siemens'):
-                print('Warning: Unknown image type. Possibly due to images sourced from non-Siemens MRI')
+            if ('Manufacturer' in json_data) and (json_data['Manufacturer'] != 'Siemens'):
+                raise ValueError('Unknown image type. Possibly due to images sourced from non-Siemens MRI')
+            else:
+                raise ValueError('Unknown image type')
 
-        return imgType
+        return img_type
 
 
 def convert_siemens_phase(image, info):
 
-    PHASE_SCALING_SIEMENS: int = 4096
+    PHASE_SCALING_SIEMENS: float = 4096
 
-    infoConverted = info
-    imgConverted = image * (math.pi / PHASE_SCALING_SIEMENS)
+    info_converted = info
+    img_converted = image * (math.pi / PHASE_SCALING_SIEMENS)
 
     # Update header: 16 is the NIfTI code for float; bitpix = number of bits
-    infoConverted.header['datatype'] = 16
-    infoConverted.header['bitpix'] = 32
+    info_converted.header['datatype'] = 16
+    info_converted.header['bitpix'] = 32
 
-    return imgConverted, infoConverted
+    return img_converted, info_converted
