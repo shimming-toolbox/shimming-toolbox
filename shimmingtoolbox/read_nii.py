6fca6edd3ad25b7d6ef791d1172530ce3a28803f
Gaspard Cereza <gaspard.cereza@gmail.com>
2020-07-17 14:12:59 -0400

    Uptaded functions according to reviews

--- shimmingtoolbox/read_nii.py
+++ shimmingtoolbox/read_nii.py
@@ -1,11 +1,13 @@
+import copy
 import json
 import math
 import nibabel as nib
+import numpy as np
 import os
 
 
 def read_nii(nii_path):
-    """
+    """ Reads a nifti file and returns image, info and json_data
 
     Args:
         nii_path (str): direct path to the .nii or .nii.gz file
@@ -15,7 +17,7 @@ def read_nii(nii_path):
     """
 
     info = nib.load(nii_path)
-    image = info.get_fdata()
+    image = np.asanyarray(info.dataobj)
 
     # `extractBefore` should get the correct filename in both.nii and.nii.gz cases
     json_path = nii_path.split('.nii')[0] + '.json'
@@ -23,20 +25,19 @@ def read_nii(nii_path):
     if os.path.isfile(json_path):
         json_data = json.load(open(json_path))
     else:
-        raise ValueError('Ambiguous ImageType entry in json file: Indicates magnitude AND phase?')
+        raise ValueError('Missing json file')
 
     # NOTE: nib.load automatically scales the nifti and replaces scl_inter and scl_slope with 'nan'
     # More info in the "Data scaling" section in https://nipy.org/nibabel/nifti_images.html
     if ('Manufacturer' in json_data) and (json_data['Manufacturer'] == 'Siemens') \
-            and (check_json_image_type(json_data) == 'phase'):
+            and (image_type(json_data) == 'phase'):
 
         image, info = convert_siemens_phase(image, info)
 
     return image, info, json_data
 
 
-def check_json_image_type(json_data):
-    img_type = []
+def image_type(json_data):
 
     # Check that jsonData exists
     if not json_data:
@@ -68,13 +69,9 @@ def check_json_image_type(json_data):
 
 def convert_siemens_phase(image, info):
 
-    PHASE_SCALING_SIEMENS: float = 4096
+    PHASE_SCALING_SIEMENS = 4096
 
-    info_converted = info
+    info_converted = copy.copy(info)
     img_converted = image * (math.pi / PHASE_SCALING_SIEMENS)
-
-    # Update header: 16 is the NIfTI code for float; bitpix = number of bits
-    info_converted.header['datatype'] = 16
-    info_converted.header['bitpix'] = 32
 
     return img_converted, info_converted
