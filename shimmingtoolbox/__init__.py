d96a473cc266e16c957616e81fd99160e99c3adb
Julien Cohen-Adad <jcohen@polymtl.ca>
2020-07-20 18:00:43 -0400

    Added __dir_testing__ to be called by unit tests

--- shimmingtoolbox/__init__.py
+++ shimmingtoolbox/__init__.py
@@ -5,6 +5,8 @@ except ImportError:
     # Running on pre-3.8 Python; use importlib-metadata package
     import importlib_metadata as metadata
 __version__ = metadata.version(__name__)
+__dir_testing__ = 'testing_data'
+
 del metadata
 
 from .read_nii import read_nii
