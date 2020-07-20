781400c15d8562776d8e88a0b1457d94f6c86a4a
Julien Cohen-Adad <jcohen@polymtl.ca>
2020-07-20 18:19:59 -0400

    Changed variable name

--- test/test_dicom_to_nifti.py
+++ test/test_dicom_to_nifti.py
@@ -10,8 +10,8 @@ from shimmingtoolbox import __dir_testing__
 def test_dicom_to_nifti():
     # Create temporary folder for processing
     with tempfile.TemporaryDirectory(prefix='st_test_dicom_to_nifti_') as tmp:
-        nifti_path = os.path.join(tmp, 'niftis')
-        dicom_to_nifti(__dir_testing__, nifti_path)
+        path_nifti = os.path.join(tmp, 'niftis')
+        dicom_to_nifti(__dir_testing__, path_nifti)
         # Check if one of the file is created
-        assert os.path.exists(os.path.join(nifti_path, 'sub-', '6_a_gre_DYNshim.json', 'sub-_run-01_MR.nii.gz'))
+        assert os.path.exists(os.path.join(path_nifti, 'sub-', '6_a_gre_DYNshim.json', 'sub-_run-01_MR.nii.gz'))
         # TODO: check if json is created, or maybe implement more sensitive test (e.g. integrity, etc.)
