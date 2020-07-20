71b5e7eeae3b2c74b6329e20754b760e7b7e04a4
Alexandre D'Astous <po09i@hotmail.com>
2020-07-19 23:36:36 -0400

    Removed complex array handling

--- examples/unwrap_script.py
+++ examples/unwrap_script.py
@@ -16,7 +16,7 @@ import requests
 
 import nibabel as nib
 
-from shimmingtoolbox.unwrap import unwrap_phase
+from shimmingtoolbox.unwrap import prelude
 
 
 def main():
@@ -43,17 +43,9 @@ def main():
     nii_phase_e1 = nib.load(fname_phases[0])
     nii_phase_e2 = nib.load(fname_phases[1])
 
-    # Subtract phase
-    phase_diff = nii_phase_e2.get_fdata() - nii_phase_e1.get_fdata()
-
-    # TODO: Convert to a function b0_map
-    # phasediff: (matlab code for 2 echoes)
-    # Z1(:,:,:) = mag_data(:,:,:,1).*exp(1i*ph_data(:,:,:,1));
-    # Z2(:,:,:) = mag_data(:,:,:,2).*exp(1i*ph_data(:,:,:,2));
-    # atan2(imag(Z1(:,:,:).*conj(Z2(:,:,:))),real(Z1(:,:,:).*conj(Z2(:,:,:))));
-    # Convert to radians (Assumes there are wraps)
-
-    phase_diff = phase_diff / 4096 * 2 * np.pi - np.pi
+    # Scale to phase to radians
+    phase_e1 = nii_phase_e1.get_fdata() / 4096 * 2 * np.pi - np.pi
+    phase_e2 = nii_phase_e2.get_fdata() / 4096 * 2 * np.pi - np.pi
 
     # Open mag data
     fname_mags = glob.glob(os.path.join(path_data, 'sub-fieldmap', 'fmap', '*magnitude*.nii.gz'))
@@ -64,21 +56,31 @@ def main():
     nii_mag_e1 = nib.load(fname_mags[0])
     nii_mag_e2 = nib.load(fname_mags[1])
 
-    # TODO: create mask (probably in script)
+    # TODO: Convert to a function b0_map
+    # phasediff: (matlab code for 2 echoes)
+    # Z1(:,:,:) = mag_data(:,:,:,1).*exp(1i*ph_data(:,:,:,1));
+    # Z2(:,:,:) = mag_data(:,:,:,2).*exp(1i*ph_data(:,:,:,2));
+    # atan2(imag(Z1(:,:,:).*conj(Z2(:,:,:))),real(Z1(:,:,:).*conj(Z2(:,:,:))));
+    # Convert to radians (Assumes there are wraps)
+
+    # TODO: create mask
     # Call SCT or user defined mask
     # mask = np.ones(phase_diff.shape)
 
-    complex_array = nii_mag_e1.get_fdata() * np.exp(1j * phase_diff)
-    affine = nii_phase_e1.affine
-    # temp test
-    # complex_array = complex_array[..., np.newaxis, np.newaxis]
-    #
-    unwrapped_phase = unwrap_phase(complex_array, affine, 'prelude')
-    unwrapped_phase = np.real(unwrapped_phase)
+    # Call prelude to unwrap the phase
+    unwrapped_phase_e1 = prelude(phase_e1, nii_mag_e1.get_fdata(), nii_phase_e1.affine)
+    unwrapped_phase_e2 = prelude(phase_e2, nii_mag_e2.get_fdata(), nii_phase_e1.affine)
 
+    # Compute phase difference
+    unwrapped_phase = unwrapped_phase_e2 - unwrapped_phase_e1
+
+    # Compute wrapped phase diff
+    wrapped_phase = phase_e2 - phase_e1
+
+    # Plot results
     plt.figure(figsize=(10, 10))
     plt.subplot(1, 2, 1)
-    plt.imshow(np.angle(complex_array[:-1, :-1, 0]))
+    plt.imshow(wrapped_phase[:-1, :-1, 0])
     plt.title("Wrapped")
     plt.colorbar()
     plt.subplot(1, 2, 2)
