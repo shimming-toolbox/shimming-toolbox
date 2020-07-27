e99a9a7d33a76e8f3bd10c6465a2b7c4532e021c
Julien Cohen-Adad <jcohen@polymtl.ca>
2020-07-27 14:13:17 -0400

    Removed test of dcm2bids version; now specified in setup

--- test/test_dicom_to_nifti.py
+++ test/test_dicom_to_nifti.py
@@ -8,10 +8,9 @@ from shimmingtoolbox.dicom_to_nifti import dicom_to_nifti
 from shimmingtoolbox import __dir_testing__
 
 
-def test_dicom_to_nifti():
-    # Check that the installed version of dcm2bids is recent enough
-    assert(version.parse(dcm2bids.__version__) >= version.parse('2.1.4'))
+def test_dcm2bids_versiondicom_to_nifti_version():
 
+def test_dicom_to_nifti():
     # Create temporary folder for processing the data and execute the tests
     with tempfile.TemporaryDirectory(prefix='st_test_dicom_to_nifti_') as tmp:
         # path_nifti is the folder where the niftis will be temporarily stored
