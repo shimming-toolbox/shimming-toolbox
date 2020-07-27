679879d3a744ce0535f39d231284b1e8abfbfc51
Julien Cohen-Adad <jcohen@polymtl.ca>
2020-07-27 17:00:55 -0400

    Added API: add_suffix()

--- shimmingtoolbox/utils.py
+++ shimmingtoolbox/utils.py
@@ -6,6 +6,40 @@ import os
 import tqdm
 
 
+def add_suffix(fname, suffix):
+    """
+    Add suffix between end of file name and extension.
+
+    :param fname: absolute or relative file name. Example: t2.nii
+    :param suffix: suffix. Example: _mean
+    :return: file name with suffix. Example: t2_mean.nii
+
+    Examples:
+
+    - add_suffix(t2.nii, _mean) -> t2_mean.nii
+    - add_suffix(t2.nii.gz, a) -> t2a.nii.gz
+    """
+
+    def _splitext(fname):
+        """
+        Split a fname (folder/file + ext) into a folder/file and extension.
+
+        Note: for .nii.gz the extension is understandably .nii.gz, not .gz
+        (``os.path.splitext()`` would want to do the latter, hence the special case).
+        """
+        dir, filename = os.path.split(fname)
+        for special_ext in ['.nii.gz', '.tar.gz']:
+            if filename.endswith(special_ext):
+                stem, ext = filename[:-len(special_ext)], special_ext
+                return os.path.join(dir, stem), ext
+        # If no special case, behaves like the regular splitext
+        stem, ext = os.path.splitext(filename)
+        return os.path.join(dir, stem), ext
+
+    stem, ext = _splitext(fname)
+    return os.path.join(stem + suffix + ext)
+
+
 def st_progress_bar(*args, **kwargs):
     """Thin wrapper around `tqdm.tqdm` which checks `SCT_PROGRESS_BAR` muffling the progress
        bar if the user sets it to `no`, `off`, or `false` (case insensitive)."""
