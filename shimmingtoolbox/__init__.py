b7cf6d486a2362ad10e13eb86dc06dd0d151c7e9
Julien Cohen-Adad <jcohen@polymtl.ca>
2020-07-27 15:07:55 -0400

    Refactored dicom_to_nifti
    Direct use of dcm2bids with a config .json file

--- shimmingtoolbox/__init__.py
+++ shimmingtoolbox/__init__.py
@@ -1,11 +1,15 @@
 # https://packaging.python.org/guides/single-sourcing-package-version/
+import os
+
 try:
     from importlib import metadata
 except ImportError:
     # Running on pre-3.8 Python; use importlib-metadata package
     import importlib_metadata as metadata
 __version__ = metadata.version(__name__)
+__dir_shimmingtoolbox__ = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 __dir_testing__ = 'testing_data'
+__dir_config_dcm2bids__ = os.path.join(__dir_shimmingtoolbox__, 'config', 'dcm2bids.json')
 
 del metadata
 
