fad89e5916a54b28d3b580c196d70f12fed3d7a7
Julien Cohen-Adad <jcohen@polymtl.ca>
2020-07-27 15:07:55 -0400

    Refactored dicom_to_nifti
    Direct use of dcm2bids with a config .json file

--- test/test_dicom_to_nifti.py
+++ test/test_dicom_to_nifti.py
@@ -1,20 +1,17 @@
 # coding: utf-8
-import dcm2bids
+
 import os
-from packaging import version
+import pathlib
 import tempfile
 
 from shimmingtoolbox.dicom_to_nifti import dicom_to_nifti
 from shimmingtoolbox import __dir_testing__
 
 
-def test_dcm2bids_versiondicom_to_nifti_version():
-
 def test_dicom_to_nifti():
-    # Create temporary folder for processing the data and execute the tests
-    with tempfile.TemporaryDirectory(prefix='st_test_dicom_to_nifti_') as tmp:
-        # path_nifti is the folder where the niftis will be temporarily stored
-        path_nifti = os.path.join(tmp, 'niftis')
+    # TODO: put as decorator
+    with tempfile.TemporaryDirectory(prefix='st_'+pathlib.Path(__file__).stem) as tmp:
+        path_nifti = os.path.join(tmp, 'nifti')
         dicom_to_nifti(__dir_testing__, path_nifti)
         # Check that all the files (.nii.gz and .json) are created with the expected names
         for i in range(1, 7):
