f906be93cae8290f610d7e534754b8006c32c93a
Julien Cohen-Adad <jcohen@polymtl.ca>
2020-07-27 14:13:17 -0400

    Removed test of dcm2bids version; now specified in setup

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     packages=find_packages(exclude=["contrib", "docs", "tests"]),
     install_requires=[
         "click",
-        "dcm2bids",
+        "dcm2bids==2.1.4",
         'importlib-metadata ~= 1.0 ; python_version < "3.8"',
         "numpy~=1.19.0",
         "phantominator~=0.6.4",
