ba90a299503165e398a1ee4c479f2f9c3f0e80e4
Julien Cohen-Adad <jcohen@polymtl.ca>
2020-07-27 15:46:08 -0400

    Working unit test

--- test/test_dicom_to_nifti.py
+++ test/test_dicom_to_nifti.py
@@ -12,11 +12,12 @@ def test_dicom_to_nifti():
     # TODO: put as decorator
     with tempfile.TemporaryDirectory(prefix='st_'+pathlib.Path(__file__).stem) as tmp:
         path_nifti = os.path.join(tmp, 'nifti')
-        dicom_to_nifti(__dir_testing__, path_nifti)
-        # Check that all the files (.nii.gz and .json) are created with the expected names
+        subject_id = 'sub-test'
+        dicom_to_nifti(__dir_testing__, path_nifti, subject_id=subject_id)
+        # Check that all the files (.nii.gz and .json) are created with the expected names. The test data has 6
+        # magnitude and phase data.
         for i in range(1, 7):
-            i = str(i)
-            assert os.path.exists(os.path.join(path_nifti, 'sub-', '6_a_gre_DYNshim', 'sub-_run-0' + i + '_MR.nii.gz'))
-            assert os.path.exists(os.path.join(path_nifti, 'sub-', '6_a_gre_DYNshim', 'sub-_run-0' + i + '_MR.json'))
-            assert os.path.exists(os.path.join(path_nifti, 'sub-', '7_a_gre_DYNshim', 'sub-_run-0' + i + '_MR.nii.gz'))
-            assert os.path.exists(os.path.join(path_nifti, 'sub-', '7_a_gre_DYNshim', 'sub-_run-0' + i + '_MR.json'))
+            for modality in ['phase', 'magnitude']:
+                for ext in ['nii.gz', 'json']:
+                    assert os.path.exists(os.path.join(path_nifti, subject_id, 'fmap', subject_id + '_{}{}.{}'.format(
+                        modality, i, ext)))
