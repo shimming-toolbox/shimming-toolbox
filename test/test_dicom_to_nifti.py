b01f7a016b23a6a05d690258dfde69aa7d3cd685
Gaspard Cereza <gaspard.cereza@gmail.com>
2020-07-27 14:02:53 -0400

    added test for version of dcm2bids

--- test/test_dicom_to_nifti.py
+++ test/test_dicom_to_nifti.py
@@ -1,6 +1,7 @@
 # coding: utf-8
-
+import dcm2bids
 import os
+from packaging import version
 import tempfile
 
 from shimmingtoolbox.dicom_to_nifti import dicom_to_nifti
@@ -8,11 +9,15 @@ from shimmingtoolbox import __dir_testing__
 
 
 def test_dicom_to_nifti():
-    # Create temporary folder for processing
+    # Check that the installed version of dcm2bids is recent enough
+    assert(version.parse(dcm2bids.__version__) >= version.parse('2.1.4'))
+
+    # Create temporary folder for processing the data and execute the tests
     with tempfile.TemporaryDirectory(prefix='st_test_dicom_to_nifti_') as tmp:
+        # path_nifti is the folder where the niftis will be temporarily stored
         path_nifti = os.path.join(tmp, 'niftis')
         dicom_to_nifti(__dir_testing__, path_nifti)
-        # Check if one of the file is created
+        # Check that all the files (.nii.gz and .json) are created with the expected names
         for i in range(1, 7):
             i = str(i)
             assert os.path.exists(os.path.join(path_nifti, 'sub-', '6_a_gre_DYNshim', 'sub-_run-0' + i + '_MR.nii.gz'))
