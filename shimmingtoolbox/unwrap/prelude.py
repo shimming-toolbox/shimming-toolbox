0d84d9b4e1876c7f44c593eb9944798ffa2e7390
Alex Dastous <47249340+po09i@users.noreply.github.com>
2020-07-30 15:25:46 -0400

    Add unwrap phase functionality (#15)
    * NEW : Added unwrap folder structure
    
    * Added numpy requirement
    
    * Remove version from __init__ subfolder
    
    * Initial function for unwrap phase
    
    * NEW : Initial prelude wrapper
    
    * NEW : fill init
    
    * update requirements
    
    * NEW : add debug script
    
    * make script callable from CL
    
    * Fix delete of files
    
    * Added .idea
    
    * Added download of example data
    
    * Added phase loading and diff
    
    * Update script to use test data
    
    * Return 3d instead of 2d
    
    * Added support for 5d complex arrays
    
    * Added better error handling
    
    * Added 2d support
    
    * Updated according to comments
    
    * Update todo for phaseunwrapping
    
    * Added docstring
    
    * moved mask to be defined in script
    
    * moved mask to script
    
    * added docstring
    
    * added todo
    
    * Update docstring
    
    * Default to no mask
    
    * Changed download function
    
    * Removed unnecessary comment
    
    * Resolve issues in script according to comments
    
    * Update according to comments
    
    * Removed interp function
    
    * Added simple test
    
    * Added deps
    
    * clustered phase code together
    
    * Minor fixes
    
    * Commented out the whole function (not useable anymore)
    
    * Rewrote prelude to have mag and phase inputs
    
    * Removed complex array handling
    
    * Wrote test to accomodate new prelude inputs
    
    * Added download in travis
    
    * forgot a comma
    
    * and another one
    
    * Removed reference to unwrap_phase
    
    * set up tests with the new download function
    
    * Added handling for non default paths
    
    * Added test for non default path
    
    * Added mask tests
    
    * Added tests for mag and phase input
    
    * Added more checks for non_default_masks
    
    * Refactored test to remove duplication
    
    * Now possible to download non-compressed data, e.g. prelude
    
    * Download prelude binary
    
    Related to https://github.com/shimming-toolbox/shimming-toolbox-py/issues/23
    
    * Added click as a dependency
    
    * Fixed hardcoded name when deleting unwrapped nii file
    
    * Calls prelude in tmp folder for every test
    
    * Added default back
    
    * Test prelude -h
    
    * Changed test to .yml file
    
    * Added prelude to the path to be called within script
    
    * Add permissions
    
    * extra char
    
    * Add FSL required variable
    
    * Reduced permissions
    
    * Clean up
    
    * Use download function
    
    * Updated default for mask input argument
    
    * Added API: add_suffix()
    
    * Refactored output of prelude() API
    
    * Removed phase difference todo and opened issue #36
    
    * Updates variables to reflect convention
    
    * Removed error handling from example script
    
    * Resolved comments
    
    * Added 2D as an input type and changed is_unwrapping_in_2d to false
    
    * Removed logging
    
    * Updated tests
    
    * Removed B0 mapping code: out of scope
    
    * Add threshold for more precise masking
    
    * forgot shape
    
    * Apply suggestions from code review
    
    Co-authored-by: Julien Cohen-Adad <jcohen@polymtl.ca>
    
    * Added run_subprocess function
    
    * Use run_subprocess() function
    
    * Replaced glob.glob by __dir_testing__
    
    * Changed matplotlib for oriented-object use
    
    So it doesn't crash on headless systems, see: https://github.com/neuropoly/spinalcordtoolbox/wiki/Moving-away-from-pyplot
    
    * Cosmetic fixes
    
    * Removed unused file
    
    This file is now referenced in #45
    
    Co-authored-by: Julien Cohen-Adad <jcohen@polymtl.ca>

---
+++ shimmingtoolbox/unwrap/prelude.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Wrapper to FSL Prelude (https://fsl.fmrib.ox.ac.uk/fsl/fslwiki/FUGUE/Guide#PRELUDE_.28phase_unwrapping.29)
+"""
+
+import glob
+import os
+import nibabel as nib
+import pathlib
+import tempfile
+import logging
+
+from shimmingtoolbox.utils import run_subprocess
+
+
+def prelude(wrapped_phase, mag, affine, mask=None, threshold=None, is_unwrapping_in_2d=False):
+    """wrapper to FSL prelude
+
+    This function enables phase unwrapping by calling FSL prelude on the command line. A mask can be provided to mask
+    the phase image provided. 2D unwrapping can be turned off. The output path can be specified. The temporary niis
+    can optionally be saved.
+
+    Args:
+        wrapped_phase (numpy.ndarray): 2D or 3D radian numpy array to perform phase unwrapping. (2 pi interval)
+        mag (numpy.ndarray): 2D or 3D magnitude numpy array corresponding to the phase array
+        affine (numpy.ndarray): 2D array containing the transformation coefficients. Can be calculated by using:
+            nii = nib.load("nii_path")
+            affine = nii.affine
+        mask (numpy.ndarray, optional): numpy array of booleans with shape of `complex_array` to mask during phase
+                                        unwrapping
+        threshold: Threshold value for automatic mask generation (Use either mask or threshold, not both)
+        is_unwrapping_in_2d (bool, optional): prelude parameter to unwrap slice by slice
+
+    Returns:
+        numpy.ndarray: 3D array with the shape of `complex_array` of the unwrapped phase output from prelude
+    """
+    # Make sure phase and mag are the right shape
+    if wrapped_phase.ndim not in [2, 3]:
+        raise RuntimeError("Wrapped_phase must be 2d or 3d")
+    if wrapped_phase.shape != mag.shape:
+        raise RuntimeError("The magnitude image (mag) must be the same shape as wrapped_phase")
+
+    tmp = tempfile.TemporaryDirectory(prefix='st_' + pathlib.Path(__file__).stem)
+    path_tmp = tmp.name
+
+    # Save phase and mag images
+    nii_phase = nib.Nifti1Image(wrapped_phase, affine)
+    nib.save(nii_phase, os.path.join(path_tmp, 'rawPhase.nii'))
+    nii_mag = nib.Nifti1Image(mag, affine)
+    nib.save(nii_mag, os.path.join(path_tmp, 'mag.nii'))
+
+    # Fill options
+    options = ''
+    if is_unwrapping_in_2d:
+        options = ' -s'
+
+    # Add mask data and options if there is a mask provided
+    if mask is not None:
+        if mask.shape != wrapped_phase.shape:
+            raise RuntimeError("Mask must be the same shape as wrapped_phase")
+        nii_mask = nib.Nifti1Image(mask, affine)
+
+        options += ' -m '
+        options += os.path.join(path_tmp, 'mask.nii')
+        nib.save(nii_mask, os.path.join(path_tmp, 'mask.nii'))
+
+    if threshold is not None:
+        options += ' -t {}'.format(threshold)
+        if mask is not None:
+            logging.warning('Specifying both a mask and a threshold is not recommended, results might not be what is '
+                            'expected')
+
+    # Unwrap
+    unwrap_command = 'prelude -p {} -a {} -o {}{}'.format(os.path.join(path_tmp, 'rawPhase'),
+                                                          os.path.join(path_tmp, 'mag'),
+                                                          os.path.join(path_tmp, 'rawPhase_unwrapped'), options)
+    logging.info('Unwrap with prelude')
+    run_subprocess(unwrap_command)
+
+    fname_phase_unwrapped = glob.glob(os.path.join(path_tmp, 'rawPhase_unwrapped*'))[0]
+
+    return nib.load(fname_phase_unwrapped).get_fdata()
