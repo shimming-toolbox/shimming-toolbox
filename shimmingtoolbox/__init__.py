1607eceb5fb3ff8a2f2188fab760b61cb90f6d23
Nick Guenther <nguenthe@uwaterloo.ca>
2020-07-30 04:05:42 -0400

    Put my versioning snippet back together.

--- shimmingtoolbox/__init__.py
+++ shimmingtoolbox/__init__.py
@@ -1,16 +1,16 @@
-# https://packaging.python.org/guides/single-sourcing-package-version/
 import os
 
+# https://packaging.python.org/guides/single-sourcing-package-version/
 try:
     from importlib import metadata
 except ImportError:
     # Running on pre-3.8 Python; use importlib-metadata package
     import importlib_metadata as metadata
 __version__ = metadata.version(__name__)
+del metadata
+
 __dir_shimmingtoolbox__ = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 __dir_testing__ = 'testing_data'
 __dir_config_dcm2bids__ = os.path.join(__dir_shimmingtoolbox__, 'config', 'dcm2bids.json')
-
-del metadata
 
 from .dicom_to_nifti import dicom_to_nifti
