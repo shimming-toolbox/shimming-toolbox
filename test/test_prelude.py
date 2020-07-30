b4ab51977b67e7f12d9058d3315da292ad6f1efa
Alex Dastous <47249340+po09i@users.noreply.github.com>
2020-07-30 15:25:46 -0400

    Add unwrap phase functionality (#15)
    * NEW : Added unwrap folder structure
    
    * Added numpy requirement
    
    * Remove version from __init__ subfolder
    
    * Initial function for unwrap phase
    
    * NEW : Initial prelude wrapper
    
    * NEW : fill init
    
    * update requirements
    
    * NEW : add debug script
    
    * make script callable from CL
    
    * Fix delete of files
    
    * Added .idea
    
    * Added download of example data
    
    * Added phase loading and diff
    
    * Update script to use test data
    
    * Return 3d instead of 2d
    
    * Added support for 5d complex arrays
    
    * Added better error handling
    
    * Added 2d support
    
    * Updated according to comments
    
    * Update todo for phaseunwrapping
    
    * Added docstring
    
    * moved mask to be defined in script
    
    * moved mask to script
    
    * added docstring
    
    * added todo
    
    * Update docstring
    
    * Default to no mask
    
    * Changed download function
    
    * Removed unnecessary comment
    
    * Resolve issues in script according to comments
    
    * Update according to comments
    
    * Removed interp function
    
    * Added simple test
    
    * Added deps
    
    * clustered phase code together
    
    * Minor fixes
    
    * Commented out the whole function (not useable anymore)
    
    * Rewrote prelude to have mag and phase inputs
    
    * Removed complex array handling
    
    * Wrote test to accomodate new prelude inputs
    
    * Added download in travis
    
    * forgot a comma
    
    * and another one
    
    * Removed reference to unwrap_phase
    
    * set up tests with the new download function
    
    * Added handling for non default paths
    
    * Added test for non default path
    
    * Added mask tests
    
    * Added tests for mag and phase input
    
    * Added more checks for non_default_masks
    
    * Refactored test to remove duplication
    
    * Now possible to download non-compressed data, e.g. prelude
    
    * Download prelude binary
    
    Related to https://github.com/shimming-toolbox/shimming-toolbox-py/issues/23
    
    * Added click as a dependency
    
    * Fixed hardcoded name when deleting unwrapped nii file
    
    * Calls prelude in tmp folder for every test
    
    * Added default back
    
    * Test prelude -h
    
    * Changed test to .yml file
    
    * Added prelude to the path to be called within script
    
    * Add permissions
    
    * extra char
    
    * Add FSL required variable
    
    * Reduced permissions
    
    * Clean up
    
    * Use download function
    
    * Updated default for mask input argument
    
    * Added API: add_suffix()
    
    * Refactored output of prelude() API
    
    * Removed phase difference todo and opened issue #36
    
    * Updates variables to reflect convention
    
    * Removed error handling from example script
    
    * Resolved comments
    
    * Added 2D as an input type and changed is_unwrapping_in_2d to false
    
    * Removed logging
    
    * Updated tests
    
    * Removed B0 mapping code: out of scope
    
    * Add threshold for more precise masking
    
    * forgot shape
    
    * Apply suggestions from code review
    
    Co-authored-by: Julien Cohen-Adad <jcohen@polymtl.ca>
    
    * Added run_subprocess function
    
    * Use run_subprocess() function
    
    * Replaced glob.glob by __dir_testing__
    
    * Changed matplotlib for oriented-object use
    
    So it doesn't crash on headless systems, see: https://github.com/neuropoly/spinalcordtoolbox/wiki/Moving-away-from-pyplot
    
    * Cosmetic fixes
    
    * Removed unused file
    
    This file is now referenced in #45
    
    Co-authored-by: Julien Cohen-Adad <jcohen@polymtl.ca>

---
+++ test/test_prelude.py
@@ -0,0 +1,167 @@
+# coding: utf-8
+
+from pathlib import Path
+import shutil
+import os
+import glob
+import nibabel as nib
+import numpy as np
+import logging
+
+from shimmingtoolbox.unwrap import prelude
+
+
+class TestCore(object):
+
+    def setup(self):
+        # Get the directory where this current file is saved
+        self.full_path = Path(__file__).resolve().parent
+        # "test/" directory
+        self.test_path = self.full_path
+
+        # Create temp folder
+        # Get the directory where this current file is saved
+        self.tmp_path = self.test_path / '__tmp__'
+        if not self.tmp_path.exists():
+            self.tmp_path.mkdir()
+        self.toolbox_path = self.test_path.parent
+
+        self.get_phases_mags_affines()
+
+    def teardown(self):
+        # Remove temporary files
+        if self.tmp_path.exists():
+            shutil.rmtree(self.tmp_path)
+            pass
+
+    def get_phases_mags_affines(self):
+        """
+        Get the phase and mag images (as np.array) and affine matrices from the testing data.
+        """
+        path_data = glob.glob(os.path.join(self.toolbox_path, 'testing_data*'))[0]
+
+        # Open phase data
+        fname_phases = glob.glob(os.path.join(path_data, 'sub-fieldmap', 'fmap', '*phase*.nii.gz'))
+        if len(fname_phases) > 2:
+            raise IndexError('Phase data parsing is wrongly parsed')
+
+        nii_phase_e1 = nib.load(fname_phases[0])
+        nii_phase_e2 = nib.load(fname_phases[1])
+
+        # Scale to phase to radians
+        phase_e1 = nii_phase_e1.get_fdata() / 4096 * 2 * np.pi - np.pi
+        phase_e2 = nii_phase_e2.get_fdata() / 4096 * 2 * np.pi - np.pi
+
+        # Open mag data
+        fname_mags = glob.glob(os.path.join(path_data, 'sub-fieldmap', 'fmap', '*magnitude*.nii.gz'))
+
+        if len(fname_mags) > 2:
+            raise IndexError('Mag data parsing is wrongly parsed')
+
+        nii_mag_e1 = nib.load(fname_mags[0])
+        nii_mag_e2 = nib.load(fname_mags[1])
+
+        self.phase_e1 = phase_e1
+        self.phase_e2 = phase_e2
+        self.mag_e1 = nii_mag_e1.get_fdata()
+        self.mag_e2 = nii_mag_e2.get_fdata()
+        self.affine_phase_e1 = nii_phase_e1.affine
+        self.affine_phase_e2 = nii_phase_e2.affine
+
+    def test_default_works(self):
+        """
+        Runs prelude and check output integrity.
+        """
+        # default prelude call
+        unwrapped_phase_e1 = prelude(self.phase_e1, self.mag_e1, self.affine_phase_e1)
+
+        assert (unwrapped_phase_e1.shape == self.phase_e1.shape)
+
+    def test_non_default_mask(self):
+        """
+        Check prelude function with input binary mask.
+        """
+        # Create mask with all ones
+        mask = np.ones(self.phase_e1.shape)
+
+        # Call prelude with mask (is_unwrapping_in_2d is also used because it is significantly faster)
+        unwrapped_phase_e1 = prelude(self.phase_e1, self.mag_e1, self.affine_phase_e1, mask, is_unwrapping_in_2d=True)
+
+        # Make sure the phase is not 0. When there isn't a mask, the phase is 0
+        assert(unwrapped_phase_e1[5, 5, 5] != 0)
+
+    def test_wrong_size_mask(self):
+        # Create mask with wrong dimensions
+        mask = np.ones([4, 4, 4])
+
+        # Call prelude with mask
+        try:
+            prelude(self.phase_e1, self.mag_e1, self.affine_phase_e1, mask)
+        except RuntimeError:
+            # If an exception occurs, this is the desired behaviour since the mask is the wrong dimensions
+            return 0
+
+        # If there isn't an error, then there is a problem
+        print('\nWrong dimensions for mask does not throw an error')
+        assert False
+
+    def test_wrong_phase_dimensions(self):
+        # Call prelude phase with wrong dimensions
+        phase_e1 = np.ones([4, 4])
+
+        try:
+            prelude(phase_e1, self.mag_e1, self.affine_phase_e1)
+        except RuntimeError:
+            # If an exception occurs, this is the desired behaviour
+            return 0
+
+        # If there isn't an error, then there is a problem
+        print('\nWrong dimensions for phase input')
+        assert False
+
+    def test_wrong_mag_dimensions(self):
+        # Call prelude phase with wrong dimensions
+        mag_e1 = np.ones([4, 4, 4])
+
+        try:
+            prelude(self.phase_e1, mag_e1, self.affine_phase_e1)
+        except RuntimeError:
+            # If an exception occurs, this is the desired behaviour
+            return 0
+
+        # If there isn't an error, then there is a problem
+        print('\nWrong dimensions for mag input')
+        assert False
+
+    def test_wrong_mag_and_phase_dimensions(self):
+        # Call prelude phase with wrong dimensions
+        mag_e1 = np.ones([4, 4, 4])
+        phase_e1 = np.ones([4])
+
+        try:
+            prelude(phase_e1, mag_e1, self.affine_phase_e1)
+        except RuntimeError:
+            # If an exception occurs, this is the desired behaviour
+            return 0
+
+        # If there isn't an error, then there is a problem
+        print('\nWrong dimensions both mag and phase input')
+        assert False
+
+    def test_phase_2d(self):
+        """
+        Call prelude with a 2D phase and mag array
+        """
+        # Get first slice
+        phase_e1_2d = self.phase_e1[:, :, 0]
+        mag_e1_2d = self.mag_e1[:, :, 0]
+        unwrapped_phase_e1 = prelude(phase_e1_2d, mag_e1_2d, self.affine_phase_e1)
+
+        assert(unwrapped_phase_e1.shape == phase_e1_2d.shape)
+
+    def test_threshold(self):
+        """
+        Call prelude with a threshold for masking
+        """
+        unwrapped_phase_e1 = prelude(self.phase_e1, self.mag_e1, self.affine_phase_e1, threshold=200)
+        assert(unwrapped_phase_e1.shape == self.phase_e1.shape)
