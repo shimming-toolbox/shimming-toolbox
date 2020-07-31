5fcecc6721c2b10f1d9edf9762837f17c3656f19
Alexandre D'Astous <po09i@hotmail.com>
2020-07-31 15:38:36 -0400

    Add a test file to test if scripts run appropriatly

---
+++ test/test_example_scripts.py
@@ -0,0 +1,36 @@
+# coding: utf-8
+
+from pathlib import Path
+import shutil
+import os
+
+from shimmingtoolbox.utils import run_subprocess
+
+
+class TestCore(object):
+
+    def setup(self):
+        # Get the directory where this current file is saved
+        self.full_path = Path(__file__).resolve().parent
+        self.path_nifti = os.path.join(self.full_path, 'niftis')
+        self.path_testing_data = os.path.join(self.full_path, 'testing_data')
+        self.fname_nifti = os.path.join(self.full_path, 'unwrap_phase_plot.png')
+
+    def teardown(self):
+        # Remove temporary files
+        if os.path.isdir(self.path_nifti):
+            shutil.rmtree(self.path_nifti)
+
+        if os.path.isdir(self.path_testing_data):
+            shutil.rmtree(self.path_testing_data)
+
+        if os.path.isfile(self.fname_nifti):
+            os.remove(self.fname_nifti)
+
+    def test_demo_script_outputs_figure(self):
+
+        run_subprocess('st_general_demo')
+
+        assert (os.path.isfile(self.fname_nifti))
+        assert (os.path.isdir(self.path_nifti))
+        assert (os.path.isdir(self.path_testing_data))
