31dffbc145b08c9959296662c3a7a69d918ea3ec
Alexandre D'Astous <po09i@hotmail.com>
2020-08-05 01:13:43 -0400

    Refactored test

--- test/test_example_scripts.py
+++ test/test_example_scripts.py
@@ -5,36 +5,27 @@ import shutil
 import os
 
 from examples import general_demo
-from shimmingtoolbox.utils import run_subprocess
+
 
 class TestCore(object):
 
     def setup(self):
         # Get the directory where this current file is saved
-        self.full_path = Path(__file__).resolve().parent
-        self.path_nifti = os.path.join(self.full_path, 'niftis')
-        self.path_testing_data = os.path.join(self.full_path, 'testing_data')
-        self.fname_nifti = os.path.join(self.full_path, 'unwrap_phase_plot.png')
+        self.test_path = Path(__file__).resolve().parent
+
+        # Create tmp folder
+        self.tmp_path = self.test_path / '__tmp__'
+        if not self.tmp_path.exists():
+            self.tmp_path.mkdir()
 
     def teardown(self):
         # Remove temporary files
-        if os.path.isdir(self.path_nifti):
-            shutil.rmtree(self.path_nifti)
-
-        if os.path.isdir(self.path_testing_data):
-            shutil.rmtree(self.path_testing_data)
-
-        if os.path.isfile(self.fname_nifti):
-            os.remove(self.fname_nifti)
+        if self.tmp_path.exists():
+            shutil.rmtree(self.tmp_path)
 
     def test_demo_script_outputs_figure(self):
 
-        general_demo.main()
-        
-        # Degug
-        print(self.full_path)
-        run_subprocess('ls {}'.format(self.full_path))
+        fname_nifti = general_demo.main(self.test_path)
 
-        assert (os.path.isfile(self.fname_nifti))
-        assert (os.path.isdir(self.path_nifti))
-        assert (os.path.isdir(self.path_testing_data))
+        assert (os.path.isfile(fname_nifti))
+
