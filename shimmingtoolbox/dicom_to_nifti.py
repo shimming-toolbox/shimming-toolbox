04557b62cc571a7685adf0a1847dc47fdba39e71
Gaspard Cereza <gaspard.cereza@gmail.com>
2020-08-06 14:31:02 -0400

    added possibility to handle the conversion of rfmap particular dicoms

--- shimmingtoolbox/dicom_to_nifti.py
+++ shimmingtoolbox/dicom_to_nifti.py
@@ -1,13 +1,15 @@
 # coding: utf-8
 
-# TODO: check in unit test if dcm2bids_scaffold is installed, and also check for the required version.
-
+import os
 import subprocess
+import shutil
+import tempfile
 
 from shimmingtoolbox import __dir_config_dcm2bids__
 
 
-def dicom_to_nifti(path_dicom, path_nifti, subject_id='sub-01', path_config_dcm2bids=__dir_config_dcm2bids__):
+def dicom_to_nifti(path_dicom, path_nifti, subject_id='sub-01', path_config_dcm2bids=__dir_config_dcm2bids__,
+                   special_dicom = False):
     """ Converts dicom files into nifti files by calling dcm2bids
 
     Args:
@@ -15,6 +17,26 @@ def dicom_to_nifti(path_dicom, path_nifti, subject_id='sub-01', path_config_dcm2
         path_nifti (str): path to the output nifti folder
 
     """
-    cmd = 'dcm2bids -d {} -p {} -o {} -l DEBUG -c {}'.format(
-        path_dicom, subject_id, path_nifti, path_config_dcm2bids)
-    subprocess.run(cmd.split(' '))
+    if special_dicom:
+        with tempfile.TemporaryDirectory(prefix='st_mag_') as tmp_mag:
+            dicom_number = len(os.listdir(path_dicom))
+            dicom_list = os.listdir(path_dicom)
+            for i in range(int(dicom_number/2)):
+                shutil.copy(os.path.join(path_dicom, dicom_list[i]), tmp_mag)
+            cmd = 'dcm2bids -d {} -p {} -o {} -l DEBUG -c {}'.format(
+                tmp_mag, subject_id, path_nifti, path_config_dcm2bids)
+            subprocess.run(cmd.split(' '))
+
+        with tempfile.TemporaryDirectory(prefix='st_mag_') as tmp_phase:
+            dicom_number = len(os.listdir(path_dicom))
+            dicom_list = os.listdir(path_dicom)
+            for i in range(int(dicom_number/2), int(dicom_number)):
+                shutil.copy(os.path.join(path_dicom, dicom_list[i]), tmp_phase)
+            cmd = 'dcm2bids -d {} -p {} -o {} -l DEBUG -c {}'.format(
+                tmp_phase, subject_id, path_nifti, path_config_dcm2bids)
+            subprocess.run(cmd.split(' '))
+
+    else:
+        cmd = 'dcm2bids -d {} -p {} -o {} -l DEBUG -c {}'.format(
+            path_dicom, subject_id, path_nifti, path_config_dcm2bids)
+        subprocess.run(cmd.split(' '))
--- test/test_dicom_to_nifti.py
+++ test/test_dicom_to_nifti.py
@@ -13,7 +13,9 @@ def test_dicom_to_nifti():
     with tempfile.TemporaryDirectory(prefix='st_'+pathlib.Path(__file__).stem) as tmp:
         path_nifti = os.path.join(tmp, 'nifti')
         subject_id = 'sub-test'
-        dicom_to_nifti(__dir_testing__, path_nifti, subject_id=subject_id)
+        # Conversion of the B0 mapping dicoms
+        dicom_to_nifti(os.path.join(__dir_testing__, 'dicom_unsorted'), path_nifti, subject_id=subject_id)
+
         # Check that all the files (.nii.gz and .json) are created with the expected names. The test data has 6
         # magnitude and phase data.
         for i in range(1, 7):
@@ -21,3 +23,10 @@ def test_dicom_to_nifti():
                 for ext in ['nii.gz', 'json']:
                     assert os.path.exists(os.path.join(path_nifti, subject_id, 'fmap', subject_id + '_{}{}.{}'.format(
                         modality, i, ext)))
+
+        # Conversion of the RF maps
+        dicom_to_nifti(os.path.join(__dir_testing__, 'b1_maps'), path_nifti, subject_id=subject_id, special_dicom=True)
+        for modality in ['phase', 'mag']:
+            for ext in ['nii.gz', 'json']:
+                assert os.path.exists(os.path.join(path_nifti, subject_id, 'rfmap',
+                                                   subject_id + '_rfmap_{}.{}'.format(modality, ext)))
