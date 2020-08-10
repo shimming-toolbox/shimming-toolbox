82e09cf2dfe86b0755d0b9cde9e6e63548efc24e
Julien Cohen-Adad <jcohen@polymtl.ca>
2020-08-10 15:09:13 -0400

    Implemented special case-- unfinished

--- shimmingtoolbox/dicom_to_nifti.py
+++ shimmingtoolbox/dicom_to_nifti.py
@@ -9,32 +9,41 @@ from shimmingtoolbox import __dir_config_dcm2bids__
 
 
 def dicom_to_nifti(path_dicom, path_nifti, subject_id='sub-01', path_config_dcm2bids=__dir_config_dcm2bids__,
-                   special_dicom = False):
+                   special_dicom=[]):
     """ Converts dicom files into nifti files by calling dcm2bids
 
     Args:
         path_dicom (str): path to the input dicom folder
         path_nifti (str): path to the output nifti folder
-
+        subject_id (str): ID of subject
+        path_config_dcm2bids (str): Path to dcm2bids.json file
+        special_dicom (:obj:`list` of :obj:`str`): List of modalities for which the output NIFTI from dcm2niix should
+        be half-split along the last dimension.
     """
-    # Case where the DICOM files have
-    if special_dicom:
-        with tempfile.TemporaryDirectory(prefix='st_mag_') as tmp_mag:
-            dicom_number = len(os.listdir(path_dicom))
-            dicom_list = os.listdir(path_dicom)
-            for i in range(int(dicom_number/2)):
-                shutil.copy(os.path.join(path_dicom, dicom_list[i]), tmp_mag)
-            cmd = f'dcm2bids -d {tmp_mag} -p {subject_id} -o {path_nifti} -l DEBUG -c {path_config_dcm2bids}'
-            subprocess.run(cmd.split(' '))
+    # # Case where the DICOM files have
+    # if special_dicom:
+    #     with tempfile.TemporaryDirectory(prefix='st_mag_') as tmp_mag:
+    #         dicom_number = len(os.listdir(path_dicom))
+    #         dicom_list = os.listdir(path_dicom)
+    #         for i in range(int(dicom_number/2)):
+    #             shutil.copy(os.path.join(path_dicom, dicom_list[i]), tmp_mag)
+    #         cmd = f'dcm2bids -d {tmp_mag} -p {subject_id} -o {path_nifti} -l DEBUG -c {path_config_dcm2bids}'
+    #         subprocess.run(cmd.split(' '))
+    #
+    #     with tempfile.TemporaryDirectory(prefix='st_mag_') as tmp_phase:
+    #         dicom_number = len(os.listdir(path_dicom))
+    #         dicom_list = os.listdir(path_dicom)
+    #         for i in range(int(dicom_number/2), int(dicom_number)):
+    #             shutil.copy(os.path.join(path_dicom, dicom_list[i]), tmp_phase)
+    #         cmd = f'dcm2bids -d {tmp_phase} -p {subject_id} -o {path_nifti} -l DEBUG -c {path_config_dcm2bids}'
+    #         subprocess.run(cmd.split(' '))
+    #
+    # else:
+    cmd = f'dcm2bids -d {path_dicom} -p {subject_id} -o {path_nifti} -l DEBUG -c {path_config_dcm2bids}'
+    subprocess.run(cmd.split(' '))
 
-        with tempfile.TemporaryDirectory(prefix='st_mag_') as tmp_phase:
-            dicom_number = len(os.listdir(path_dicom))
-            dicom_list = os.listdir(path_dicom)
-            for i in range(int(dicom_number/2), int(dicom_number)):
-                shutil.copy(os.path.join(path_dicom, dicom_list[i]), tmp_phase)
-            cmd = f'dcm2bids -d {tmp_phase} -p {subject_id} -o {path_nifti} -l DEBUG -c {path_config_dcm2bids}'
-            subprocess.run(cmd.split(' '))
-
-    else:
-        cmd = f'dcm2bids -d {path_dicom} -p {subject_id} -o {path_nifti} -l DEBUG -c {path_config_dcm2bids}'
-        subprocess.run(cmd.split(' '))
+    # If output nifti needs to be split along the last dimension. This could happen if there is not enough information
+    # in the input dicom data.
+    # TODO: assert input special_dicom
+    if special_dicom is not []:
+        a=1
--- test/test_dicom_to_nifti.py
+++ test/test_dicom_to_nifti.py
@@ -8,7 +8,7 @@ from shimmingtoolbox.dicom_to_nifti import dicom_to_nifti
 from shimmingtoolbox import __dir_testing__
 
 
-def test_dicom_to_nifti():
+def test_dicom_to_nifti_unsorted():
     # TODO: put as decorator
     with tempfile.TemporaryDirectory(prefix='st_'+pathlib.Path(__file__).stem) as tmp:
         path_nifti = os.path.join(tmp, 'nifti')
@@ -24,8 +24,18 @@ def test_dicom_to_nifti():
                     assert os.path.exists(os.path.join(path_nifti, subject_id, 'fmap',
                                                        subject_id + f'_{modality}{i}.{ext}'))
 
+
+def test_dicom_to_nifti_b1map():
+    """"
+    Test special cases of DICOM files that don't have enough information to be properly converted by dcm2niix.
+    More details at: https://github.com/shimming-toolbox/shimming-toolbox-py/issues/58
+    """
+    with tempfile.TemporaryDirectory(prefix='st_'+pathlib.Path(__file__).stem) as tmp:
+        path_nifti = os.path.join(tmp, 'nifti')
+        subject_id = 'sub-test'
+
         # Conversion of the RF maps
-        dicom_to_nifti(os.path.join(__dir_testing__, 'b1_maps'), path_nifti, subject_id=subject_id, special_dicom=True)
+        dicom_to_nifti(os.path.join(__dir_testing__, 'b1_maps'), path_nifti, subject_id=subject_id, special_dicom=['TB1map'])
         for modality in ['phase', 'mag']:
             for ext in ['nii.gz', 'json']:
                 assert os.path.exists(os.path.join(path_nifti, subject_id, 'rfmap',
