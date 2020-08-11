cfd158d88f9413c4e89ef8be4fbce412b1181724
Alexandre D'Astous <po09i@hotmail.com>
2020-08-10 23:32:25 -0400

    Added suggestions and new implementation

--- shimmingtoolbox/masking/shape.py
+++ shimmingtoolbox/masking/shape.py
@@ -2,47 +2,10 @@
 # -*- coding: utf-8 -*-
 """
 Image mask with shape API
+# TODO: Make sure specified center and length fit within data.shape
 """
 
 import numpy as np
-
-
-def shape(data, shape, center_x=None, center_y=None, center_z=None, len_x=None, len_y=None, len_z=None, radius=None):
-    """
-    Wrapper to different shape masking algos
-
-    Args:
-
-    Returns:
-        nd.array: mask
-    """
-
-    # allowed_shapes = {'square', 'cube', 'disk', 'ball'}
-
-    # Make sure input params are valid
-    if data.ndim not in [2, 3]:
-        raise RuntimeError('Number of dimensions not supported')
-
-    # TODO: Make sure specified center and length fit within data.shape
-
-    # Create mask with all zeros
-    mask = np.zeros_like(data)
-
-    if shape == 'square':
-        mask = shape_square(data, center_x, center_y, len_x, len_y)
-    elif shape == 'cube':
-        # TODO
-        pass
-    elif shape == 'disk':
-        # TODO
-        pass
-    elif shape == 'ball':
-        # TODO
-        pass
-    else:
-        raise RuntimeError('Input shapes not part of the allowed shapes for masking')
-
-    return mask
 
 
 def shape_square(data, center_x, center_y, len_x, len_y):
@@ -64,7 +27,47 @@ def shape_square(data, center_x, center_y, len_x, len_y):
     if center_y is None:
         center_y = int(data.shape[1] / 2)
 
-    mask = np.zeros_like(data)
-    mask[center_y - int(np.floor(len_y / 2)):center_y + int(np.ceil(len_y / 2)), center_x - int(np.floor(len_x / 2)):center_x + int(np.ceil(len_x / 2))] = 1
+    # Create a meshgrid of the size of input data
+    yv, xv = np.meshgrid(np.arange(0, data.shape[1]), np.arange(0, data.shape[0]))
+
+    # Create the rectangle by allowing values from greater or lower than specified inputs
+    xv_logical = np.logical_and(xv >= center_x - int(np.floor(len_x / 2)), xv < center_x + int(np.ceil(len_x / 2)))
+    yv_logical = np.logical_and(yv >= center_y - int(np.floor(len_y / 2)), yv < center_y + int(np.ceil(len_y / 2)))
+    mask = np.logical_and(xv_logical, yv_logical)
+
+    return mask
+
+
+def shape_cube(data, center_x, center_y, center_z, len_x, len_y, len_z):
+    pass
+
+
+def shape_disk(data, center_x, center_y, radius):
+    pass
+
+
+def shape_sphere(data, center_x, center_y, center_z, radius):
+    pass
+
+
+shape_mask = {'square': shape_square, 'cube': shape_cube, 'disk': shape_disk, 'sphere': shape_sphere}
+
+
+def shape(data, shape, **kargs):
+    # center_x=None, center_y=None, center_z=None, len_x=None, len_y=None, len_z=None, radius=None
+    """
+    Wrapper to different shape masking algos
+
+    Args:
+
+    Returns:
+        nd.array: mask
+    """
+
+    mask_info = {}
+    for key, value in kargs.items():
+        mask_info[key] = value
+
+    mask = shape_mask[shape](data, **mask_info)
 
     return mask
