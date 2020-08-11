f3509ac3dc26a06aed3ca1e7a9293f3948b9d541
Alexandre D'Astous <po09i@hotmail.com>
2020-08-10 23:33:00 -0400

    Added cube mask

--- shimmingtoolbox/masking/shape.py
+++ shimmingtoolbox/masking/shape.py
@@ -39,7 +39,36 @@ def shape_square(data, center_x, center_y, len_x, len_y):
 
 
 def shape_cube(data, center_x, center_y, center_z, len_x, len_y, len_z):
-    pass
+    """
+    Create a cube mask
+
+    Args:
+
+    Returns:
+        nd.array: mask
+    """
+
+    # Only takes data as an X,Y array
+    if data.ndim != 3:
+        raise RuntimeError('shape_square only allows for 2 dimensions')
+
+    if center_x is None:
+        center_x = int(data.shape[0] / 2)
+    if center_y is None:
+        center_y = int(data.shape[1] / 2)
+    if center_z is None:
+        center_z = int(data.shape[2] / 2)
+
+    # Create a meshgrid of the size of input data
+    yv, xv, zv = np.meshgrid(np.arange(0, data.shape[1]), np.arange(0, data.shape[0]), np.arange(0, data.shape[2]))
+
+    # Create the rectangle by allowing values from greater or lower than specified inputs
+    xv_logical = np.logical_and(xv >= center_x - int(np.floor(len_x / 2)), xv < center_x + int(np.ceil(len_x / 2)))
+    yv_logical = np.logical_and(yv >= center_y - int(np.floor(len_y / 2)), yv < center_y + int(np.ceil(len_y / 2)))
+    zv_logical = np.logical_and(zv >= center_z - int(np.floor(len_z / 2)), zv < center_z + int(np.ceil(len_z / 2)))
+    mask = np.logical_and(xv_logical, yv_logical, zv_logical)
+
+    return mask
 
 
 def shape_disk(data, center_x, center_y, radius):
