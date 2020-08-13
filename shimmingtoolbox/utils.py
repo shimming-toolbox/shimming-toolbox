de05090595251259ef4fb271b8d50c79b613cba5
jidicula <johanan.idicula@gmail.com>
2020-08-13 19:24:31 -0400

    [style][utils] Reformat with Black

--- shimmingtoolbox/utils.py
+++ shimmingtoolbox/utils.py
@@ -14,8 +14,8 @@ def run_subprocess(cmd):
     Args:
         cmd (string): full command to be run on the command line
     """
-    logging.debug('{}'.format(cmd))
-    subprocess.run(cmd.split(' '), stdout=subprocess.PIPE, text=True, check=True)
+    logging.debug("{}".format(cmd))
+    subprocess.run(cmd.split(" "), stdout=subprocess.PIPE, text=True, check=True)
 
 
 def add_suffix(fname, suffix):
@@ -42,9 +42,9 @@ def add_suffix(fname, suffix):
         (``os.path.splitext()`` would want to do the latter, hence the special case).
         """
         dir, filename = os.path.split(fname)
-        for special_ext in ['.nii.gz', '.tar.gz']:
+        for special_ext in [".nii.gz", ".tar.gz"]:
             if filename.endswith(special_ext):
-                stem, ext = filename[:-len(special_ext)], special_ext
+                stem, ext = filename[: -len(special_ext)], special_ext
                 return os.path.join(dir, stem), ext
         # If no special case, behaves like the regular splitext
         stem, ext = os.path.splitext(filename)
@@ -57,8 +57,8 @@ def add_suffix(fname, suffix):
 def st_progress_bar(*args, **kwargs):
     """Thin wrapper around `tqdm.tqdm` which checks `SCT_PROGRESS_BAR` muffling the progress
        bar if the user sets it to `no`, `off`, or `false` (case insensitive)."""
-    do_pb = os.environ.get('SCT_PROGRESS_BAR', 'yes')
-    if do_pb.lower() in ['off', 'no', 'false']:
-        kwargs['disable'] = True
+    do_pb = os.environ.get("SCT_PROGRESS_BAR", "yes")
+    if do_pb.lower() in ["off", "no", "false"]:
+        kwargs["disable"] = True
 
     return tqdm.tqdm(*args, **kwargs)
