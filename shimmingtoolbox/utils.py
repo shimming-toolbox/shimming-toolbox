9c27ce2e3357e31df04599a7efd3e7ad6f6c62b7
jidicula <johanan.idicula@gmail.com>
2020-08-13 19:37:08 -0400

    [docs][utils] Fix docstring in `st_progress_bar()`
    **Why this change was necessary**
    The docstring was previously formatted incorrectly and did not comply
    with Google docstring style.
    
    **What this change does**
    Refactors the docstring according to Google docstring style.
    
    **Any side-effects?**
    None
    
    **Additional context/notes/links**
    
    Resolves: #67

--- shimmingtoolbox/utils.py
+++ shimmingtoolbox/utils.py
@@ -56,8 +56,8 @@ def add_suffix(fname, suffix):
 
 
 def st_progress_bar(*args, **kwargs):
-    """Thin wrapper around `tqdm.tqdm` which checks `SCT_PROGRESS_BAR` muffling the progress
-       bar if the user sets it to `no`, `off`, or `false` (case insensitive)."""
+    """Thin wrapper around ``tqdm.tqdm`` which checks ``SCT_PROGRESS_BAR``, muffling the progress
+       bar if the user sets it to ``no``, ``off``, or ``false`` (case insensitive)."""
     do_pb = os.environ.get("SCT_PROGRESS_BAR", "yes")
     if do_pb.lower() in ["off", "no", "false"]:
         kwargs["disable"] = True
