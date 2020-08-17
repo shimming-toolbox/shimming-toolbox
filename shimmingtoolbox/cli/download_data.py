09cea619e951d46b02bca347692df4712c12e6a2
jidicula <johanan.idicula@gmail.com>
2020-08-17 09:54:52 -0400

    Revert "[style][download_data] Format with Black"
    This reverts commit 7d3cb3b9305858e83fc972680ca3e288df362029.
    
    Addresses: https://github.com/shimming-toolbox/shimming-toolbox-py/pull/72#discussion_r470665353

--- shimmingtoolbox/cli/download_data.py
+++ shimmingtoolbox/cli/download_data.py
@@ -7,19 +7,15 @@ import click
 from shimmingtoolbox.download import install_data
 
 dict_url = {
-    "testing_data": [
-        "https://github.com/shimming-toolbox/data-testing/archive/r20200713.zip"
-    ],
-    "prelude": ["https://github.com/shimming-toolbox/binaries/raw/master/prelude"],
+    "testing_data": ["https://github.com/shimming-toolbox/data-testing/archive/r20200713.zip"],
+    "prelude": ["https://github.com/shimming-toolbox/binaries/raw/master/prelude"]
 }
 
 
 # TODO: display automatically the list of data available from the dict above
 # TODO: wrap the help properly
-@click.command(
-    help="Download data from the internet. The available datasets are:"
-    "- testing_data: Light-weighted dataset for testing purpose."
-)
+@click.command(help="Download data from the internet. The available datasets are:"
+                    "- testing_data: Light-weighted dataset for testing purpose.")
 @click.option("--verbose", is_flag=True, help="Be more verbose.")
 @click.option("--output", help="Output folder.")
 @click.argument("data")
@@ -35,12 +31,12 @@ def main(verbose, output, data):
     # TODO: logging does not seem to output on the terminal
     if verbose:
         logging.getLogger().setLevel(logging.INFO)
-    logging.info(f"{output}, {data}")
+    logging.info(f'{output}, {data}')
     url = dict_url[data]
     if output is None:
         output = os.path.join(os.path.abspath(os.curdir), data)
     install_data(url, output, keep=True)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
