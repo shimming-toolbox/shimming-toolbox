f7590760397c56691bad832299cc3b076f877c9c
Alexandre D'Astous <po09i@hotmail.com>
2020-08-21 19:28:23 -0400

    Ported spherical_harmonics function from matlab

--- shimmingtoolbox/coils/spherical_harmonics.py
+++ shimmingtoolbox/coils/spherical_harmonics.py
@@ -1,23 +1,149 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*
 
-from scipy.special import sph_harm
 import numpy as np
+import math
+from scipy.special import factorial2
+from scipy.special import lpmv
+from matplotlib.figure import Figure
+import os
 
 
-# def spherical_harmonics(order, X,Y,Z):
+def number_of_coef(orders):
+    out = 0
+    for n in orders:
+        m = 2 * n + 1
+        out += m
 
-phi = np.linspace(0, np.pi, 100)
-theta = np.linspace(0, 2 * np.pi, 100)
-phi, theta = np.meshgrid(phi, theta)
-
-x = np.sin(phi) * np.cos(theta)
-y = np.sin(phi) * np.sin(theta)
-z = np.cos(phi)
-
-m, n = 1, 1
-
-basis = sph_harm(m, n, theta, phi)
+    return out
 
 
+# Removed as it seems to be the same as scipy's lpmv function
+# def leg_rec(n, m, u):
+#     # compute legendre polynomial values for dat using recursive relations
+#
+#     if m > 0:
+#         p_mm = (-1) ** m * factorial2(2 * m - 1) * (1 - u ** 2) ** (m / 2)
+#     else:
+#         p_mm = 1
+#
+#     if n == m:
+#         out = p_mm
+#     else:
+#         p_mm1 = (2 * m + 1) * u * p_mm
+#
+#         if n == m + 1:
+#             out = p_mm1
+#         else:
+#             # recursive calculation needed
+#             a = m + 2
+#             p_ma_2 = p_mm
+#             p_ma_1 = p_mm1
+#
+#             while True:
+#                 p_ma = ((2 * a - 1) * u * p_ma_1 - (a + m - 1) * p_ma_2) / (a - m)
+#
+#                 if a == n:
+#                     break
+#
+#                 # prepare next iteration
+#                 p_ma_2 = p_ma_1
+#                 p_ma_1 = p_ma
+#                 a = a + 1
+#
+#             out = p_ma
+#
+#     return out
 
+
+def leg_rec_harmonic_cz(n, m, pos_x, pos_y, pos_z):
+    # returns harmonic field for the required solid harmonic addressed by n, m based on the  iterative Legendre
+    # polynomial calculation. Positive m values correspond to the cosine component, negative to the sine. Returned
+    # fields will eventually follow RRI 's convention pos_...can be both value and vector / matrix
+
+    r2 = pos_x ** 2 + pos_y ** 2 + pos_z ** 2
+    r = np.sqrt(r2)
+    phi = np.arctan2(pos_y, pos_x)
+    cos_theta = np.cos(np.arctan2(np.sqrt(pos_x ** 2 + pos_y ** 2), pos_z))
+    # cos_theta = pos_z / r
+
+    if m >= 0:
+        c = 1
+    else:
+        c = 0
+        m = -m
+
+    # y_mn = leg_rec(n, m, cos_theta)  # Does the same as lpmv function (Will remove after more tests)
+    y_mn = lpmv(m, n, cos_theta)
+
+    rri_norm = math.factorial(n + m + 1) / math.factorial(n - m) / factorial2(2 * m)
+
+    out = (n + m + 1) * (r ** n) * (np.cos(m * phi) * c + np.sin(m * phi) * (1 - c)) * y_mn / rri_norm
+
+    return out
+
+
+def spherical_harmonics(orders, x, y, z):
+
+    # Check inputs
+    if not (x.ndim == y.ndim == z.ndim):
+        raise RuntimeError('Input arrays X, Y, and Z must be identically sized')
+
+    if x.ndim == 2:
+        grid_size = [x.shape, 1]
+    elif x.ndim == 3:
+        grid_size = x.shape
+    else:
+        raise RuntimeError('Input arrays X, Y, and Z must have 2 or 3 dimensions')
+
+    # Initialize variables
+    n_voxels = x.size
+    n_orders = orders.size
+    n_basis = number_of_coef(orders)
+    harm_all = np.zeros([n_voxels, n_basis])
+
+    ii = 0
+
+    # Compute basis
+    for iOrder in range(0, n_orders):
+
+        n = orders[iOrder]
+        if n == 0:
+            m = np.zeros([1], dtype=int)
+        else:
+            m = np.array(range(-orders[iOrder], orders[iOrder] + 1, 1))
+
+        for mm in range(0, m.size):
+
+            # The first 2 * n(1) + 1 columns of the output correspond to harmonics of order n(1), and the next
+            # 2 * n(2) + 1 columns correspond to harmonics of order n(2), etc.
+            harm_all[:, ii] = leg_rec_harmonic_cz(n, m[mm], np.reshape(x, [n_voxels]), np.reshape(y, [n_voxels]),
+                                                  np.reshape(z, [n_voxels]))
+
+            ii += 1
+
+    # Reshape to initial grid_size
+    basis = np.zeros([grid_size[0], grid_size[1], grid_size[2], n_basis])
+
+    for i_basis in range(0, n_basis):
+        basis[:, :, :, i_basis] = np.reshape(harm_all[:, i_basis], grid_size)
+
+    return basis
+
+
+[x, y, z] = np.meshgrid(np.array(range(-10, 11)), np.array(range(-10, 11)), np.array(range(-10, 11)),
+                        indexing='ij')
+orders = np.array(range(0, 3))
+basis = spherical_harmonics(orders, x, y, z)
+
+# Plot results
+fig = Figure(figsize=(10, 10))
+
+for iBasis in range(0, number_of_coef(orders)):
+    ax = fig.add_subplot(3, 3, iBasis + 1)
+    im = ax.imshow(basis[:, :, 11, iBasis])
+    fig.colorbar(im)
+    ax.set_title(f'basis {iBasis + 1}')
+
+fname_figure = os.path.join(os.path.curdir, 'basis_plot.png')
+fig.savefig(fname_figure)
