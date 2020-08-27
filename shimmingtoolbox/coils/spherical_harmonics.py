0bc1b4dea9d26e7b10291936734540cca77c1898
Alexandre D'Astous <po09i@hotmail.com>
2020-08-27 15:14:37 -0400

    Switched functions to local functions and fixed docstring

--- shimmingtoolbox/coils/spherical_harmonics.py
+++ shimmingtoolbox/coils/spherical_harmonics.py
@@ -5,51 +5,10 @@ import numpy as np
 import math
 from scipy.special import factorial2
 from scipy.special import lpmv
-from matplotlib.figure import Figure
-import os
-
-
-def number_of_coef(orders):
-    out = 0
-    for n in orders:
-        m = 2 * n + 1
-        out += m
-
-    return out
-
-
-def leg_rec_harmonic_cz(n, m, pos_x, pos_y, pos_z):
-    """
-    Returns harmonic field for the required solid harmonic addressed by n, m based on the Legendre
-    polynomial calculation. Positive m values correspond to the cosine component, negative to the sine. Returned
-    fields will eventually follow RRI 's convention pos_... can be both value and vector/matrix
-    """
-
-    r2 = pos_x ** 2 + pos_y ** 2 + pos_z ** 2
-    r = np.sqrt(r2)
-    phi = np.arctan2(pos_y, pos_x)
-    cos_theta = np.cos(np.arctan2(np.sqrt(pos_x ** 2 + pos_y ** 2), pos_z))
-    # cos_theta = pos_z / r
-
-    if m >= 0:
-        c = 1
-    else:
-        c = 0
-        m = -m
-
-    # y_mn = leg_rec(n, m, cos_theta)  # Does the same as lpmv function (Will remove after more tests)
-    y_mn = lpmv(m, n, cos_theta)
-
-    rri_norm = math.factorial(n + m + 1) / math.factorial(n - m) / factorial2(2 * m)
-
-    out = (n + m + 1) * (r ** n) * (np.cos(m * phi) * c + np.sin(m * phi) * (1 - c)) * y_mn / rri_norm
-
-    return out
 
 
 def spherical_harmonics(orders, x, y, z):
-    """Return orthonormal spherical harmonic basis set
-
+    """
     Returns an array of spherical harmonic basis fields with the order/degree index along the 4th dimension.
 
     Args:
@@ -65,30 +24,71 @@ def spherical_harmonics(orders, x, y, z):
 
     Examples:
         Initialize grid positions
-        >>> [x, y, z] = np.meshgrid(np.array(range(-10, 11)), np.array(range(-10, 11)), np.array(range(-10, 11)),
-                        indexing='ij')
+
+        >>> [x, y, z] = np.meshgrid(np.array(range(-10, 11)), np.array(range(-10, 11)), np.array(range(-10, 11)), indexing='ij')
+
         0th-to-2nd order terms inclusive
+
         >>> orders = np.array(range(0, 3))
         >>> basis = spherical_harmonics(orders, x, y, z)
 
-    Note:
-        - basis[:,:,:,1] corresponds to the 0th-order constant term (globally=unity)
+    Notes:
+        - basis[:, :, :,0] corresponds to the 0th-order constant term (globally=unity)
+            - 0: *c*
 
-        - basis[:,:,:,2:4] to 1st-order linear terms
-            - 2: *y*
-            - 3: *z*
-            - 4: *x*
-        - basis[:,:,:,5:9] to 2nd-order terms
-            - 5: *xy*
-            - 6: *zy*
-            - 7: *z2*
-            - 8: *zx*
-            - 9: *x2y2*
+        - basis[:, :, :, 1:3] to 1st-order linear terms
+            - 1: *y*
+            - 2: *z*
+            - 3: *x*
+
+        - basis[:, :, :, 4:8] to 2nd-order terms
+            - 4: *xy*
+            - 5: *zy*
+            - 6: *z2*
+            - 7: *zx*
+            - 8: *x2y2*
 
         Based on
             - spherical_harmonics.m by topfer@ualberta.ca
             - calc_spherical_harmonics_arb_points_cz.m by jaystock@nmr.mgh.harvard.edu
     """
+
+    def number_of_coef(orders):
+        out = 0
+        for n in orders:
+            m = 2 * n + 1
+            out += m
+
+        return out
+
+    def leg_rec_harmonic_cz(n, m, pos_x, pos_y, pos_z):
+        """
+        Returns harmonic field for the required solid harmonic addressed by n, m based on the Legendre
+        polynomial calculation. Positive m values correspond to the cosine component, negative to the sine. Returned
+        fields will eventually follow RRI s convention
+        """
+
+        r2 = pos_x ** 2 + pos_y ** 2 + pos_z ** 2
+        r = np.sqrt(r2)
+        phi = np.arctan2(pos_y, pos_x)
+        cos_theta = np.cos(np.arctan2(np.sqrt(pos_x ** 2 + pos_y ** 2), pos_z))
+        # cos_theta = pos_z / r
+
+        if m >= 0:
+            c = 1
+        else:
+            c = 0
+            m = -m
+
+        # y_mn = leg_rec(n, m, cos_theta)  # Does the same as lpmv function (Will remove after more tests)
+        y_mn = lpmv(m, n, cos_theta)
+
+        rri_norm = math.factorial(n + m + 1) / math.factorial(n - m) / factorial2(2 * m)
+
+        out = (n + m + 1) * (r ** n) * (np.cos(m * phi) * c + np.sin(m * phi) * (1 - c)) * y_mn / rri_norm
+
+        return out
+
     # Check inputs
     if not (x.shape == y.shape == z.shape):
         raise RuntimeError('Input arrays X, Y, and Z must be identically sized')
