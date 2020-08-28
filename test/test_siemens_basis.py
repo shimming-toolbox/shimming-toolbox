a2f73677124d680511dc0a086dd83d8d668d61cb
Alexandre D'Astous <po09i@hotmail.com>
2020-08-27 23:34:03 -0400

    Added comment to be more explicit within test_siemens_basis

--- test/test_siemens_basis.py
+++ test/test_siemens_basis.py
@@ -19,5 +19,8 @@ def test_normal_siemens_basis(x, y, z):
 
     # Test for shape
     assert(np.all(basis.shape == (x.shape[0], x.shape[1], x.shape[2], 8)))
-    # Test for first value
+
+    # Test for a value, arbitrarily chose basis[0, 0, 0, 0].
+    # The full matrix could be checked to be more thorough but would require explicitly defining the matrix which is
+    # 2x2x2x8. -4.25760000e-02 was worked out to be the value that should be in basis[0, 0, 0, 0].
     assert(math.isclose(basis[0, 0, 0, 0], -4.25760000e-02, rel_tol=1e-09))
