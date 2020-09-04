874adfdae14259f1f77abfb420e74791153e0f9d
gab-berest <gabriel.berestovoy@gmail.com>
2020-09-04 13:34:40 -0400

    Update code with checks (commented code for python package use since dcm2bids.scaffold is broken as import)

--- shimmingtoolbox/dicom_to_nifti.py
+++ shimmingtoolbox/dicom_to_nifti.py
@@ -7,7 +7,10 @@ from distutils.dir_util import copy_tree
 import json
 import numpy as np
 import os
+import sys
 import subprocess
+import dcm2bids
+from dcm2bids.scaffold import scaffold
 
 from shimmingtoolbox import __dir_config_dcm2bids__
 
@@ -30,9 +33,11 @@ def dicom_to_nifti(path_dicom, path_nifti, subject_id='sub-01', path_config_dcm2
         raise FileNotFoundError("No dcm2bids config file found")
     if not os.path.exists(path_nifti):
         os.makedirs(path_nifti)
+
+    # dcm2bids is broken for windows as a python package so using CLI
     # Create bids structure for data
-    # TODO; use dcm2bids as python package (no system call)
     subprocess.run(['dcm2bids_scaffold', '-o', path_nifti], check=True)
+
     #
     # # Copy original dicom files into nifti_path/sourcedata
     copy_tree(path_dicom, os.path.join(path_nifti, 'sourcedata'))
@@ -50,39 +55,44 @@ def dicom_to_nifti(path_dicom, path_nifti, subject_id='sub-01', path_config_dcm2
     if not helper_file_list:
         raise ValueError('No data to process')
 
-    # # Create list of acquisitions
-    # acquisition_names = []
-    # acquisition_numbers = []
-    # modality = []
+    subprocess.run(['dcm2bids', '-d', path_dicom, '-o', path_nifti, '-p', subject_id, '-c', path_config_dcm2bids],
+                   check=True)
+
+    # if 'win' in sys.platform:
+    #     # dcm2bids is broken for windows as a python package so using CLI
+    #     # Create bids structure for data
+    #     subprocess.run(['dcm2bids_scaffold', '-o', path_nifti], check=True)
     #
-    # # Create lists containing all acquisition names and numbers
-    # for file in [file for file in helper_file_list if file.endswith(".json")]:
-    #     name, ext = os.path.splitext(file)
-    #     # Check for both.gz and .nii
-    #     niftis = [name + ext for ext in [".nii", ".nii.gz"] if (name + ext) in helper_file_list]
-    #     nifti = str(niftis[0])
+    #     #
+    #     # # Copy original dicom files into nifti_path/sourcedata
+    #     copy_tree(path_dicom, os.path.join(path_nifti, 'sourcedata'))
+    #     #
+    #     # # Call the dcm2bids_helper
+    #     subprocess.run(['dcm2bids_helper', '-d', path_dicom, '-o', path_nifti], check=True)
+    #     #
+    #     # Check if the helper folder has been created
+    #     helper_path = os.path.join(path_nifti, 'tmp_dcm2bids', 'helper')
+    #     if not os.path.isdir(helper_path):
+    #         raise ValueError('dcm2bids_helper could not create directory helper')
     #
-    #     # Read json file
-    #     _, json_data = read_nii(os.path.join(helper_path, nifti))
+    #     # Make sure there is data in nifti_path / tmp_dcm2bids / helper
+    #     helper_file_list = os.listdir(helper_path)
+    #     if not helper_file_list:
+    #         raise ValueError('No data to process')
     #
-    #     # Create future folder name
-    #     acquisition_numbers.append(json_data['SeriesNumber'])
-    #     acquisition_names.append(json_data['SeriesDescription'])
-    #     # Modality could be used as acquisition name
-    #     modality.append(json_data['Modality'])
+    #     subprocess.run(['dcm2bids', '-d', path_dicom, '-o', path_nifti, '-p', subject_id, '-c', path_config_dcm2bids], check=True)
+    # else:
+    #     bids_info = dcm2bids.Dcm2bids([path_dicom], subject_id, path_config_dcm2bids, path_nifti)
+    #     scaffold()
     #
-    # # Remove duplicates
-    # acquisition_numbers, ia = np.unique(acquisition_numbers, return_index=True)
-    # acquisition_names_short = []
-    # modality_short = []
-    # for iAcq in ia:
-    #     acquisition_names_short.append(acquisition_names[iAcq])
-    #     modality_short.append(modality[iAcq])
+    #     helper_path = os.path.join(path_nifti, 'tmp_dcm2bids', 'helper')
+    #     if not os.path.isdir(helper_path):
+    #         raise ValueError('dcm2bids_helper could not create directory helper')
+    #     helper_file_list = os.listdir(helper_path)
+    #     if not helper_file_list:
+    #         raise ValueError('No data to process')
     #
-    # # Folder where the different nifti acquisitions will be stored
-    # output_dir = os.path.join(path_nifti, 'code')
-    #
-    subprocess.run(['dcm2bids', '-d', path_dicom, '-o', path_nifti, '-p', subject_id, '-c', path_config_dcm2bids], check=True)
+    #     bids_info.run()
 
     if remove_tmp:
         os.removedirs(os.path.join(path_dicom, 'tmp_dcm2bids'))
