4819d4584d756ee6382cf632359e91970aaa6cb6
Julien Cohen-Adad <jcohen@polymtl.ca>
2020-09-10 14:42:22 -0400

    Fixed input arg of np.zeros

--- shimmingtoolbox/optimizer/sequential.py
+++ shimmingtoolbox/optimizer/sequential.py
@@ -18,7 +18,7 @@ def sequential_zslice(unshimmed, coils, full_mask, z_slices):
 
     """
     z_slices.reshape((z_slices.size, 1))
-    currents = np.zeros(z_slices.size, coils.shape[3])
+    currents = np.zeros([z_slices.size, coils.shape[3]])
     optimizer = LeastSquares(coils)
     for i in range(z_slices.size):
         z = z_slices[i]
