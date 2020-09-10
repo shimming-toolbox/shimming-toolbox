f7aa93acf6e163ad7fa561e309e2aae80b3181b8
Julien Cohen-Adad <jcohen@polymtl.ca>
2020-09-10 14:21:03 -0400

    Added docstrings for sequential_zslice()

--- shimmingtoolbox/optimizer/sequential.py
+++ shimmingtoolbox/optimizer/sequential.py
@@ -6,6 +6,17 @@ from shimmingtoolbox.optimizer.least_squares import LeastSquares
 
 
 def sequential_zslice(unshimmed, coils, full_mask, z_slices):
+    """
+    Args:
+        unshimmed (numpy.ndarray): B0 map
+        coils (numpy.ndarray): Coil sensitivity profile as defined in coils.siemens_basis.siemens_basis()
+        full_mask: (numpy.ndarray): Mask used for the optimizer (only consider voxels with non-zero values).
+        z_slices: TODO
+    
+    Returns:
+        numpy.ndarray: Coefficients to enter in the Syngo console (this might change in the future)
+
+    """
     z_slices.reshape((z_slices.size, 1))
     currents = np.zeros(z_slices.size, coils.shape[3])
     optimizer = LeastSquares(coils)
