83ad9fff2fad07534172be2f9eecbb02c28c12ce
Alexandre D'Astous <po09i@hotmail.com>
2020-09-14 14:00:51 -0400

    Improved docstring and removed unecessary print

--- shimmingtoolbox/optimizer/sequential.py
+++ shimmingtoolbox/optimizer/sequential.py
@@ -8,14 +8,17 @@ from shimmingtoolbox.optimizer.basic_opt import BasicOptimizer
 
 def sequential_zslice(unshimmed, coils, full_mask, z_slices):
     """
+    Performs shimming slice by slice using shimmingtoolbox.optimizer.basic_opt.BasicOptimizer
+
     Args:
-        unshimmed (numpy.ndarray): B0 map
+        unshimmed (numpy.ndarray): 3D B0 map
         coils (numpy.ndarray): Coil sensitivity profile as defined in coils.siemens_basis.siemens_basis()
-        full_mask: (numpy.ndarray): Mask used for the optimizer (only consider voxels with non-zero values).
-        z_slices: TODO
+        full_mask (numpy.ndarray): 3D mask used for the optimizer (only consider voxels with non-zero values).
+        z_slices (numpy.ndarray): 1D array containing z slices to shim
 
     Returns:
         numpy.ndarray: Coefficients to enter in the Syngo console (this might change in the future)
+                       (coils.size x z_slices.size)
 
     """
     z_slices.reshape(z_slices.size)
@@ -27,6 +30,3 @@ def sequential_zslice(unshimmed, coils, full_mask, z_slices):
         currents[i, :] = optimizer.optimize(unshimmed, mask, mask_origin=(0, 0, z))
 
     return currents
-
-
-print("test")
