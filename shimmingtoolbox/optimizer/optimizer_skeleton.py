d8cea0eacf1f0ce6575be5fa3d88de7bdc61285f
Alexandre D'Astous <po09i@hotmail.com>
2020-09-15 14:37:29 -0400

    Indent docstring causing errors

--- shimmingtoolbox/optimizer/optimizer_skeleton.py
+++ shimmingtoolbox/optimizer/optimizer_skeleton.py
@@ -6,14 +6,14 @@ import logging
 
 
 class Optimizer(object):
-"""
-Optimizer object that stores coil profiles and optimizes an unshimmed volume given a mask
-"""
+    """
+    Optimizer object that stores coil profiles and optimizes an unshimmed volume given a mask
+    """
     def __init__(self, coil_profiles=None):
-    """
-    Args:
-        coil_profiles (np.ndarray): (X, Y, Z, N) 4d array of N 3d coil profiles
-    """
+        """
+        Args:
+            coil_profiles (np.ndarray): (X, Y, Z, N) 4d array of N 3d coil profiles
+        """
 
         # Logging
         self.logger = logging.getLogger()
@@ -31,26 +31,26 @@ Optimizer object that stores coil profiles and optimizes an unshimmed volume giv
 
     # Load coil profiles and check dimensions
     def load_coil_profiles(self, coil_profiles):
-    """
-    Load new coil profiles into Optimizer
+        """
+        Load new coil profiles into Optimizer
 
-    Args:
-        coil_profiles (np.ndarray): (X, Y, Z, N) 4d array of N 3d coil profiles
-    """
+        Args:
+            coil_profiles (np.ndarray): (X, Y, Z, N) 4d array of N 3d coil profiles
+        """
         self._error_if(coil_profiles.ndim != 4,
                        f"Coil profile has {coil_profiles.ndim} dimensions, expected 4 (X, Y, Z, N)")
         self.X, self.Y, self.Z, self.N = coil_profiles.shape
         self.coils = coil_profiles
 
     def optimize(self, unshimmed, mask, mask_origin=(0, 0, 0)):
-    """
-    Optimize unshimmed volume by varying current to each channel
+        """
+        Optimize unshimmed volume by varying current to each channel
 
-    Args:
-        unshimmed (np.ndarray): (X, Y, Z) 3d array of unshimmed volume
-        mask (np.ndarray of ints): (X, Y, Z) 3d array of integers marking volume for optimization -- 0 indicates unused
-        mask_origin (3-tuple): Origin of mask if mask volume does not cover unshimmed volume
-    """
+        Args:
+            unshimmed (np.ndarray): (X, Y, Z) 3d array of unshimmed volume
+            mask (np.ndarray of ints): (X, Y, Z) 3d array of integers marking volume for optimization -- 0 indicates unused
+            mask_origin (3-tuple): Origin of mask if mask volume does not cover unshimmed volume
+        """
 
         # Check for sizing errors
         self._error_if(self.coils is None, "No loaded coil profiles!")
@@ -70,12 +70,12 @@ Optimizer object that stores coil profiles and optimizes an unshimmed volume giv
 
     # TODO: refactor to raise errors instead of assert
     def _error_if(self, err_condition, message):
-    """
-    Helper function throwing errors
+        """
+        Helper function throwing errors
 
-    Args:
-        err_condition (bool): Condition to throw error on
-        message (string): Message to log and throw
-    """
+        Args:
+            err_condition (bool): Condition to throw error on
+            message (string): Message to log and throw
+        """
         if err_condition: self.logger.error(message)
         assert not err_condition, message
