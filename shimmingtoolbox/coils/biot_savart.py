24b6352b515d6ac306c2d21b8aecbe0680dec00b
Alexandre D'Astous <po09i@hotmail.com>
2020-09-21 17:22:17 -0400

    Small fixes to style

--- shimmingtoolbox/coils/biot_savart.py
+++ shimmingtoolbox/coils/biot_savart.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-# Code ported and refactored from Jason Stockmann and Fa-Hsuan Lin, "Magnetic field by Biot-Savart's Law" http://maki.bme.ntu.edu.tw/?page_id=333
+# Code ported and refactored from Jason Stockmann and Fa-Hsuan Lin, "Magnetic field by Biot-Savart's Law"
+# http://maki.bme.ntu.edu.tw/?page_id=333
 
 import numpy as np
 
-mu0 = 1.256637e-6 # [H/m]
-H_gyromagnetic_ratio = 4.258e+7 # [Hz/T]
+MU0 = 1.256637e-6  # [H/m]
+H_GYROMAGNETIC_RATIO = 4.258e+7  # [Hz/T]
+
 
 def biot_savart(centers, normals, radii, segment_numbers, fov_min, fov_max, fov_n):
-    """Creates coil profiles for arbitrary loops, for use in multichannel shim examples that do not match spherical harmonics
+    """
+    Creates coil profiles for arbitrary loops, for use in multichannel shim examples that do not match spherical
+    harmonics
     Args:
         centers (list of (3,) array-like floats): List of center points for each loop in mm
         normals (list of (3,) array-like 3D floats): List of 3D normal vectors for each loop in mm
@@ -26,7 +30,6 @@ def biot_savart(centers, normals, radii, segment_numbers, fov_min, fov_max, fov_
     for i in range(3):
         ranges.append(np.linspace(fov_min[i], fov_max[i], num=fov_n[i]))
     x, y, z = np.meshgrid(ranges[0], ranges[1], ranges[2], indexing='ij', sparse=True)
-    
 
     channels = len(centers)
     profiles = np.zeros((x.size, y.size, z.size, channels))
@@ -50,6 +53,7 @@ def biot_savart(centers, normals, radii, segment_numbers, fov_min, fov_max, fov_
 
     return profiles
 
+
 def _loop_segments(center, normal, radius, segment_num):
     """Creates loop segments for loop approximation, given loop details
     Args:
@@ -64,8 +68,8 @@ def _loop_segments(center, normal, radius, segment_num):
     segments = unit_circle = np.zeros((2, 3, segment_num))
 
     theta = np.linspace(0, 2 * np.pi, num=segment_num+1, endpoint=True).reshape((1, segment_num+1))
-    unit_circle[0, :-1, :] = np.concatenate((np.cos(theta[:, :-1]), np.sin(theta[:, :-1])), axis=0) # Start points
-    unit_circle[1, :-1, :] = np.concatenate((np.cos(theta[:, 1:]), np.sin(theta[:, 1:])), axis=0) # End points
+    unit_circle[0, :-1, :] = np.concatenate((np.cos(theta[:, :-1]), np.sin(theta[:, :-1])), axis=0)  # Start points
+    unit_circle[1, :-1, :] = np.concatenate((np.cos(theta[:, 1:]), np.sin(theta[:, 1:])), axis=0)  # End points
 
     segments[:, :, :] = np.round(unit_circle * radius, decimals=9)
     segments = _rotate_z_to(normal) @ segments
@@ -84,19 +88,20 @@ def _rotate_z_to(target):
     rhat = np.cross((0, 0, 1), vhat)
 
     r_cross_mat = np.array([[0, -rhat[2], rhat[1]],
-                     [rhat[2], 0, -rhat[0]],
-                     [-rhat[1], rhat[0], 0]])
+                            [rhat[2], 0, -rhat[0]],
+                            [-rhat[1], rhat[0], 0]])
 
-    cs = np.dot((0, 0, 1), vhat) # cosine
-    sn = np.linalg.norm(rhat) # sine
+    cs = np.dot((0, 0, 1), vhat)  # cosine
+    sn = np.linalg.norm(rhat)  # sine
 
-    if sn == 0: # Target is parallel to z-axis
+    if sn == 0:  # Target is parallel to z-axis
         return np.array([[1, 0, 0],
                         [0, cs, 0],
-                        [0, 0, cs]]) # Return rotation around x (180 or 0 degree)
+                        [0, 0, cs]])  # Return rotation around x (180 or 0 degree)
 
     else:
         return np.identity(3) + r_cross_mat + r_cross_mat @ r_cross_mat * (1 - cs)/sn**2
+
 
 def _z_field(l, dl, r):
     """Calculate z-field at point r from line segment centered at l with length dl
@@ -108,10 +113,10 @@ def _z_field(l, dl, r):
     Returns:
         float: z-component of magnetic field at r in T/A
     """
-    l, dl, r = l / 1000, dl / 1000, r / 1000 # Convert mm to m
+    l, dl, r = l / 1000, dl / 1000, r / 1000  # Convert mm to m
     rp = r - l
     rp_norm = np.linalg.norm(rp)
     if rp_norm == 0:
         return np.nan
-    B_per_I = mu0 / (4 * np.pi) * np.cross(dl, rp) / rp_norm ** 3
-    return B_per_I[2] # [T/A]
+    b_per_i = MU0 / (4 * np.pi) * np.cross(dl, rp) / rp_norm ** 3
+    return b_per_i[2]  # [T/A]
