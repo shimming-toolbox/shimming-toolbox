50cffd01e2ab060fa5a2c69c16e9662027f66dc7
Alexandre D'Astous <po09i@hotmail.com>
2020-10-02 17:27:30 -0400

    Added the CLI

---
+++ shimmingtoolbox/cli/dicom_to_nifti.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import click
+import os
+
+from shimmingtoolbox.dicom_to_nifti import dicom_to_nifti
+from shimmingtoolbox import __dir_config_dcm2bids__
+
+CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+
+
+@click.command(
+    context_settings=CONTEXT_SETTINGS,
+)
+@click.option('-i', '--input', 'path_dicoms', type=click.Path(), required=True, help="Input path of dicom folder")
+@click.option('-o', '--output', 'path_nifti', type=click.Path(), default=os.curdir, help="Output path for niftis")
+@click.option('-s', '--subject', required=True, help="Name of the patient")
+@click.option('-c', '--config', 'fname_config', type=click.Path(), default=__dir_config_dcm2bids__,
+              help="Full file path and name of the bids config file")
+@click.option('--remove_tmp/--dont_remove_tmp', default=False,
+              help="Specifies if tmp folder will be deleted after processing")
+def dicom_to_nifti(path_dicoms, path_nifti, subject, fname_config, remove_tmp):
+    """Converts dicom files into nifti files by calling dcm2bids."""
+
+    dicom_to_nifti(path_dicoms, path_nifti, subject_id=subject, path_config_dcm2bids=fname_config,
+                   remove_tmp=remove_tmp)
