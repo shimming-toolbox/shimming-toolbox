854a057aaebdf81de78bb4589990fcb73b71b5c4
Alexandre D'Astous <po09i@hotmail.com>
2020-10-05 12:45:48 -0400

    Added a_gre back to allow unsorted dicoms to be recognised as fmap

--- test/test_dicom_to_nifti.py
+++ test/test_dicom_to_nifti.py
@@ -6,6 +6,7 @@ import tempfile
 
 from shimmingtoolbox.dicom_to_nifti import dicom_to_nifti
 from shimmingtoolbox import __dir_testing__
+from shimmingtoolbox import __dir_shimmingtoolbox__
 
 
 def test_dicom_to_nifti():
@@ -13,9 +14,36 @@ def test_dicom_to_nifti():
     with tempfile.TemporaryDirectory(prefix='st_'+pathlib.Path(__file__).stem) as tmp:
         path_nifti = os.path.join(tmp, 'nifti')
         subject_id = 'sub-test'
-        dicom_to_nifti(__dir_testing__, path_nifti, subject_id=subject_id)
+        dicom_to_nifti(os.path.join(__dir_shimmingtoolbox__, __dir_testing__, 'dicom_unsorted'), path_nifti, subject_id=subject_id)
         # Check that all the files (.nii.gz and .json) are created with the expected names. The test data has 6
         # magnitude and phase data.
+        for i in range(1, 7):
+            for modality in ['phase', 'magnitude']:
+                for ext in ['nii.gz', 'json']:
+                    assert os.path.exists(os.path.join(path_nifti, subject_id, 'fmap', subject_id + '_{}{}.{}'.format(
+                        modality, i, ext)))
+
+
+def test_dicom_to_nifti_realtime_zshim():
+    # TODO: put as decorator
+    with tempfile.TemporaryDirectory(prefix='st_'+pathlib.Path(__file__).stem) as tmp:
+        path_nifti = os.path.join(tmp, 'nifti')
+        subject_id = 'sub-test'
+        dicom_to_nifti(os.path.join(__dir_shimmingtoolbox__, __dir_testing__, 'realtime_zshimming_data'), path_nifti,
+                       subject_id=subject_id)
+        # Check that all the files (.nii.gz and .json) are created with the expected names. The test data has 6
+        # magnitude and phase data.
+
+        for sequence_type in ['anat', 'fmap', 'func']:
+            if sequence_type == 'fmap':
+                pass
+            if sequence_type == 'anat':
+                pass
+            else:  # sequence_type == func
+                pass
+
+        assert True
+
         for i in range(1, 7):
             for modality in ['phase', 'magnitude']:
                 for ext in ['nii.gz', 'json']:
