edec83c42ecf82318aca9b7fc4854f936f10925a
Alexandre D'Astous <po09i@hotmail.com>
2020-10-06 11:17:44 -0400

    Specified dicom_unsorted for test

--- test/test_dicom_to_nifti.py
+++ test/test_dicom_to_nifti.py
@@ -28,7 +28,7 @@ def test_dicom_to_nifti_remove_tmp():
     with tempfile.TemporaryDirectory(prefix='st_'+pathlib.Path(__file__).stem) as tmp:
         path_nifti = os.path.join(tmp, 'nifti')
         subject_id = 'sub-test'
-        dicom_to_nifti(__dir_testing__, path_nifti, subject_id=subject_id, remove_tmp=True)
+        dicom_to_nifti(os.path.join(__dir_testing__, 'dicom_unsorted'), path_nifti, subject_id=subject_id, remove_tmp=True)
         # Check that all the files (.nii.gz and .json) are created with the expected names. The test data has 6
         # magnitude and phase data.
         assert os.path.exists(path_nifti)
