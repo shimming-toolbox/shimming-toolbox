aee284825301f97f0b26c47831bd49979f3a935a
Alex Dastous <47249340+po09i@users.noreply.github.com>
2020-10-08 19:51:27 -0400

    Update dicom_to_nifti with a CLI (#120)
    * Initial dicom_to_nifti CLI
    
    * Added the CLI
    
    * Remove defacing for ant and added bold sequence to config file
    
    * Fix bug where the cli and function called had the same name
    
    * Added a_gre back to allow unsorted dicoms to be recognised as fmap
    
    * Added test for zshimming data
    
    * Added brief description
    
    * __dir_testing__ now includes shimming-toolbox path
    
    * Smaller line for PEP8
    
    * Remove TODO, added to issue #133
    
    * Added dicom_to_nifti CLI test
    
    * Switched to -XXXX CLI convention
    
    * Added cli as a prefix instead of as a suffix
    
    * Fixed options in test

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ setup(
             "st_referencemaps=shimmingtoolbox.cli.referencemaps:main",
             "st_b0maps=shimmingtoolbox.cli.b0map:main",
             "st_download_data=shimmingtoolbox.cli.download_data:download_data",
+            "st_dicom_to_nifti=shimmingtoolbox.cli.dicom_to_nifti:dicom_to_nifti_cli",
         ]
     },
     packages=find_packages(exclude=["contrib", "docs", "tests"]),
