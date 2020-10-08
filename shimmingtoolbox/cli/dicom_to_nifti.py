65565afe902996417b5122eae43425a8e59d76e3
Alexandre D'Astous <po09i@hotmail.com>
2020-10-08 13:25:43 -0400

    Switched to -XXXX CLI convention

--- shimmingtoolbox/cli/dicom_to_nifti.py
+++ shimmingtoolbox/cli/dicom_to_nifti.py
@@ -13,12 +13,12 @@ CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 @click.command(
     context_settings=CONTEXT_SETTINGS,
 )
-@click.option('-i', '--input', 'path_dicoms', type=click.Path(), required=True, help="Input path of dicom folder")
-@click.option('-o', '--output', 'path_nifti', type=click.Path(), default=os.curdir, help="Output path for niftis")
-@click.option('-s', '--subject', required=True, help="Name of the patient")
-@click.option('-c', '--config', 'fname_config', type=click.Path(), default=__dir_config_dcm2bids__,
+@click.option('-input', 'path_dicoms', type=click.Path(), required=True, help="Input path of dicom folder")
+@click.option('-output', 'path_nifti', type=click.Path(), default=os.curdir, help="Output path for niftis")
+@click.option('-subject', required=True, help="Name of the patient")
+@click.option('-config', 'fname_config', type=click.Path(), default=__dir_config_dcm2bids__,
               help="Full file path and name of the bids config file")
-@click.option('--remove_tmp/--dont_remove_tmp', default=False,
+@click.option('-remove_tmp/-dont_remove_tmp', default=False,
               help="Specifies if tmp folder will be deleted after processing")
 def dicom_to_nifti_cli(path_dicoms, path_nifti, subject, fname_config, remove_tmp):
     """Converts dicom files into nifti files by calling dcm2bids."""
