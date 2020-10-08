4b8294b7b9b00e43e200d045d0f35698a27c1e61
Alexandre D'Astous <po09i@hotmail.com>
2020-10-08 19:46:09 -0400

    Completed test set up

--- shimmingtoolbox/cli/realtime_zshim.py
+++ shimmingtoolbox/cli/realtime_zshim.py
@@ -8,43 +8,50 @@ import nibabel as nib
 from matplotlib.figure import Figure
 
 from shimmingtoolbox.optimizer.sequential import sequential_zslice
-from shimmingtoolbox.coils.siemens_basis import siemens_basis
-from shimmingtoolbox.masking.shapes import shapes
-from shimmingtoolbox import __dir_testing__
 from shimmingtoolbox import __dir_shimmingtoolbox__
 
-
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+
 
 @click.command(
     context_settings=CONTEXT_SETTINGS,
     help=f"Perform realtime z-shimming."
 )
-@click.option("-coil", help="Coil basis to use for shimming. Enter multiple files if you wish to use more than one set "
-                            "of shim coils (eg: Siemens gradient/shim coils and external custom coils).")
-@click.option("-fmap", help="B0 fieldmap. For realtime shimming, this should be a 4d file (4th dimension being time")
-@click.option("-mask", help="3D nifti file with voxels between 0 and 1 used to weight the spatial region to shim.")
+@click.option("-coil", required=True, type=click.Path(), help="Coil basis to use for shimming. Enter multiple files if "
+                                                              "you wish to use more than one set of shim coils (eg: "
+                                                              "Siemens gradient/shim coils and external custom coils).")
+@click.option("-fmap", required=True, type=click.Path(), help="B0 fieldmap. For realtime shimming, this should be a 4d "
+                                                              "file (4th dimension being time")
+@click.option("-mask", type=click.Path(), help="3D nifti file with voxels between 0 and 1 used to weight the spatial "
+                                               "region to shim.")
 @click.option("-verbose", is_flag=True, help="Be more verbose.")
 def realtime_zshim(coil, fmap, mask, verbose):
 
-    nii_coil = nib.load(coil)
+    coil = nib.load(coil).get_fdata()
     nii_fmap = nib.load(fmap)
+    fieldmap = nii_fmap.get_fdata()
+
+    # TODO: Error handling might move to API
+    if fieldmap.ndim != 4:
+        raise RuntimeError('fmap must be 4d (x, y, z, t)')
+
     # TODO: check good practice below
     if mask is not None:
-        nii_mask = nib.load(mask)
+        mask = nib.load(mask).get_fdata()
     else:
-        nii_mask = None
+        mask = np.ones_like(fieldmap)
 
+    nx, ny, nz, nt = fieldmap.shape
 
     currents = np.zeros([8, nt])
-    shimmed = np.zeros_like(fieldmaps)
-    masked_fieldmaps = np.zeros_like(fieldmaps)
+    shimmed = np.zeros_like(fieldmap)
+    masked_fieldmaps = np.zeros_like(fieldmap)
     masked_shimmed = np.zeros_like(shimmed)
     for i_t in range(nt):
-        currents[:, i_t] = sequential_zslice(fieldmaps[:, :, :, i_t], basis, full_mask, z_slices=np.array(range(nz)))
-        shimmed[:, :, :, i_t] = fieldmaps[:, :, :, i_t] + np.sum(currents[:, i_t] * basis, axis=3, keepdims=False)
-        masked_fieldmaps[:, :, :, i_t] = full_mask * fieldmaps[:, :, :, i_t]
-        masked_shimmed[:, :, :, i_t] = full_mask * shimmed[:, :, :, i_t]
+        currents[:, i_t] = sequential_zslice(fieldmap[:, :, :, i_t], coil, mask, z_slices=np.array(range(nz)))
+        shimmed[:, :, :, i_t] = fieldmap[:, :, :, i_t] + np.sum(currents[:, i_t] * coil, axis=3, keepdims=False)
+        masked_fieldmaps[:, :, :, i_t] = mask * fieldmap[:, :, :, i_t]
+        masked_shimmed[:, :, :, i_t] = mask * shimmed[:, :, :, i_t]
 
     i_t = 0
     # Plot results
@@ -59,7 +66,7 @@ def realtime_zshim(coil, fmap, mask, verbose):
     fig.colorbar(im)
     ax.set_title("Masked shimmed")
     ax = fig.add_subplot(2, 2, 3)
-    im = ax.imshow(fieldmaps[:-1, :-1, 0, i_t])
+    im = ax.imshow(fieldmap[:-1, :-1, 0, i_t])
     fig.colorbar(im)
     ax.set_title("Unshimmed")
     ax = fig.add_subplot(2, 2, 4)
@@ -67,12 +74,8 @@ def realtime_zshim(coil, fmap, mask, verbose):
     fig.colorbar(im)
     ax.set_title("Shimmed")
 
-    print(f"\nThe associated current coefficients are : {currents[:, i_t]}")
+    click.echo(f"\nThe associated current coefficients are : {currents[:, i_t]}")
 
     fname_figure = os.path.join(__dir_shimmingtoolbox__, 'realtime_zshim_plot.png')
     fig.savefig(fname_figure)
     return fname_figure
-
-
-if __name__ == '__main__':
-    realtime_zshim()
--- test/cli/test_cli_realtime_zshim.py
+++ test/cli/test_cli_realtime_zshim.py
@@ -4,9 +4,14 @@
 import os
 import pathlib
 import tempfile
+import nibabel as nib
+import numpy as np
 
 from click.testing import CliRunner
 from shimmingtoolbox.cli.realtime_zshim import realtime_zshim
+from shimmingtoolbox.masking.shapes import shapes
+from shimmingtoolbox.coils.coordinates import generate_meshgrid
+from shimmingtoolbox.coils.siemens_basis import siemens_basis
 from shimmingtoolbox import __dir_testing__
 
 
@@ -14,19 +19,28 @@ def test_cli_realtime_zshim():
     with tempfile.TemporaryDirectory(prefix='st_' + pathlib.Path(__file__).stem) as tmp:
         runner = CliRunner()
 
-        fname_fieldmap = os.path.join(__dir_testing__, 'nifti', 'sub-example', 'fmap', 'sub-example_fieldmap.nii.gz')
+        fname_fieldmap = os.path.join(__dir_testing__, 'sub-fieldmap', 'fmap', 'sub-example_fieldmap.nii.gz')
+        nii_fmap = nib.load(fname_fieldmap)
+        fmap = nii_fmap.get_fdata()
+        affine = nii_fmap.affine
 
         # Set up mask
-        # TODO
-        full_mask = shapes(fieldmaps[:, :, :, 0], 'cube', center_dim1=round(nx / 2) - 5, len_dim1=40, len_dim2=40,
-                           len_dim3=nz)
+        nx, ny, nz, nt = fmap.shape
+        mask = shapes(fmap[:, :, :, 0], 'cube', center_dim1=round(nx / 2) - 5, len_dim1=40, len_dim2=40,
+                      len_dim3=nz)
 
-        path_dicoms = os.path.join(__dir_testing__, 'dicom_unsorted')
-        path_nifti = os.path.join(tmp, 'nifti')
-        subject_id = 'sub-test'
+        nii_mask = nib.Nifti1Image(mask.astype(int), affine)
+        fname_mask = os.path.join(tmp, 'mask.nii.gz')
+        nib.save(nii_mask, fname_mask)
 
-        result = runner.invoke(realtime_zshim, ['-i', path_dicoms, '-o', path_nifti, '-s', subject_id])
+        # Set up coils
+        coord_phys = generate_meshgrid(fmap.shape[0:3], affine)
+        coil_profile = siemens_basis(coord_phys[0], coord_phys[1], coord_phys[2])
+
+        nii_coil = nib.Nifti1Image(coil_profile, affine)
+        fname_coil = os.path.join(tmp, 'coil_profile.nii.gz')
+        nib.save(nii_coil, fname_coil)
+
+        result = runner.invoke(realtime_zshim, ['-fmap', fname_fieldmap, '-coil', fname_coil, '-mask', fname_mask])
 
         assert result.exit_code == 0
-        # Check that dicom_to_nifti was invoked, not if files were actually created (API test already looks for that)
-        assert os.path.isdir(path_nifti)
