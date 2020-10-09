Fixed test_biot_savart to match same units

test/test_biot_savart.py
@@ -6,10 +6,12 @@ import pytest
 
 from shimmingtoolbox.coils.biot_savart import biot_savart
 
+H_GYRO_R = 42.577478518e+6 # Hz/T
+
 dummy_data = [
-    ([(0, 0, -0.5)], [(0, 0, 1)], [1, ], [250, ], (0, 0, 0), (0, 0, 1), (1, 1, 3), (0.4495881427866065e-3,
-                                                                                    2.2214414690791835e-4,
-                                                                                    1.0723951147113824e-4)),
+    ([(0, 0, -0.5)], [(0, 0, 1)], [1, ], [250, ], (0, 0, 0), (0, 0, 1), (1, 1, 3), (0.4495881427866065e-3 * H_GYRO_R,
+                                                                                    2.2214414690791835e-4 * H_GYRO_R,
+                                                                                    1.0723951147113824e-4 * H_GYRO_R)),
     ([(-0.5, 0, 0)], [(1, 0, 0)], [1, ], [250, ], (0, 0, 0), (1, 0, 0), (3, 1, 1), (0, 0, 0)),
     ([(0, -0.5, 0)], [(0, 1, 0)], [1, ], [250, ], (0, 0, 0), (0, 1, 0), (1, 3, 1), (0, 0, 0)),
 ]
@@ -23,6 +25,8 @@ def test_normal_use(centers, normals, radii, segment_numbers, fov_min, fov_max, 
     assert(basis[:, :, :, 0].shape == fov_n)
 
     # Check expected values
-    actual = np.round(basis[:, :, :, 0].reshape(len(axis_answers)), decimals=7)
-    expected = np.round(np.asarray(axis_answers), decimals=7)
+    actual = np.round(basis[:, :, :, 0].reshape(len(axis_answers)), decimals=-2)
+    expected = np.round(np.asarray(axis_answers), decimals=-2)
+    print(actual)
+    print(expected)
     assert(np.all(actual == expected))