f80f2b3cf85c86deb6cbaea7c731ef6ffd9d690f
Lincoln Craven-Brightman <68962755+lcbMGH@users.noreply.github.com>
2020-10-09 14:36:08 -0400

    Update shimmingtoolbox/optimizer/lsq_optimizer.py
    Co-authored-by: Alex Dastous <47249340+po09i@users.noreply.github.com>

--- shimmingtoolbox/optimizer/lsq_optimizer.py
+++ shimmingtoolbox/optimizer/lsq_optimizer.py
@@ -23,7 +23,7 @@ class LSQ_Optimizer(Optimizer):
 
         """
         self._error_if(unshimmed_vec.shape[0] != coil_mat.shape[0], 
-                        (f'Unshimmed ({unshimmed_vec.shape}) and coil ({coil_mat.shape})'
+                       (f'Unshimmed ({unshimmed_vec.shape}) and coil ({coil_mat.shape})'
                         ' arrays do not align on axis 0'))
         return unshimmed_vec + np.sum(coil_mat * coef, axis=1, keepdims=False)
 
