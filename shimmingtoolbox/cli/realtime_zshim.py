6855ae4959d90e51f11fbd49c8b199e479d7bd5a
Julien Cohen-Adad <jcohen@polymtl.ca>
2020-10-21 16:57:17 -0400

    Clarified comments, added TODO, minor refactoring
    - Added bounds to sequential_zslice() because of bug: https://github.com/shimming-toolbox/shimming-toolbox/issues/152
    - Commented out Click decorator because not working in Pycharm: https://github.com/shimming-toolbox/shimming-toolbox/issues/156

--- shimmingtoolbox/cli/realtime_zshim.py
+++ shimmingtoolbox/cli/realtime_zshim.py
@@ -5,6 +5,7 @@ import click
 import numpy as np
 import os
 import nibabel as nib
+# TODO: remove matplotlib import
 from matplotlib.figure import Figure
 
 from shimmingtoolbox.optimizer.sequential import sequential_zslice
@@ -13,21 +14,31 @@ from shimmingtoolbox import __dir_shimmingtoolbox__
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
 
-@click.command(
-    context_settings=CONTEXT_SETTINGS,
-    help=f"Perform realtime z-shimming."
-)
-@click.option('-coil', 'fname_coil', required=True, type=click.Path(),
-              help="Coil basis to use for shimming. Enter multiple files if "
-                   "you wish to use more than one set of shim coils (eg: "
-                   "Siemens gradient/shim coils and external custom coils).")
-@click.option('-fmap', 'fname_fmap', required=True, type=click.Path(),
-              help="B0 fieldmap. For realtime shimming, this should be a 4d file (4th dimension being time")
-@click.option('-mask', 'fname_mask', type=click.Path(),
-              help="3D nifti file with voxels between 0 and 1 used to weight the spatial region to shim.")
-@click.option("-verbose", is_flag=True, help="Be more verbose.")
-def realtime_zshim(fname_coil, fname_fmap, fname_mask, verbose):
+# @click.command(
+#     context_settings=CONTEXT_SETTINGS,
+#     help=f"Perform realtime z-shimming."
+# )
+# @click.option('-coil', 'fname_coil', required=True, type=click.Path(),
+#               help="Coil basis to use for shimming. Enter multiple files if "
+#                    "you wish to use more than one set of shim coils (eg: "
+#                    "Siemens gradient/shim coils and external custom coils).")
+# @click.option('-fmap', 'fname_fmap', required=True, type=click.Path(),
+#               help="B0 fieldmap. For realtime shimming, this should be a 4d file (4th dimension being time")
+# @click.option('-mask', 'fname_mask', type=click.Path(),
+#               help="3D nifti file with voxels between 0 and 1 used to weight the spatial region to shim.")
+# @click.option("-verbose", is_flag=True, help="Be more verbose.")
+def realtime_zshim(fname_coil, fname_fmap, fname_mask, verbose=True):
+    """
 
+    Args:
+        fname_coil: Pointing to coil profile. 4-dimensional: x, y, z, coil.
+        fname_fmap:
+        fname_mask:
+        verbose:
+
+    Returns:
+
+    """
     coil = nib.load(fname_coil).get_fdata()
     nii_fmap = nib.load(fname_fmap)
     fieldmap = nii_fmap.get_fdata()
@@ -35,6 +46,7 @@ def realtime_zshim(fname_coil, fname_fmap, fname_mask, verbose):
     # TODO: Error handling might move to API
     if fieldmap.ndim != 4:
         raise RuntimeError('fmap must be 4d (x, y, z, t)')
+    nx, ny, nz, nt = fieldmap.shape
 
     # TODO: check good practice below
     if fname_mask is not None:
@@ -42,17 +54,19 @@ def realtime_zshim(fname_coil, fname_fmap, fname_mask, verbose):
     else:
         mask = np.ones_like(fieldmap)
 
-    nx, ny, nz, nt = fieldmap.shape
+    # Setup coil
+    n_coils = coil.shape[-1]
+    currents = np.zeros([n_coils, nt])
 
-    currents = np.zeros([8, nt])
     shimmed = np.zeros_like(fieldmap)
     masked_fieldmaps = np.zeros_like(fieldmap)
     masked_shimmed = np.zeros_like(shimmed)
     for i_t in range(nt):
-        currents[:, i_t] = sequential_zslice(fieldmap[:, :, :, i_t], coil, mask, z_slices=np.array(range(nz)))
-        shimmed[:, :, :, i_t] = fieldmap[:, :, :, i_t] + np.sum(currents[:, i_t] * coil, axis=3, keepdims=False)
-        masked_fieldmaps[:, :, :, i_t] = mask * fieldmap[:, :, :, i_t]
-        masked_shimmed[:, :, :, i_t] = mask * shimmed[:, :, :, i_t]
+        currents[:, i_t] = sequential_zslice(fieldmap[..., i_t], coil, mask, z_slices=np.array(range(nz)),
+                                             bounds=[(-np.inf, np.inf)]*8)
+        shimmed[..., i_t] = fieldmap[..., i_t] + np.sum(currents[:, i_t] * coil, axis=3, keepdims=False)
+        masked_fieldmaps[..., i_t] = mask * fieldmap[..., i_t]
+        masked_shimmed[..., i_t] = mask * shimmed[..., i_t]
 
     i_t = 0
     # Plot results
@@ -81,6 +95,7 @@ def realtime_zshim(fname_coil, fname_fmap, fname_mask, verbose):
     fig.savefig(fname_figure)
 
     fig = Figure(figsize=(10, 10))
+    # TODO: display time as X -- add label for Y
     for i_t in range(nt):
         ax = fig.add_subplot(nt, 1, i_t + 1)
         ax.plot(np.arange(8), currents[:, i_t])
@@ -89,4 +104,20 @@ def realtime_zshim(fname_coil, fname_fmap, fname_mask, verbose):
     fname_figure = os.path.join(__dir_shimmingtoolbox__, 'realtime_zshim_currents.png')
     fig.savefig(fname_figure)
 
+    # TODO: fetch PMU timing
+    get_acquisition_time()
+
+
+    # TODO:
+    #  fit PMU and fieldmap values
+    #  do regression to separate static componant and RIRO component
+    #  output coefficient with proper scaling
+
     return fname_figure
+
+
+if __name__ == '__main__':
+    fname_coil='/Users/julien/code/shimming-toolbox/shimming-toolbox/test_realtime_zshim/coil_profile.nii.gz'
+    fname_fmap='/Users/julien/code/shimming-toolbox/shimming-toolbox/test_realtime_zshim/sub-example_fieldmap.nii.gz'
+    fname_mask='/Users/julien/code/shimming-toolbox/shimming-toolbox/test_realtime_zshim/mask.nii.gz'
+    realtime_zshim(fname_coil, fname_fmap, fname_mask)
