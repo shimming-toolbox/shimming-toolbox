4c0d76bd4217fab741413ac4f68c988c8a3e343c
Alex Dastous <47249340+po09i@users.noreply.github.com>
2020-10-24 23:08:15 -0400

    Add ability to get timing data from scans (#149)
    * Update URL to testing data
    
    * Started testing matching between MRI and PMU timestamp
    
    * Initial test to read times from dicoms
    
    * Refactored test to use data_testing and add new format to dicom_times_to_ms. Need to update fieldmap and json in data-testing. More details in TODOs.
    
    * Verify times B0 timeseries fits pressure data
    
    * Added non-interpolated pmu on the graph
    
    * Removed todos and updated field for json
    
    * Small refactor
    
    * Added get_acquisition_times function and TODOs
    
    * Added comments
    
    * Moved dicom_times_to_ms() to utils
    
    * Renamed dicom_times_to_ms --> iso_times_to_ms
    
    * Moved get_acquisition_times() to load_nifti.py
    
    * Added comments
    
    * Refactor get_acquisition_times to use nii and json
    
    * Added correlation check
    
    * Clean up
    
    * Clean up imports
    
    * Change variable name to iso
    
    * Fix what is being checked for the correlation: fieldmap average and pmu pressure
    
    * Fix travis error with np.isclose
    
    Co-authored-by: Julien Cohen-Adad <jcohen@polymtl.ca>

--- test/test_pmu.py
+++ test/test_pmu.py
@@ -3,9 +3,14 @@
 
 import os
 import numpy as np
+import nibabel as nib
+import json
+import scipy.signal
 
+from shimmingtoolbox.masking.shapes import shapes
 from shimmingtoolbox import __dir_testing__
 from shimmingtoolbox.pmu import PmuResp
+from shimmingtoolbox.load_nifti import get_acquisition_times
 
 
 def test_read_resp():
@@ -44,3 +49,87 @@ def test_interp_resp_trace():
 
     assert(np.all(np.isclose(acq_pressure[index_pmu_interp], pmu.data[index_pmu_data], atol=1, rtol=0.08)))
 
+
+def test_timing_images():
+    """Check the matching of timing between MR images and PMU timestamps"""
+
+    # Get fieldmap
+    fname_fieldmap = os.path.join(__dir_testing__, 'realtime_zshimming_data', 'nifti', 'sub-example', 'fmap',
+                                  'sub-example_fieldmap.nii.gz')
+    nii_fieldmap = nib.load(fname_fieldmap)
+    fieldmap = nii_fieldmap.get_fdata()
+
+    # Get the pressure values
+    fname_pmu = os.path.join(__dir_testing__, 'realtime_zshimming_data', 'PMUresp_signal.resp')
+    pmu = PmuResp(fname_pmu)
+
+    # Get acquisition timestamps
+    fname_phase_diff_json = os.path.join(__dir_testing__, 'realtime_zshimming_data', 'nifti', 'sub-example', 'fmap',
+                                         'sub-example_phasediff.json')
+    with open(fname_phase_diff_json) as json_file:
+        json_data = json.load(json_file)
+    fieldmap_timestamps = get_acquisition_times(nii_fieldmap, json_data)
+
+    # Interpolate PMU values onto MRI acquisition timestamp
+    acquisition_pressures = pmu.interp_resp_trace(fieldmap_timestamps)
+
+    # Set up mask
+    mask_len1 = 15
+    mask_len2 = 5
+    mask_len3 = fieldmap.shape[2]
+    mask = shapes(fieldmap[:, :, :, 0], shape='cube',
+                  center_dim1=int(fieldmap.shape[0] / 2 - 8),
+                  center_dim2=int(fieldmap.shape[1] / 2 - 20),
+                  len_dim1=mask_len1, len_dim2=mask_len2, len_dim3=mask_len3)
+
+    # Apply mask and compute the average for each timepoint
+    fieldmap_masked = np.zeros_like(fieldmap)
+    fieldmap_avg = np.zeros([fieldmap.shape[3]])
+    for i_time in range(fieldmap.shape[3]):
+        fieldmap_masked[:, :, :, i_time] = fieldmap[:, :, :, i_time] * mask
+        masked_array = np.ma.array(fieldmap[:, :, :, i_time], mask=mask == False)
+        fieldmap_avg[i_time] = np.ma.average(masked_array)
+
+    # Reshape pmu datapoints to fit those of the acquisition
+    pmu_times = np.linspace(pmu.start_time_mdh, pmu.stop_time_mdh, len(pmu.data))
+    pmu_times_within_range = pmu_times[pmu_times > fieldmap_timestamps[0]]
+    pmu_data_within_range = pmu.data[pmu_times > fieldmap_timestamps[0]]
+    pmu_data_within_range = pmu_data_within_range[pmu_times_within_range < fieldmap_timestamps[fieldmap.shape[3] - 1]]
+    pmu_times_within_range = pmu_times_within_range[pmu_times_within_range < fieldmap_timestamps[fieldmap.shape[3] - 1]]
+
+    # Compute correlation
+    pmu_data_within_range_ds = scipy.signal.resample(pmu_data_within_range, fieldmap_avg.shape[0])
+    pearson = np.corrcoef(fieldmap_avg, pmu_data_within_range_ds)
+
+    assert(np.isclose(pearson[0, 1], 0.6031485150782748))
+
+    # # Plot results
+    # fig = Figure(figsize=(10, 10))
+    # ax = fig.add_subplot(211)
+    # ax.plot(fieldmap_timestamps / 1000, acquisition_pressures, label='Interpolated pressures')
+    # # ax.plot(pmu_times, pmu.data, label='Raw pressures')
+    # ax.plot(pmu_times_within_range / 1000, pmu_data_within_range, label='Pmu pressures')
+    # ax.legend()
+    # ax.set_title("Pressure [-2048, 2047] vs time (s) ")
+    # ax = fig.add_subplot(212)
+    # ax.plot(fieldmap_timestamps / 1000, fieldmap_avg, label='Mean B0')
+    # ax.legend()
+    # ax.set_title("Fieldmap average over unmasked region (Hz) vs time (s)")
+    #
+    # fname_figure = os.path.join(__dir_shimmingtoolbox__, 'pmu_plot.png')
+    # fig.savefig(fname_figure)
+    #
+    # # Plot mask
+    # fig = Figure(figsize=(10, 10))
+    # ax = fig.add_subplot(211)
+    # im = ax.imshow(fieldmap_masked[:, :, 0, 0])
+    # fig.colorbar(im)
+    # ax.set_title("Mask (Hz)")
+    #
+    # ax = fig.add_subplot(212)
+    # im = ax.imshow(fieldmap[:, :, 0, 0])
+    # fig.colorbar(im)
+    # ax.set_title("Fieldmap (Hz)")
+    #
+    # fname_figure = os.path.join(__dir_shimmingtoolbox__, 'mask.png')
+    # fig.savefig(fname_figure)
