9dfaed08db3f7a214c3500604760d4f075d59349
Alexandre D'Astous <po09i@hotmail.com>
2020-10-25 23:02:48 -0400

    Add resp path, json path and fix fieldmap path

--- test/cli/test_cli_realtime_zshim.py
+++ test/cli/test_cli_realtime_zshim.py
@@ -19,7 +19,8 @@ def test_cli_realtime_zshim():
     with tempfile.TemporaryDirectory(prefix='st_' + pathlib.Path(__file__).stem) as tmp:
         runner = CliRunner()
 
-        fname_fieldmap = os.path.join(__dir_testing__, 'sub-fieldmap', 'fmap', 'sub-example_fieldmap.nii.gz')
+        fname_fieldmap = os.path.join(__dir_testing__, 'realtime_zshimming_data', 'nifti', 'sub-example', 'fmap',
+                                      'sub-example_fieldmap.nii.gz')
         nii_fmap = nib.load(fname_fieldmap)
         fmap = nii_fmap.get_fdata()
         affine = nii_fmap.affine
@@ -43,7 +44,15 @@ def test_cli_realtime_zshim():
         fname_coil = os.path.join(tmp, 'coil_profile.nii.gz')
         nib.save(nii_coil, fname_coil)
 
-        result = runner.invoke(realtime_zshim, ['-fmap', fname_fieldmap, '-coil', fname_coil, '-mask', fname_mask],
+        # Path for resp data
+        fname_resp = os.path.join(__dir_testing__, 'realtime_zshimming_data', 'PMUresp_signal.resp')
+
+        # Path for json file
+        fname_json = os.path.join(__dir_testing__, 'realtime_zshimming_data', 'nifti', 'sub-example', 'fmap',
+                                  'sub-example_magnitude1.json')
+
+        result = runner.invoke(realtime_zshim, ['-fmap', fname_fieldmap, '-coil', fname_coil, '-mask', fname_mask,
+                                                '-resp', fname_resp, '-json', fname_json],
                                catch_exceptions=False)
 
         assert result.exit_code == 0
