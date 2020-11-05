ff61f5c9e5e6c66cc18213cbe7d90bbc6b0b66ec
maheuss <71348807+maheuss@users.noreply.github.com>
2020-11-05 13:13:32 -0500

    Adaptation of entry points with mask.py

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ setup(
         'console_scripts': [
             "st_download_data=shimmingtoolbox.cli.download_data:download_data",
             "st_dicom_to_nifti=shimmingtoolbox.cli.dicom_to_nifti:dicom_to_nifti_cli",
+            "st_mask=shimmingtoolbox.cli.mask:mask",
         ]
     },
     packages=find_packages(exclude=["docs"]),
