a5a0bbace8cca7abc04e01e1e16ba10d1417e7e2
Alexandre D'Astous <po09i@hotmail.com>
2020-11-21 18:59:04 -0500

    Initial tests

--- test/test_coordinates.py
+++ test/test_coordinates.py
@@ -2,8 +2,14 @@
 # -*- coding: utf-8 -*
 
 import numpy as np
+import math
+import os
+import nibabel as nib
 
 from shimmingtoolbox.coils.coordinates import generate_meshgrid
+from shimmingtoolbox.coils.coordinates import phys_gradient
+from shimmingtoolbox import __dir_shimmingtoolbox__
+from shimmingtoolbox import __dir_testing__
 
 
 def test_generate_meshgrid():
@@ -33,3 +39,50 @@ def test_generate_meshgrid():
                            [-126.9379747, -126.9379747]]])]
 
     assert(np.all(np.isclose(coord, expected)))
+
+
+def test_phys_gradient_synt():
+    """Define a previously calculated matrix (matrix was defined at 45 deg for a gx=-6, gy=2)"""
+    img_array = np.expand_dims(np.array([[6 * math.sqrt(2), 4 * math.sqrt(2), 2 * math.sqrt(2)],
+                                         [2 * math.sqrt(2), 0, -2 * math.sqrt(2)],
+                                         [-2 * math.sqrt(2), -4 * math.sqrt(2), -6 * math.sqrt(2)]]), -1)
+
+    scale = np.array([[1, 0, 0],
+                      [0, 1, 0],
+                      [0, 0, 1]])
+    deg_angle = -45
+    rot = np.array([[math.cos(deg_angle * math.pi / 180), -math.sin(deg_angle * math.pi / 180), 0],
+                    [math.sin(deg_angle * math.pi / 180), math.cos(deg_angle * math.pi / 180), 0],
+                    [0, 0, 1]])
+
+    m_affine = np.dot(scale, rot)
+    static_affine = [0, 0, 0, 1]
+
+    affine = np.zeros([4, 4])
+    affine[:3, :3] = m_affine
+    affine[3, :] = static_affine
+
+    gx, gy, gz = phys_gradient(img_array, affine)  # gx = -6, gy = 2, gz = 0
+
+    assert np.all(np.isclose(gx, -6)) and np.all(np.isclose(gy, 2)) and np.all(np.isclose(gz, 0))
+
+
+def test_phys_gradient_reel():
+    fname_fieldmap = os.path.join(__dir_testing__, 'realtime_zshimming_data', 'nifti', 'sub-example', 'fmap',
+                                  'sub-example_fieldmap.nii.gz')
+
+    nii_fieldmap = nib.load(fname_fieldmap)
+
+    affine = nii_fieldmap.affine
+    fmap = nii_fieldmap.get_fdata()
+
+    g_x, g_y, g_z = phys_gradient(fmap[..., 0], affine)
+
+    # Test against scaled but non rotated fieldmap, should get the same results as phys_gradient
+    x_coord, y_coord, z_coord = generate_meshgrid(fmap[..., 0].shape, affine)
+    gz_truth = np.gradient(fmap[..., 0], z_coord[0, :, 0], axis=1)
+    gy_truth = np.gradient(fmap[..., 0], y_coord[:, 0, 0], axis=0)
+
+    assert np.all(np.isclose(g_z, gz_truth)) and np.all(np.isclose(g_y, gy_truth))
+
+
