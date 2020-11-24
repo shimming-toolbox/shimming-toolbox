74c8fcbe9e6d2e7f9f479c5cc11e99234cd918e7
Alexandre D'Astous <po09i@hotmail.com>
2020-11-23 21:58:23 -0500

    Update tests

--- test/test_coordinates.py
+++ test/test_coordinates.py
@@ -8,7 +8,6 @@ import nibabel as nib
 
 from shimmingtoolbox.coils.coordinates import generate_meshgrid
 from shimmingtoolbox.coils.coordinates import phys_gradient
-from shimmingtoolbox import __dir_shimmingtoolbox__
 from shimmingtoolbox import __dir_testing__
 
 
@@ -46,28 +45,63 @@ def test_phys_gradient_synt():
     img_array = np.expand_dims(np.array([[6 * math.sqrt(2), 4 * math.sqrt(2), 2 * math.sqrt(2)],
                                          [2 * math.sqrt(2), 0, -2 * math.sqrt(2)],
                                          [-2 * math.sqrt(2), -4 * math.sqrt(2), -6 * math.sqrt(2)]]), -1)
-
+    # Define a scale matrix
     scale = np.array([[1, 0, 0],
                       [0, 1, 0],
                       [0, 0, 1]])
+
+    # Define a rotation matrix
     deg_angle = -45
     rot = np.array([[math.cos(deg_angle * math.pi / 180), -math.sin(deg_angle * math.pi / 180), 0],
                     [math.sin(deg_angle * math.pi / 180), math.cos(deg_angle * math.pi / 180), 0],
                     [0, 0, 1]])
 
-    m_affine = np.dot(scale, rot)
+    # Calculate affine matrix
+    m_affine = np.dot(rot, scale)
     static_affine = [0, 0, 0, 1]
-
     affine = np.zeros([4, 4])
     affine[:3, :3] = m_affine
     affine[3, :] = static_affine
 
-    gx, gy, gz = phys_gradient(img_array, affine)  # gx = -6, gy = 2, gz = 0
+    g_x, g_y, g_z = phys_gradient(img_array, affine)  # gx = -6, gy = 2, gz = 0
 
-    assert np.all(np.isclose(gx, -6)) and np.all(np.isclose(gy, 2)) and np.all(np.isclose(gz, 0))
+    assert np.all(np.isclose(g_x, -6)) and np.all(np.isclose(g_y, 2)) and np.all(np.isclose(g_z, 0))
+
+
+def test_phys_gradient_synt_scaled():
+    """Define a previously calculated matrix (matrix was defined at 45 deg for a gx=-3, gy=1)"""
+    img_array = np.expand_dims(np.array([[6 * math.sqrt(2), 4 * math.sqrt(2), 2 * math.sqrt(2)],
+                                         [2 * math.sqrt(2), 0, -2 * math.sqrt(2)],
+                                         [-2 * math.sqrt(2), -4 * math.sqrt(2), -6 * math.sqrt(2)]]), -1)
+    # Define a scale matrix
+    scale = np.array([[2, 0, 0],
+                      [0, 2, 0],
+                      [0, 0, 2]])
+
+    # Define a rotation matrix
+    deg_angle = -45
+    rot = np.array([[math.cos(deg_angle * math.pi / 180), -math.sin(deg_angle * math.pi / 180), 0],
+                    [math.sin(deg_angle * math.pi / 180), math.cos(deg_angle * math.pi / 180), 0],
+                    [0, 0, 1]])
+
+    # Calculate affine matrix
+    m_affine = np.dot(rot, scale)
+    static_affine = [0, 0, 0, 1]
+    affine = np.zeros([4, 4])
+    affine[:3, :3] = m_affine
+    affine[3, :] = static_affine
+
+    g_x, g_y, g_z = phys_gradient(img_array, affine)  # gx = -6, gy = 2, gz = 0
+
+    assert np.all(np.isclose(g_x, -3)) and np.all(np.isclose(g_y, 1)) and np.all(np.isclose(g_z, 0))
 
 
 def test_phys_gradient_reel():
+    """
+    Test the function on real data at 0 degrees of rotation so a ground truth can be calculated with a simple
+    gradient calculation since they are parallel. The reel data adds a degree of complexity since it is a sagittal image
+    """
+
     fname_fieldmap = os.path.join(__dir_testing__, 'realtime_zshimming_data', 'nifti', 'sub-example', 'fmap',
                                   'sub-example_fieldmap.nii.gz')
 
@@ -80,9 +114,10 @@ def test_phys_gradient_reel():
 
     # Test against scaled but non rotated fieldmap, should get the same results as phys_gradient
     x_coord, y_coord, z_coord = generate_meshgrid(fmap[..., 0].shape, affine)
-    gz_truth = np.gradient(fmap[..., 0], z_coord[0, :, 0], axis=1)
+    gx_truth = np.zeros_like(fmap[..., 0])
     gy_truth = np.gradient(fmap[..., 0], y_coord[:, 0, 0], axis=0)
+    gz_truth = np.gradient(fmap[..., 0], z_coord[0, :, 0], axis=1)
 
-    assert np.all(np.isclose(g_z, gz_truth)) and np.all(np.isclose(g_y, gy_truth))
+    assert np.all(np.isclose(g_z, gz_truth)) and np.all(np.isclose(g_y, gy_truth)) and np.all(np.isclose(g_x, gx_truth))
 
 
