75b53dfe6b27395c5e68cbddd3ce5c7088c88e28
Alex Dastous <47249340+po09i@users.noreply.github.com>
2020-11-25 14:53:41 -0500

    Add ability to create fieldmaps (#169)
    * Initial set up for CLI and test
    
    * Add st_prepare_fieldmap as an entry_point
    
    * Added comments and started to implement
    
    * Added read_nii and echo_time_diff
    
    * Now checking the input phase is a phase diff
    
    For the "single-echo" case, which corresponds to phase diff input
    
    * Added TODOs, added call to prelude, added unwrapper case
    
    * Fixed wrong input for mag
    
    * Deal with 4d phase data
    
    * Divide by echo_time, output fieldmap, add support for 2d fieldmaps
    
    * start implementing dual echo
    
    * Seperate single and dual echo test
    
    * Update dual echo method
    
    * Add magnitude and remove unecessary debug lines
    
    * Move unwrapper to seperate API
    
    * Add unwrap_phase
    
    * Add tests for unwrap_phase and update unwrap_phase to correct found errors
    
    * Cleanup
    
    * Transfer to an API
    
    * Rename dli test to follow convention
    
    * Add tests for prepare_fieldmap API and fix errors found with tests
    
    * Add new tests for error handling
    
    * Add comments
    
    * Updated help
    
    * Added TODO
    
    * Minor comments
    
    * Added integrity test for prepare fieldmap
    
    * More intuitive if, and add NotImplementedError test
    
    * Add and fix comments
    
    * Add not implemented error
    
    * Switch phasediff variable to phase_img
    
    * Refactor unwrap_phase
    
    * Fix merge with master, make mag optional in unwrap phase and prepare fieldmap and fix comments
    
    Co-authored-by: Julien Cohen-Adad <jcohen@polymtl.ca>

---
+++ test/cli/test_cli_prepare_fieldmap.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*
+
+from click.testing import CliRunner
+import os
+import pathlib
+import tempfile
+import pytest
+
+from shimmingtoolbox.cli.prepare_fieldmap import prepare_fieldmap_cli
+from shimmingtoolbox import __dir_testing__
+
+
+@pytest.mark.prelude
+def test_cli_prepare_fieldmap_1_echo():
+    with tempfile.TemporaryDirectory(prefix='st_' + pathlib.Path(__file__).stem) as tmp:
+        runner = CliRunner()
+
+        fname_phasediff = os.path.join(__dir_testing__, 'realtime_zshimming_data', 'nifti', 'sub-example', 'fmap',
+                                       'sub-example_phasediff.nii.gz')
+        fname_mag = os.path.join(__dir_testing__, 'realtime_zshimming_data', 'nifti', 'sub-example', 'fmap',
+                                 'sub-example_magnitude1.nii.gz')
+        fname_output = os.path.join(tmp, 'fieldmap.nii.gz')
+
+        result = runner.invoke(prepare_fieldmap_cli, [fname_phasediff, '-mag', fname_mag, '-output', fname_output],
+                               catch_exceptions=False)
+
+        assert result.exit_code == 0
+        assert os.path.isfile(fname_output)
+
+
+@pytest.mark.prelude
+def test_cli_prepare_fieldmap_2_echos():
+    with tempfile.TemporaryDirectory(prefix='st_' + pathlib.Path(__file__).stem) as tmp:
+        runner = CliRunner()
+
+        fname_phase1 = os.path.join(__dir_testing__, 'sub-fieldmap', 'fmap', 'sub-fieldmap_phase1.nii.gz')
+        fname_phase2 = os.path.join(__dir_testing__, 'sub-fieldmap', 'fmap', 'sub-fieldmap_phase2.nii.gz')
+        fname_output = os.path.join(tmp, 'fieldmap.nii.gz')
+
+        fname_mag = os.path.join(__dir_testing__, 'sub-fieldmap', 'fmap', 'sub-fieldmap_magnitude1.nii.gz')
+
+        result = runner.invoke(prepare_fieldmap_cli, [fname_phase1, fname_phase2, '-mag', fname_mag,
+                                                      '-output', fname_output], catch_exceptions=False)
+
+        assert result.exit_code == 0
+        assert os.path.isfile(fname_output)
