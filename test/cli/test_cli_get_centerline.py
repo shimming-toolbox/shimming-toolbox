79f4923b2c5bf9d6fee0705758d53ada4c50ea60
maheuss <71348807+maheuss@users.noreply.github.com>
2020-11-26 12:26:25 -0500

    Using CLI before creating mask w/ centerline proc.

---
+++ test/cli/test_cli_get_centerline.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import pathlib
+import tempfile
+import os
+
+from click.testing import CliRunner
+from shimmingtoolbox.cli.get_centerline import get_centerline_cli
+from shimmingtoolbox import __dir_testing__
+
+
+def test_cli_get_centerline():
+    with tempfile.TemporaryDirectory(prefix='st_' + pathlib.Path(__file__).stem) as tmp:
+        runner = CliRunner()
+
+        inp = os.path.join(__dir_testing__, 'sub-fieldmap', 'fmap', 'sub-fieldmap_phase1.nii.gz')
+        out = os.path.join(tmp, 'nifti')
+        method = "fitseg"
+        centerline_algo = "linear"
+
+        result = runner.invoke(get_centerline_cli, ['-input', inp, '-method', method, '-centerline-algo',
+                                                    centerline_algo, '-output', out])
+
+        assert result.exit_code == 0
+        assert result is not None
+        assert len(os.listdir(out)) != 0
