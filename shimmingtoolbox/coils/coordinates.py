a943378f3c51fb3c8cb91da34980d5801c1ceb1a
Alex Dastous <47249340+po09i@users.noreply.github.com>
2020-11-29 15:47:43 -0500

    Compute gradient in physical and voxel coordinate systems (#181)
    * Initial gradient api
    
    * Initial tests
    
    * Fix mistakes and bugs
    
    * Update tests
    
    * Add comments
    
    * Add comments to tests
    
    * Add initial phys_to_vox_gradient
    
    * Add testing
    
    * Start solving for rotation matrix
    
    * Remove unecessary comments and fix phys_to_vox

--- shimmingtoolbox/coils/coordinates.py
+++ shimmingtoolbox/coils/coordinates.py
@@ -4,6 +4,7 @@
 
 import numpy as np
 from nibabel.affines import apply_affine
+import math
 
 
 def generate_meshgrid(dim, affine):
@@ -31,3 +32,96 @@ def generate_meshgrid(dim, affine):
                 for i in range(3):
                     coord_phys[i][ix, iy, iz] = coord_phys_list[i]
     return coord_phys
+
+
+def phys_gradient(data, affine):
+    """Calculate the gradient of ``data`` along physical coordinates defined by ``affine``
+
+    Args:
+        data (numpy.ndarray): 3d array containing data to apply gradient
+        affine (numpy.ndarray): 4x4 array containing affine transformation
+
+    Returns
+        numpy.ndarray: 3D matrix containing the gradient along the x direction in the physical coordinate system
+        numpy.ndarray: 3D matrix containing the gradient along the y direction in the physical coordinate system
+        numpy.ndarray: 3D matrix containing the gradient along the z direction in the physical coordinate system
+    """
+
+    x_vox = 0
+    y_vox = 1
+    z_vox = 2
+
+    # Calculate the spacing along the different voxel axis
+    x_vox_spacing = math.sqrt((affine[0, x_vox] ** 2) + (affine[1, x_vox] ** 2) + (affine[2, x_vox] ** 2))
+    y_vox_spacing = math.sqrt((affine[0, y_vox] ** 2) + (affine[1, y_vox] ** 2) + (affine[2, y_vox] ** 2))
+    z_vox_spacing = math.sqrt((affine[0, z_vox] ** 2) + (affine[1, z_vox] ** 2) + (affine[2, z_vox] ** 2))
+
+    # Compute the gradient along the different voxel axis
+    if data.shape[x_vox] != 1:
+        x_vox_gradient = np.gradient(data, x_vox_spacing, axis=x_vox)
+    else:
+        x_vox_gradient = np.zeros_like(data)
+
+    if data.shape[y_vox] != 1:
+        y_vox_gradient = np.gradient(data, y_vox_spacing, axis=y_vox)
+    else:
+        y_vox_gradient = np.zeros_like(data)
+
+    if data.shape[z_vox] != 1:
+        z_vox_gradient = np.gradient(data, z_vox_spacing, axis=z_vox)
+    else:
+        z_vox_gradient = np.zeros_like(data)
+
+    # Compute the gradient along the physical axis
+    x_gradient = (x_vox_gradient * affine[0, x_vox] / x_vox_spacing) + \
+                 (y_vox_gradient * affine[0, y_vox] / y_vox_spacing) + \
+                 (z_vox_gradient * affine[0, z_vox] / z_vox_spacing)
+    y_gradient = (x_vox_gradient * affine[1, x_vox] / x_vox_spacing) + \
+                 (y_vox_gradient * affine[1, y_vox] / y_vox_spacing) + \
+                 (z_vox_gradient * affine[1, z_vox] / z_vox_spacing)
+    z_gradient = (x_vox_gradient * affine[2, x_vox] / x_vox_spacing) + \
+                 (y_vox_gradient * affine[2, y_vox] / y_vox_spacing) + \
+                 (z_vox_gradient * affine[2, z_vox] / z_vox_spacing)
+
+    return x_gradient, y_gradient, z_gradient
+
+
+def phys_to_vox_gradient(gx, gy, gz, affine):
+    """
+    Calculate the gradient along the voxel coordinates defined by ``affine`` with gradients in the physical
+    coordinate system
+
+    Args:
+        gx (numpy.ndarray): 3D matrix containing the gradient along the x direction in the physical coordinate system
+        gy (numpy.ndarray): 3D matrix containing the gradient along the y direction in the physical coordinate system
+        gz (numpy.ndarray): 3D matrix containing the gradient along the z direction in the physical coordinate system
+        affine (numpy.ndarray): 4x4 array containing affine transformation
+
+    Returns:
+        numpy.ndarray: 3D matrix containing the gradient along the x direction in the voxel coordinate system
+        numpy.ndarray: 3D matrix containing the gradient along the y direction in the voxel coordinate system
+        numpy.ndarray: 3D matrix containing the gradient along the z direction in the voxel coordinate system
+    """
+
+    x_vox = 0
+    y_vox = 1
+    z_vox = 2
+
+    # Calculate the spacing along the different voxel axis
+    x_vox_spacing = math.sqrt((affine[0, x_vox] ** 2) + (affine[1, x_vox] ** 2) + (affine[2, x_vox] ** 2))
+    y_vox_spacing = math.sqrt((affine[0, y_vox] ** 2) + (affine[1, y_vox] ** 2) + (affine[2, y_vox] ** 2))
+    z_vox_spacing = math.sqrt((affine[0, z_vox] ** 2) + (affine[1, z_vox] ** 2) + (affine[2, z_vox] ** 2))
+
+    inv_affine = np.linalg.inv(affine[:3, :3])
+
+    gx_vox = (gx * inv_affine[0, x_vox] * x_vox_spacing) + \
+             (gy * inv_affine[0, y_vox] * x_vox_spacing) + \
+             (gz * inv_affine[0, z_vox] * x_vox_spacing)
+    gy_vox = (gx * inv_affine[1, x_vox] * y_vox_spacing) + \
+             (gy * inv_affine[1, y_vox] * y_vox_spacing) + \
+             (gz * inv_affine[1, z_vox] * y_vox_spacing)
+    gz_vox = (gx * inv_affine[2, x_vox] * z_vox_spacing) + \
+             (gy * inv_affine[2, y_vox] * z_vox_spacing) + \
+             (gz * inv_affine[2, z_vox] * z_vox_spacing)
+
+    return gx_vox, gy_vox, gz_vox
