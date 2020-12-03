4d46f724d61221361bd13c0527e976bad7c5efc1
po09i <po09i@hotmail.com>
2020-12-03 03:40:36 -0500

    Add phasediff instead of phase for phasediff images

--- shimmingtoolbox/dicom_to_nifti.py
+++ shimmingtoolbox/dicom_to_nifti.py
@@ -13,6 +13,7 @@ import shutil
 
 from shimmingtoolbox import __dir_config_dcm2bids__
 
+
 def dicom_to_nifti(path_dicom, path_nifti, subject_id='sub-01', path_config_dcm2bids=__dir_config_dcm2bids__, remove_tmp=False):
     """ Converts dicom files into nifti files by calling dcm2bids
 
@@ -54,6 +55,42 @@ def dicom_to_nifti(path_dicom, path_nifti, subject_id='sub-01', path_config_dcm2
     subprocess.run(['dcm2bids', '-d', path_dicom, '-o', path_nifti, '-p', subject_id, '-c', path_config_dcm2bids],
                    check=True)
 
+    # In the special case where a phasediff should be created but the filename is phase instead. Find the file and
+    # rename it
+    # Go in the fieldmap folder
+    path_fmap = os.path.join(path_nifti, subject_id, 'fmap')
+    if os.path.exists(path_fmap):
+        # Make a list of the json files in fmap folder
+        file_list = []
+        [file_list.append(os.path.join(path_fmap, f)) for f in os.listdir(path_fmap)
+         if os.path.splitext(f)[1] == '.json']
+        file_list = sorted(file_list)
+
+        for fname_json in file_list:
+            is_renaming = False
+            # Open the json file
+            with open(fname_json) as json_file:
+                json_data = json.load(json_file)
+                # Make sure it is a phase data and that the keys EchoTime1 and EchoTime2 are defined
+                if ('ImageType' in json_data) and ('P' in json_data['ImageType']) and \
+                   ('EchoTime1' in json_data) and ('EchoTime2' in json_data):
+                    # Make sure it is not already named phasediff
+                    if len(os.path.basename(fname_json).rsplit('phasediff', 1)) == 1:
+                        # Split the filename in 2 and remove phase
+                        file_parts = fname_json.rsplit('phase', 1)
+                        if len(file_parts) == 2:
+                            # Stitch the filename back together making sure to remove any digits that could be after
+                            # 'phase'
+                            digits = '0123456789'
+                            fname_new_json = file_parts[0] + 'phasediff' + file_parts[1].lstrip(digits)
+                            is_renaming = True
+            # Rename the json file
+            if is_renaming:
+                os.rename(fname_json, fname_new_json)
+                fname_nifti_new = os.path.splitext(fname_new_json)[0] + '.nii.gz'
+                fname_nifti_old = os.path.splitext(fname_json)[0] + '.nii.gz'
+                os.rename(fname_nifti_old, fname_nifti_new)
+
     # if 'win' in sys.platform:
     #     # dcm2bids is broken for windows as a python package so using CLI
     #     # Create bids structure for data
@@ -76,7 +113,8 @@ def dicom_to_nifti(path_dicom, path_nifti, subject_id='sub-01', path_config_dcm2
     #     if not helper_file_list:
     #         raise ValueError('No data to process')
     #
-    #     subprocess.run(['dcm2bids', '-d', path_dicom, '-o', path_nifti, '-p', subject_id, '-c', path_config_dcm2bids], check=True)
+    #     subprocess.run(['dcm2bids', '-d', path_dicom, '-o', path_nifti, '-p', subject_id, '-c', path_config_dcm2bids],
+    #     check=True)
     # else:
     #     bids_info = dcm2bids.Dcm2bids([path_dicom], subject_id, path_config_dcm2bids, path_nifti)
     #     scaffold()
