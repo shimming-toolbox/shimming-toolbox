1ac2f59a652d9ba39ae02cd8c83e375e179c56ba
po09i <po09i@hotmail.com>
2020-12-03 12:59:55 -0500

    Update with the new CLIs

--- examples/demo_realtime_zshimming.sh
+++ examples/demo_realtime_zshimming.sh
@@ -9,44 +9,19 @@
 st_download_data testing_data
 
 # Go inside folder
-cd testing_data/realtime_zshimming_data
+cd testing_data/realtime_zshimming_data || exit
 
 # dcm2bids -d . -o nifti -p sub-example -c ../../config/dcm2bids.json
 st_dicom_to_nifti -input . -output ../nifti -subject sub-example
-cd ../nifti/sub-example/fmap
+cd ../nifti/sub-example/fmap || exit
 # TODO: Name of phase2 should be phasediff
 
-# Calling FSL directly
-fslsplit sub-example_phasediff.nii.gz sub-example_phasediff_ -t
-fslsplit sub-example_magnitude1.nii.gz sub-example_magnitude1_ -t
-
-# TODO: 9 represents the number of timepoints. This should be changed to a variable that is fetched by looking at the files.
-for i_t in {0..9..1}
-do
-  fsl_prepare_fieldmap SIEMENS "sub-example_phasediff_000${i_t}.nii.gz" "sub-example_magnitude1_000${i_t}.nii.gz" "sub-example_phasediff_unwrapped_000${i_t}.nii.gz" 2.46 --nocheck
-done
-
-fslmerge -t sub-example_phasediff_unwrapped.nii.gz sub-example_phasediff_unwrapped_0000.nii.gz sub-example_phasediff_unwrapped_0001.nii.gz
-for i_t in {2..9..1}
-do
-  fslmerge -t sub-example_phasediff_unwrapped.nii.gz sub-example_phasediff_unwrapped.nii.gz sub-example_phasediff_unwrapped_000${i_t}.nii.gz
-done
-#TODO: remove intermediate files
-
-# The output of fsl_prepare_fieldmap is in rad/s so below we convert to Hz/s
-fslmaths sub-example_phasediff_unwrapped.nii.gz -div 6.28 sub-example_fieldmap.nii.gz
-
-# Not implemented:
-# <<
-# Using our CLI:
-#st_unwrap_prelude -phase sub-example_phasediff.nii.gz -mag sub-example_magnitude1.nii.gz
-# default output will be -phase with suffix _unwrapped
-#st_compute_b0field -phase sub-example_phasediff_unwrapped.nii.gz -o sub-example_fieldmap.nii.gz
-# fieldmap.nii is a 4d file, with the 4th dimension being the time. Ie: one B0 field per time point.
-# >>
+# Create fieldmap
+st_prepare_fieldmap "sub-example_phasediff.nii.gz" -mag "sub-example_magnitude1.nii.gz" -unwrapper "prelude" -output "sub-example_fieldmap.nii.gz"
 
 # Mask anatomical image
 # Calling FSL directly
+# TODO: st_mask
 fslmaths sub-example_T2star_echo-1.nii.gz -thr 500 mask.nii.gz
 fslmaths mask.nii.gz -bin mask.nii.gz
 # Not implemented:
@@ -56,26 +31,12 @@ fslmaths mask.nii.gz -bin mask.nii.gz
 # st_mask -method shape -shape cube -size 5 -o mask.nii
 # >>
 
-# Generate a coil profile based on the spherical harmonics basis
-st_generate_profile_spherical_harmonics -i sub-example_fieldmap.nii.gz -order 2 -o coil_profile.nii.gz
-# Alternatively: use custom coil geomtry:
-#st_generate_profile_custom
-# Alternatively: Use already-available profiles
-#st_download_data coil_profile_my_awesome_coil
-
-# Note: the original realtime z-shimming implemented did not use the coil profile because it directly computed the Gz
-# from the estimated static and dynamic (riro) fieldmaps_Gz:
-# https://github.com/shimming-toolbox/shimming-toolbox-matlab/blob/master/example/realtime_zshim.m#L356
-
-st_shim -fmap fieldmap.nii [-coil-profile $SHIM_DIR/coils/siemens_terra.nii] -mask mask.nii -physio XX -method {volumewise, slicewise}
-# st_shim will:
-# - resample coil profile into the space of fieldmap.nii
+#TODO: st_realtime_zshim
+st_realtime_zshim -fmap "sub-example_fieldmap.nii.gz" -anat "../anat/sub-example_unshimmed_e1.nii.gz" -resp "../../../PMUresp_signal.resp" -mask "TODO" 
+# st_realtime_zshim will:
 # - resample (in time) the physio trace to the 4d fieldmap data so that each time point of the fieldmap has its corresponding respiratory probe value.
-# - run optimizer within mask.nii
-# - outputs:
-#   - fieldmap_shimmed.nii
-#   - coefficients.csv
-#   - figures
+# - Calculate voxelwise gradients for the fieldmap
+# - Calculate a static offset component
+# - Calculate a respiratory induce component (RIRO)
+# - Output, riro, static and mean_p per slice in a text file that can be read by the sequence
 
-# Output text file to be read by syngo console
-# TODO
