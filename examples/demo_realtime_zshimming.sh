69c1f961fe574a15fee5f7e4b3d7d81d16f6e1e0
po09i <po09i@hotmail.com>
2020-12-03 16:14:49 -0500

    Update shell practices

--- examples/demo_realtime_zshimming.sh
+++ examples/demo_realtime_zshimming.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
 # This function will generate static and dynamic (due to respiration) Gz components based on a fieldmap time series
 # (magnitude and phase images) and respiratory trace information obtained from Siemens bellows. An additional
@@ -12,17 +12,17 @@ st_download_data testing_data
 cd testing_data/realtime_zshimming_data || exit
 
 # dcm2bids -d . -o nifti -p sub-example -c ../../config/dcm2bids.json
-st_dicom_to_nifti -input . -output ../nifti -subject sub-example
+st_dicom_to_nifti -input . -output ../nifti -subject sub-example || exit
 cd ../nifti/sub-example/fmap || exit
 
 # Create fieldmap
-st_prepare_fieldmap "sub-example_phasediff.nii.gz" -mag "sub-example_magnitude1.nii.gz" -unwrapper "prelude" -output "sub-example_fieldmap.nii.gz"
+st_prepare_fieldmap "sub-example_phasediff.nii.gz" -mag "sub-example_magnitude1.nii.gz" -unwrapper "prelude" -output "sub-example_fieldmap.nii.gz" || exit
 
 # Mask anatomical image
-st_mask box -input "../anat/sub-example_unshimmed_e1.nii.gz" -size 15 15 20 -output "sub-example_anat_mask.nii.gz"
+st_mask box -input "../anat/sub-example_unshimmed_e1.nii.gz" -size 15 15 20 -output "sub-example_anat_mask.nii.gz" || exit
 
 #TODO: st_realtime_zshim
-st_realtime_zshim -fmap "sub-example_fieldmap.nii.gz" -anat "../anat/sub-example_unshimmed_e1.nii.gz" -resp "../../../PMUresp_signal.resp" -mask "anat_mask.nii.gz"
+st_realtime_zshim -fmap "sub-example_fieldmap.nii.gz" -anat "../anat/sub-example_unshimmed_e1.nii.gz" -resp "../../../PMUresp_signal.resp" -mask "anat_mask.nii.gz" || exit
 # st_realtime_zshim will:
 # - resample (in time) the physio trace to the 4d fieldmap data so that each time point of the fieldmap has its corresponding respiratory probe value.
 # - Calculate voxelwise gradients for the fieldmap
