e92a5d884c1f336b724e3bd3704a0bfe8fd73d76
maheuss <71348807+maheuss@users.noreply.github.com>
2020-12-07 19:04:04 -0500

    Removed viewer method and added default contrast

--- shimmingtoolbox/cli/get_centerline.py
+++ shimmingtoolbox/cli/get_centerline.py
@@ -13,12 +13,11 @@ CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
                     " (i) a NIFTI file with labels corresponding to the discrete centerline, and (ii) a csv file "
                     "containing the float (more precise) coordinates of the centerline in the RPI orientation.")
 @click.option('-input', 'fname_input', type=click.Path(), required=True, help="(str): Input image. Example: t1.nii.gz")
-@click.option('-contrast', type=click.Choice(['t1', 't2', 't2s', 'dwi']),
-              help="(str): Type of image contrast. Only with method=optic.")
-@click.option('-method', type=click.Choice(['optic', 'viewer', 'fitseg']), default='optic',
+@click.option('-contrast', type=click.Choice(['t1', 't2', 't2s', 'dwi']), default='t1',
+              help="(str): Type of image contrast. Only with method=optic. (default: t1)")
+@click.option('-method', type=click.Choice(['optic', 'fitseg']), default='optic',
               help="(str): Method used for extracting the centerline: "
                    "- optic: automatic spinal cord detection method"
-                   "- viewer: manual selection a few points followed by interpolation"
                    "- fitseg: fit a regularized centerline on an already-existing cord segmentation. It will "
                    "interpolate if slices are missing and extrapolate beyond the segmentation boundaries (i.e., every "
                    "axial slice will exhibit a centerline pixel). (default: optic)")
@@ -29,11 +28,9 @@ CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 @click.option('-output', type=click.Path(), default=os.path.join(os.curdir, 'mask_centerline'),
               help="(str): File name (without extension) for the centerline output files. Example: 'centerline_optic'. "
                    "(default: (os.curdir, 'mask_centerline')).")
-@click.option('-gap', default=20.0, help="(float): Gap in mm between manually selected points. Only with method=viewer."
-                                         " (default: 20.0)")
 @click.option('-v', type=click.IntRange(0, 2), default=1,
               help="(int): 1: display on (default), 0: display off, 2: extended. (default: 1)")
-def get_centerline_cli(fname_input, contrast, method, centerline_algo, centerline_smooth, output, gap, v):
+def get_centerline_cli(fname_input, contrast, method, centerline_algo, centerline_smooth, output, v):
     if method == "optic":
         subprocess.run(['sct_get_centerline', '-i', fname_input, '-c', contrast, '-o', output, '-v', v], check=True)
 
@@ -44,10 +41,6 @@ def get_centerline_cli(fname_input, contrast, method, centerline_algo, centerlin
     elif method == "fitseg" and (centerline_algo == "bspline" or centerline_algo == "linear"):
         subprocess.run(['sct_get_centerline', '-i', fname_input, '-method', method, '-centerline-algo', centerline_algo,
                         '-centerline-smooth', centerline_smooth, '-o', output, '-v', v], check=True)
-
-    elif method == "viewer":
-        subprocess.run(['sct_get_centerline', '-i', fname_input, '-method', method, '-o', output, '-gap', gap, '-v', v],
-                       check=True)
 
     else:
         raise ValueError("The implementation of get_centerline_cli is bad. Run get_centerline_cli -h for more "
