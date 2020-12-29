9571fcaea8bbe2560358212c3149b01a3ed4640c
charjl <e.jantzlee@gmail.com>
2020-12-29 13:01:44 -0400

    79 character lines are recommended. This comes close. Readability.

--- shimmingtoolbox/__init__.py
+++ shimmingtoolbox/__init__.py
@@ -9,8 +9,14 @@ except ImportError:
 __version__ = metadata.version(__name__)
 del metadata
 
-__dir_shimmingtoolbox__ = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+file_path = os.path.dirname(os.path.realpath(__file__))
+__dir_shimmingtoolbox__ = os.path.dirname(file_path)
+del file_path
+
 __dir_testing__ = os.path.join(__dir_shimmingtoolbox__, 'testing_data')
-__dir_config_dcm2bids__ = os.path.join(__dir_shimmingtoolbox__, 'config', 'dcm2bids.json')
+
+config_path = __dir_shimmingtoolbox__, 'config', 'dcm2bids.json'
+__dir_config_dcm2bids__ = os.path.join(config_path)
+del config_path
 
 from .dicom_to_nifti import dicom_to_nifti
