0226a016bdadc6a2be80bbfcf9ff28de07f14678
charjl <e.jantzlee@gmail.com>
2020-12-30 14:12:34 -0400

    Errors into localization file, types and elses need work

--- shimmingtoolbox/dicom_to_nifti.py
+++ shimmingtoolbox/dicom_to_nifti.py
@@ -1,20 +1,20 @@
 #!usr/bin/env python3
 # -*- coding: utf-8
 
+# from dcm2bids.scaffold import scaffold
 from distutils.dir_util import copy_tree
+from shimmingtoolbox import __dir_config_dcm2bids__
 
 import goop
 import json
+import language as notice
 import numpy as np
 import os
 import re
 import sys
 import subprocess
 import dcm2bids
-# from dcm2bids.scaffold import scaffold
 import shutil
-
-from shimmingtoolbox import __dir_config_dcm2bids__
 
 
 def dicom_to_nifti(path_dicom, path_nifti, subject_id='sub-01', path_config_dcm2bids=__dir_config_dcm2bids__, remove_tmp=False):
@@ -26,54 +26,55 @@ def dicom_to_nifti(path_dicom, path_nifti, subject_id='sub-01', path_config_dcm2
 
     """
 
-    # Create the folder where the nifti files will be stored
+
+    #TODO Charlotte make this in config!!
+    #TODO Charlotte check error types, you're wrong all over
+    # Check for specified nifti file, else create a new file
     if not os.path.exists(path_dicom):
-        raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), path_config_dicom)
-        #No dicom path found at
-
-
-
-    if not os.path.exists(path_config_dcm2bids):
-        #error_message = "No dcm2bids config file found at:" #TODO: Charlotte can I put error_message back in?
-        raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), path_config_dcm2bids)
-        #error_message = None
+        raise FileNotFoundError(errno.ENOENT, notice.<CONFIG VARIABLE>._no_dicom_path, path_config_dicom)
     if not os.path.exists(path_nifti):
         os.makedirs(path_nifti)
 
+    # Check for dicom config file
+    if not os.path.exists(path_config_dcm2bids):
+        raise FileNotFoundError(errno.ENOENT, notice.<CONFIG VARIABLE>._no_dicom_config, path_config_dcm2bids)
+
+
     # dcm2bids is broken for windows as a python package so using CLI
     # Create bids structure for data
-    subprocess.run(['dcm2bids_scaffold', '-o', path_nifti], check=True)
+    sub_process = subprocess.run(['dcm2bids_scaffold', '-o', path_nifti], check=True)
+    if not sub_process.<ERROR CODE> == 0: #TODO CHARLOTTE there's a python thingy here i need to check itches wrong
+        raise FileNotFoundError(errno.ENOENT, notice.<CONFIG VARIABLE>._no_bids_structure, sub_process.<ERROR STATEMENT>)
 
-    #
-    # # Copy original dicom files into nifti_path/sourcedata
+
+    # Copy original dicom files into nifti_path/sourcedata
     copy_tree(path_dicom, os.path.join(path_nifti, 'sourcedata'))
-    #
-    # # Call the dcm2bids_helper
-    subprocess.run(['dcm2bids_helper', '-d', path_dicom, '-o', path_nifti], check=True)
-    #
+    
+    # Call the dcm2bids_helper
+    sub_process = subprocess.run(['dcm2bids_helper', '-d', path_dicom, '-o', path_nifti], check=True)
+    if not sub_process.<ERROR CODE> == 0: #TODO CHARLOTTE there's a python thingy here i need to check, itches wrong
+        raise FileNotFoundError(errno.ENOENT, notice.<CONFIG VARIABLE>._failed_dcm2bids_helper, sub_process.<ERROR STATEMENT>)
+
     # Check if the helper folder has been created
     path_helper = os.path.join(path_nifti, 'tmp_dcm2bids', 'helper')
     if not os.path.isdir(path_helper):
-        error_message ='dcm2bids_helper could not create directory helper'
-        raise ValueError(error_message)
-        error_message = None
+        raise ValueError(_dcm2bids_helper_creation)
 
-    #TODO: Charlotte: put all these errors in a languae file! otherwise good luck with localizations. Clear it but leave it in globally
     # Make sure there is data in nifti_path / tmp_dcm2bids / helper
     helper_file_list = os.listdir(path_helper)
     if not helper_file_list:
-        error_message = 'No data to process'
-        raise ValueError(error_message)
-        error_message = None
+        raise ValueError(_no_data)
 
-    subprocess.run(['dcm2bids', '-d', path_dicom, '-o', path_nifti, '-p', subject_id, '-c', path_config_dcm2bids],
+    sub_process = subprocess.run(['dcm2bids', '-d', path_dicom, '-o', path_nifti, '-p', subject_id, '-c', path_config_dcm2bids],
                    check=True)
+    if not sub_process.<ERROR CODE> == 0: 
+        raise FileNotFoundError(errno.ENOENT, notice.<CONFIG VARIABLE>._failed_dcm2bids_helper, sub_process._no_dcm2bids)
 
     # In the special case where a phasediff should be created but the filename is phase instead. Find the file and
     # rename it
     # Go in the fieldmap folder
     path_fmap = os.path.join(path_nifti, subject_id, 'fmap')
-    if os.path.exists(path_fmap):
+    if os.path.exists(path_fmap): #TODO CHARLOTTE ADD AN ELSE FOR THIS BEAUTY
         # Make a list of the json files in fmap folder
         file_list = []
 
@@ -83,8 +84,8 @@ def dicom_to_nifti(path_dicom, path_nifti, subject_id='sub-01', path_config_dcm2
 
         for fname_json in file_list:
             is_renaming = False
-            # Open the json file
-            with open(fname_json) as json_file:
+            # Open the json file TO DO CHARLOTTE ADD AN ELSE
+            with open(fname_json) as json_file:  
                 json_data = json.load(json_file)
                 # Make sure it is a phase data and that the keys EchoTime1 and EchoTime2 are defined and that
                 # sequenceName's last digit is 2 (refers to number of echoes when using dcm2bids)
@@ -106,4 +107,6 @@ def dicom_to_nifti(path_dicom, path_nifti, subject_id='sub-01', path_config_dcm2
                     os.rename(fname_json, fname_new_json)
 
     if remove_tmp:
-        shutil.rmtree(os.path.join(path_nifti, 'tmp_dcm2bids'))
+        removal_tmp = shutil.rmtree(os.path.join(path_nifti, 'tmp_dcm2bids'))#TODO CHARLOTTE gentalize the raise
+    	if not removal_tmp.<ERROR CODE> == 0: 
+        	raise FileNotFoundError(errno.ENOENT, notice.<CONFIG VARIABLE>._temp_removal, sub_process._no_dcm2bids)
