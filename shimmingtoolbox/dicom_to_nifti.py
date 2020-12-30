be974ffb5762aebef43cb2d656cf6a485c351a85
charjl <e.jantzlee@gmail.com>
2020-12-30 18:04:15 -0400

    Second itteration; needs error tpyes changed and a few more catches

--- shimmingtoolbox/dicom_to_nifti.py
+++ shimmingtoolbox/dicom_to_nifti.py
@@ -74,7 +74,7 @@ def dicom_to_nifti(path_dicom, path_nifti, subject_id='sub-01', path_config_dcm2
     # rename it
     # Go in the fieldmap folder
     path_fmap = os.path.join(path_nifti, subject_id, 'fmap')
-    if os.path.exists(path_fmap): #TODO CHARLOTTE ADD AN ELSE FOR THIS BEAUTY
+    if os.path.exists(path_fmap):
         # Make a list of the json files in fmap folder
         file_list = []
 
@@ -82,29 +82,36 @@ def dicom_to_nifti(path_dicom, path_nifti, subject_id='sub-01', path_config_dcm2
             file_list.append(os.path.join(path_fmap, file)) for file in os.listdir(path_fmap)
             file_list = sorted(file_list)
 
-        for fname_json in file_list:
+        for json_file in file_list:
             is_renaming = False
-            # Open the json file TO DO CHARLOTTE ADD AN ELSE
-            with open(fname_json) as json_file:  
-                json_data = json.load(json_file)
+            # Open the json file TODO: CHARLOTTE ADD AN ELSE
+            with open(json_file) as file:  
+                json_data = json.load(file) #todo: Charlotte can this be guarded as well?
                 # Make sure it is a phase data and that the keys EchoTime1 and EchoTime2 are defined and that
                 # sequenceName's last digit is 2 (refers to number of echoes when using dcm2bids)
 
 
-#TODO Charlotte clean this up.
-                if ('ImageType' in json_data) and ('P' in json_data['ImageType']) and \
-                   ('EchoTime1' in json_data) and ('EchoTime2' in json_data) and \
-                   ('SequenceName' in json_data) and (int(json_data['SequenceName'][-1]) == 2):
-                        fname_new_json = fname_json =  re.sub('[0-9]', '', fname)
+		#TODO: Charlotte --> check these variables you've messed them up
+                if ('ImageType' in json_data) and \
+		('P' in json_data['ImageType']) and \
+		('EchoTime1' in json_data) and \
+		('EchoTime2' in json_data) and \
+                ('SequenceName' in json_data) and \
+		(int(json_data['SequenceName'][-1]) == 2):
+                        fname_new_json = fname_json =  re.sub('[0-9]', '', json_file)
                         is_renaming = True
+
             # Rename the json file an nifti file 
             if is_renaming:
-                #TODO: Charlotte I dont't like splittext
-                if os.path.exists(os.path.splitext(fname_json)[0] + '.nii.gz'):
+                #TODO: Charlotte I dont't like splittext. something better?
+		path_to_split = os.path.splitext(fname_json)[0] + '.nii.gz'
+
+                if not os.path.exists(path_to_split) == 0: #todo: check this charlotte
                     fname_nifti_new = os.path.splitext(fname_new_json)[0] + '.nii.gz'
                     fname_nifti_old = os.path.splitext(fname_json)[0] + '.nii.gz'
                     os.rename(fname_nifti_old, fname_nifti_new)
                     os.rename(fname_json, fname_new_json)
+		raise FileNotFoundError(errno.ENOENT, notice.<CONFIG VARIABLE>._json_file_location, sub_process._no_dcm2bids)
 
     if remove_tmp:
         removal_tmp = shutil.rmtree(os.path.join(path_nifti, 'tmp_dcm2bids'))#TODO CHARLOTTE gentalize the raise
--- shimmingtoolbox/langauge/English.py
+++ shimmingtoolbox/langauge/English.py
@@ -5,9 +5,11 @@
 # Error messages
 _dcm2bids_helper_creation ='dcm2bids_helper could not create directory helper'
 _failed_dcm2bids_helper = 'Could not run dcm2bids_helper'
+_json_file_location = 'Cannot find JSON file in specified location'
 _no_bids_structure = 'Creating bids structure for data failed'
 _no_data = 'No data to process'
 _no_dcm2bids = 'Cannot call dcm2bids'
 _no_dicom_config = 'No dcm2bids config file found at '
 _no_dicom_path = 'No dicom path found at '
 _temp_removal = 'Could not remove tmp file'
+
