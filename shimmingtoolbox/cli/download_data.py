788a53331ff820e891b8850a81a628d0ef8a4cd2
po09i <po09i@hotmail.com>
2021-02-09 22:25:00 -0500

    Updated link to he latest release to include spinal cord t2 data

--- shimmingtoolbox/cli/download_data.py
+++ shimmingtoolbox/cli/download_data.py
@@ -15,7 +15,7 @@ from shimmingtoolbox.download import install_data
 
 URL_DICT: Dict[str, Tuple[List[str], str]] = {
     "testing_data": (
-        ["https://github.com/shimming-toolbox/data-testing/archive/r20210126.zip"],
+        ["https://github.com/shimming-toolbox/data-testing/archive/r20210209.zip"],
         "Light-weighted dataset for testing purpose.",
     ),
     "prelude": (
