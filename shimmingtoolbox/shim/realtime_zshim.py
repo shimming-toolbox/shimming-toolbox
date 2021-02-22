1f458a2a65dc838af11fe9f537408e8d5c9735a2
Alex Dastous <47249340+po09i@users.noreply.github.com>
2021-02-22 18:47:53 -0500

    Add RIRO and Static masks to realtime zshimming (#221)
    * Update to input static and riro mask
    
    * Update tests for rtzshim API
    
    * Update the CLI and tests for the CLI
    
    * Update the shell script to input both masks
    
    * Fix typo for shell script
    
    * Updated option names and added RIRO comment

--- shimmingtoolbox/shim/realtime_zshim.py
+++ shimmingtoolbox/shim/realtime_zshim.py
@@ -16,7 +16,8 @@ from shimmingtoolbox.coils.coordinates import phys_gradient
 from shimmingtoolbox.coils.coordinates import phys_to_vox_gradient
 
 
-def realtime_zshim(nii_fieldmap, nii_anat, pmu, json_fmap, nii_mask_anat=None, path_output=None):
+def realtime_zshim(nii_fieldmap, nii_anat, pmu, json_fmap, nii_mask_anat_riro=None, nii_mask_anat_static=None,
+                   path_output=None):
     """ This function will generate static and dynamic (due to respiration) Gz components based on a fieldmap time
     series and respiratory trace information obtained from Siemens bellows  (PMUresp_signal.resp). An additional
     multi-gradient echo (MGRE) magnitude image is used to generate an ROI and resample the static and dynamic Gz
@@ -29,7 +30,10 @@ def realtime_zshim(nii_fieldmap, nii_anat, pmu, json_fmap, nii_mask_anat=None, p
         pmu (PmuResp): Filename of the file of the respiratory trace.
         json_fmap (dict): dict of the json sidecar corresponding to the fieldmap data (Used to find the acquisition
                           timestamps).
-        nii_mask_anat (nibabel.Nifti1Image): Nibabel object containing the mask to specify the shimming region.
+        nii_mask_anat_static (nibabel.Nifti1Image): Nibabel object containing the mask to specify the shimming region
+                                                    for the static component.
+        nii_mask_anat_riro (nibabel.Nifti1Image): Nibabel object containing the mask to specify the shimming region
+                                                  for the riro component.
         path_output (str): Path to output figures and temporary variables. If none is provided, no debug output is
                            provided.
 
@@ -38,7 +42,6 @@ def realtime_zshim(nii_fieldmap, nii_anat, pmu, json_fmap, nii_mask_anat=None, p
         numpy.ndarray: 1D array of the dynamic riro_correction. The correction is in (mT/m)*rms_pressure for each slice.
         float: Average pressure of the pmu
         float: RMS of the pmu pressure
-
     """
 
     # Set up output of figures
@@ -59,31 +62,47 @@ def realtime_zshim(nii_fieldmap, nii_anat, pmu, json_fmap, nii_mask_anat=None, p
     if anat.ndim != 3:
         raise RuntimeError("Anatomical image must be in 3d")
 
-    # Load mask
-    if nii_mask_anat is not None:
-        if not np.all(np.isclose(nii_anat.affine, nii_mask_anat.affine)) or \
-                not np.all(nii_mask_anat.shape == nii_anat.shape):
+    # Load riro mask
+    if nii_mask_anat_riro is not None:
+        if not np.all(np.isclose(nii_anat.affine, nii_mask_anat_riro.affine)) or \
+                not np.all(nii_mask_anat_riro.shape == nii_anat.shape):
             raise RuntimeError("Mask must have the same shape and affine transformation as anat")
         nii_fmap_3d_temp = nib.Nifti1Image(fieldmap[..., 0], nii_fieldmap.affine)
-        nii_mask_fmap = resample_from_to(nii_mask_anat, nii_fmap_3d_temp)
-        mask_fmap = nii_mask_fmap.get_fdata()
+        nii_mask_fmap_riro = resample_from_to(nii_mask_anat_riro, nii_fmap_3d_temp)
+        mask_fmap_riro = nii_mask_fmap_riro.get_fdata()
     else:
-        mask_fmap = np.ones_like(fieldmap[..., 0])
-        nii_mask_fmap = nib.Nifti1Image(mask_fmap, nii_anat.affine)
-        nii_mask_anat = nib.Nifti1Image(np.ones_like(anat), nii_anat.affine)
+        mask_fmap_riro = np.ones_like(fieldmap[..., 0])
+        nii_mask_fmap_riro = nib.Nifti1Image(mask_fmap_riro, nii_anat.affine)
+        nii_mask_anat_riro = nib.Nifti1Image(np.ones_like(anat), nii_anat.affine)
+
+    # Load static mask
+    if nii_mask_anat_static is not None:
+        if not np.all(np.isclose(nii_anat.affine, nii_mask_anat_static.affine)) or \
+                not np.all(nii_mask_anat_static.shape == nii_anat.shape):
+            raise RuntimeError("Mask must have the same shape and affine transformation as anat")
+        nii_fmap_3d_temp = nib.Nifti1Image(fieldmap[..., 0], nii_fieldmap.affine)
+        nii_mask_fmap_static = resample_from_to(nii_mask_anat_static, nii_fmap_3d_temp)
+        mask_fmap_static = nii_mask_fmap_static.get_fdata()
+    else:
+        mask_fmap_static = np.ones_like(fieldmap[..., 0])
+        nii_mask_fmap_static = nib.Nifti1Image(mask_fmap_static, nii_anat.affine)
+        nii_mask_anat_static = nib.Nifti1Image(np.ones_like(anat), nii_anat.affine)
 
     if is_outputting_figures:
-        nib.save(nii_mask_fmap, os.path.join(path_output, 'fig_mask_fmap.nii.gz'))
+        nib.save(nii_mask_fmap_riro, os.path.join(path_output, 'fig_mask_fmap_riro.nii.gz'))
+        nib.save(nii_mask_fmap_static, os.path.join(path_output, 'fig_mask_fmap_static.nii.gz'))
 
-    masked_fieldmaps = np.zeros_like(fieldmap)
+    masked_fieldmaps_static = np.zeros_like(fieldmap)
+    masked_fieldmaps_riro = np.zeros_like(fieldmap)
     for i_t in range(nt):
-        masked_fieldmaps[..., i_t] = mask_fmap * fieldmap[..., i_t]
+        masked_fieldmaps_static[..., i_t] = mask_fmap_static * fieldmap[..., i_t]
+        masked_fieldmaps_riro[..., i_t] = mask_fmap_riro * fieldmap[..., i_t]
 
     # Calculate Gx, Gy and Gz gradients (in the physical coordinate system)
     g = 1000 / 42.5774785178325552e6  # [mT / Hz]
     gradient = np.array([np.zeros_like(fieldmap), np.zeros_like(fieldmap), np.zeros_like(fieldmap)])
     for it in range(nt):
-        gradient[:][..., it] = phys_gradient(g * fieldmap[:, :, :, it], nii_fieldmap.affine) # [mT / mm]
+        gradient[:][..., it] = phys_gradient(g * fieldmap[:, :, :, it], nii_fieldmap.affine)  # [mT / mm]
     gradient *= 1000  # [mT / m]
 
     if is_outputting_figures:
@@ -134,14 +153,10 @@ def realtime_zshim(nii_fieldmap, nii_anat, pmu, json_fmap, nii_mask_anat=None, p
                     static[g_axis][i_x, i_y, i_z] = reg.intercept_
                     progress_bar.update(1)
 
-        #riro[g_axis][:, :, :] = gaussian(riro[g_axis][:, :, :], sigma = 2, mode = 'constant', cval = 0.0, multichannel=False) 
-        #static[g_axis][:, :, :] = gaussian(static[g_axis][:, :, :], sigma = 1.5, mode = 'constant', cval = 0.0, multichannel=False)             
-
-    # Resample masked_fieldmaps to target anatomical image
-    nii_masked_fieldmaps = nib.Nifti1Image(masked_fieldmaps, nii_fieldmap.affine)
-    nii_resampled_fmap = resample_from_to(nii_masked_fieldmaps, nii_anat)
-    if is_outputting_figures:
-        nib.save(nii_resampled_fmap, os.path.join(path_output, 'fig_resampled_fmap.nii.gz'))
+        #riro[g_axis][:, :, :] = gaussian(riro[g_axis][:, :, :], sigma = 2, mode = 'constant', cval = 0.0,
+        # multichannel=False)
+        #static[g_axis][:, :, :] = gaussian(static[g_axis][:, :, :], sigma = 1.5, mode = 'constant', cval = 0.0,
+        # multichannel=False)
 
     # Resample static to target anatomical image
     resampled_static = np.array([np.zeros_like(anat), np.zeros_like(anat), np.zeros_like(anat)])
@@ -153,7 +168,7 @@ def realtime_zshim(nii_fieldmap, nii_anat, pmu, json_fmap, nii_mask_anat=None, p
     _, _, resampled_static_vox = phys_to_vox_gradient(resampled_static[0], resampled_static[1], resampled_static[2],
                                                       nii_anat.affine)
     nii_resampled_static_vox = nib.Nifti1Image(resampled_static_vox, nii_anat.affine)
-    nii_resampled_static_masked = nib.Nifti1Image(resampled_static_vox * nii_mask_anat.get_fdata(),
+    nii_resampled_static_masked = nib.Nifti1Image(resampled_static_vox * nii_mask_anat_static.get_fdata(),
                                                   nii_resampled_static_vox.affine)
     if is_outputting_figures:
         nib.save(nii_resampled_static_masked, os.path.join(path_output, 'fig_resampled_static.nii.gz'))
@@ -168,7 +183,7 @@ def realtime_zshim(nii_fieldmap, nii_anat, pmu, json_fmap, nii_mask_anat=None, p
     _, _, resampled_riro_vox = phys_to_vox_gradient(resampled_riro[0], resampled_riro[1], resampled_riro[2],
                                                     nii_anat.affine)
     nii_resampled_riro_vox = nib.Nifti1Image(resampled_riro_vox, nii_anat.affine)
-    nii_resampled_static_masked = nib.Nifti1Image(resampled_riro_vox * nii_mask_anat.get_fdata(),
+    nii_resampled_static_masked = nib.Nifti1Image(resampled_riro_vox * nii_mask_anat_riro.get_fdata(),
                                                   nii_resampled_riro_vox.affine)
     if is_outputting_figures:
         nib.save(nii_resampled_static_masked, os.path.join(path_output, 'fig_resampled_riro.nii.gz'))
@@ -179,11 +194,11 @@ def realtime_zshim(nii_fieldmap, nii_anat, pmu, json_fmap, nii_mask_anat=None, p
     riro_correction = np.zeros([n_slices])
     for i_slice in range(n_slices):
         ma_static_anat = np.ma.array(resampled_static_vox[..., i_slice],
-                                     mask=nii_mask_anat.get_fdata()[..., i_slice] == False)
+                                     mask=nii_mask_anat_static.get_fdata()[..., i_slice] == False)
         static_correction[i_slice] = np.ma.mean(ma_static_anat)
 
         ma_riro_anat = np.ma.array(resampled_riro_vox[..., i_slice],
-                                   mask=nii_mask_anat.get_fdata()[..., i_slice] == False)
+                                   mask=nii_mask_anat_riro.get_fdata()[..., i_slice] == False)
         riro_correction[i_slice] = np.ma.mean(ma_riro_anat)
     static_correction[np.isnan(static_correction)] = 0.
     riro_correction[np.isnan(riro_correction)] = 0.
@@ -212,13 +227,13 @@ def realtime_zshim(nii_fieldmap, nii_anat, pmu, json_fmap, nii_mask_anat=None, p
         pmu_data_within_range = pmu_data_within_range[pmu_times_within_range < acq_timestamps[fieldmap.shape[3] - 1]]
         pmu_times_within_range = pmu_times_within_range[pmu_times_within_range < acq_timestamps[fieldmap.shape[3] - 1]]
 
-        # Calc fieldmap average within mask
+        # Calc fieldmap average within static mask
         fieldmap_avg = np.zeros([fieldmap.shape[3]])
         for i_time in range(nt):
-            masked_array = np.ma.array(fieldmap[:, :, :, i_time], mask=mask_fmap == False)
+            masked_array = np.ma.array(fieldmap[:, :, :, i_time], mask=mask_fmap_static == False)
             fieldmap_avg[i_time] = np.ma.average(masked_array)
 
-        # Plot pmu vs B0 in masked region
+        # Plot pmu vs B0 in static masked region
         fig = Figure(figsize=(10, 10))
         ax = fig.add_subplot(211)
         ax.plot(acq_timestamps / 1000, acq_pressures, label='Interpolated pressures')
@@ -229,21 +244,25 @@ def realtime_zshim(nii_fieldmap, nii_anat, pmu, json_fmap, nii_mask_anat=None, p
         ax = fig.add_subplot(212)
         ax.plot(acq_timestamps / 1000, fieldmap_avg, label='Mean B0')
         ax.legend()
-        ax.set_title("Fieldmap average over unmasked region (Hz) vs time (s)")
+        ax.set_title("Fieldmap average over static unmasked region (Hz) vs time (s)")
         fname_figure = os.path.join(path_output, 'fig_realtime_zshim_pmu_vs_B0.png')
         fig.savefig(fname_figure)
 
-        # Show anatomical image
+        # Show anatomical image and masks
         fig = Figure(figsize=(10, 10))
-        ax = fig.add_subplot(2, 1, 1)
-        im = ax.imshow(anat[:, :, 3])
+        ax = fig.add_subplot(1, 3, 1)
+        im = ax.imshow(anat[:, :, 10])
         fig.colorbar(im)
-        ax.set_title("Anatomical image [:, :, 3]")
-        ax = fig.add_subplot(2, 1, 2)
-        im = ax.imshow(nii_mask_anat.get_fdata()[:, :, 3])
+        ax.set_title("Anatomical image [:, :, 10]")
+        ax = fig.add_subplot(1, 3, 2)
+        im = ax.imshow(nii_mask_anat_static.get_fdata()[:, :, 10])
         fig.colorbar(im)
-        ax.set_title("Mask [:, :, 3]")
-        fname_figure = os.path.join(path_output, 'fig_reatime_zshim_anat.png')
+        ax.set_title("Mask static [:, :, 10]")
+        ax = fig.add_subplot(1, 3, 3)
+        im = ax.imshow(nii_mask_anat_riro.get_fdata()[:, :, 10])
+        fig.colorbar(im)
+        ax.set_title("Mask riro [:, :, 10]")
+        fname_figure = os.path.join(path_output, 'fig_reatime_zshim_anat_mask.png')
         fig.savefig(fname_figure)
 
         # Show Gradient
