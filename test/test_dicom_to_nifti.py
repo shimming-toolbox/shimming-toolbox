7c937af744198cea7ff5ba418122fcbe32800a81
Ainsleigh Hill <ainsleighhill@gmail.com>
2021-03-09 15:27:32 -0800

    add tests for dicom_to_nifti

--- test/test_dicom_to_nifti.py
+++ test/test_dicom_to_nifti.py
@@ -13,7 +13,11 @@ def test_dicom_to_nifti():
     with tempfile.TemporaryDirectory(prefix='st_'+pathlib.Path(__file__).stem) as tmp:
         path_nifti = os.path.join(tmp, 'nifti')
         subject_id = 'sub-test'
-        dicom_to_nifti(os.path.join(__dir_testing__, 'dicom_unsorted'), path_nifti, subject_id=subject_id)
+        dicom_to_nifti(
+            path_dicom=os.path.join(__dir_testing__, 'dicom_unsorted'),
+            path_nifti=path_nifti,
+            subject_id=subject_id
+        )
         # Check that all the files (.nii.gz and .json) are created with the expected names. The test data has 6
         # magnitude and phase data.
         for i in range(1, 7):
@@ -29,8 +33,11 @@ def test_dicom_to_nifti_realtime_zshim(test_dcm2niix_installation):
     with tempfile.TemporaryDirectory(prefix='st_'+pathlib.Path(__file__).stem) as tmp:
         path_nifti = os.path.join(tmp, 'nifti')
         subject_id = 'sub-test'
-        dicom_to_nifti(os.path.join(__dir_testing__, 'realtime_zshimming_data'), path_nifti,
-                       subject_id=subject_id)
+        dicom_to_nifti(
+            path_dicom=os.path.join(__dir_testing__, 'realtime_zshimming_data'),
+            path_nifti=path_nifti,
+            subject_id=subject_id
+        )
         # Check that all the files (.nii.gz and .json) are created with the expected names. The test data has 6
         # magnitude and phase data.
 
@@ -63,8 +70,42 @@ def test_dicom_to_nifti_remove_tmp(test_dcm2niix_installation):
     with tempfile.TemporaryDirectory(prefix='st_'+pathlib.Path(__file__).stem) as tmp:
         path_nifti = os.path.join(tmp, 'nifti')
         subject_id = 'sub-test'
-        dicom_to_nifti(os.path.join(__dir_testing__, 'dicom_unsorted'), path_nifti, subject_id=subject_id, remove_tmp=True)
+        dicom_to_nifti(
+            path_dicom=os.path.join(__dir_testing__, 'dicom_unsorted'),
+            path_nifti=path_nifti,
+            subject_id=subject_id,
+            remove_tmp=True
+        )
         # Check that all the files (.nii.gz and .json) are created with the expected names. The test data has 6
         # magnitude and phase data.
         assert os.path.exists(path_nifti)
         assert not os.path.exists(os.path.join(path_nifti, 'tmp_dcm2bids'))
+
+
+@pytest.mark.dcm2niix
+def test_dicom_to_nifti_path_dicom_invalid(test_dcm2niix_installation):
+    """Test the remove_tmp folder"""
+    with tempfile.TemporaryDirectory(prefix='st_'+pathlib.Path(__file__).stem) as tmp:
+        path_nifti = os.path.join(tmp, 'nifti')
+        subject_id = 'sub-test'
+        with pytest.raises(FileNotFoundError, match=r"No dicom path found"):
+            dicom_to_nifti(
+                path_dicom=os.path.join(__dir_testing__, 'invalid_folder'),
+                path_nifti=path_nifti,
+                subject_id=subject_id
+            )
+
+
+@pytest.mark.dcm2niix
+def test_dicom_to_nifti_path_config_invalid(test_dcm2niix_installation):
+    """Test the remove_tmp folder"""
+    with tempfile.TemporaryDirectory(prefix='st_'+pathlib.Path(__file__).stem) as tmp:
+        path_nifti = os.path.join(tmp, 'nifti')
+        subject_id = 'sub-test'
+        with pytest.raises(FileNotFoundError, match=r"No dcm2bids config file found"):
+            dicom_to_nifti(
+                path_dicom=os.path.join(__dir_testing__, 'dicom_unsorted'),
+                path_nifti=path_nifti,
+                subject_id=subject_id,
+                path_config_dcm2bids=os.path.join(tmp, "invalid_folder")
+            )
