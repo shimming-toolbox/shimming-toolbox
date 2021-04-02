84e3d932d10dbf4d032566eeda6738064755554a
gaspardcereza <gaspard.cereza@gmail.com>
2021-04-02 11:26:25 -0400

    Added possibility to input CP mode coefficients

--- shimmingtoolbox/b1/b1_shim.py
+++ shimmingtoolbox/b1/b1_shim.py
@@ -3,45 +3,56 @@
 
 import numpy as np
 import scipy.optimize
+import warnings
 
 
-def b1_shim(b1_maps, mask):
+def b1_shim(b1_maps, mask, cp_weights=None):
     """
     Computes static optimized shim weights that minimize the B1 field coefficient of variation over the masked region.
 
     Args:
         b1_maps (numpy.ndarray): 4D array (x, y, slice, coil) corresponding to the measured B1 field.
         mask (numpy.ndarray): 3D array (x, y, slice) corresponding to the region where shimming will be performed.
+        cp_weights: 1D array of complex weights corresponding to the CP mode of the coil. Must be normalized.
 
     Returns:
         numpy.ndarray: Optimized shimming weights.
 
     """
     if b1_maps.ndim == 4:
-        pass
+        x, y, n_slices, n_coils = b1_maps.shape
     else:
         raise ValueError("Unexpected negative magnitude values.")
 
     if b1_maps.shape[:-1] == mask.shape:
-        pass
+        b1_roi = np.reshape(b1_maps, [x * y * n_slices, n_coils])[np.reshape(mask, x * y * n_slices), :]
     else:
         raise ValueError("Mask and maps dimensions not matching.")
 
-    x, y, n_slices, n_coils = b1_maps.shape
-    b1_roi = np.reshape(b1_maps, [x*y*n_slices, n_coils])[np.reshape(mask, x*y*n_slices), :]
-
-    # TODO: add possibility to input CP coefficients
-    weights_init = np.concatenate((np.ones(n_coils)/np.linalg.norm(np.ones(n_coils)), np.linspace(0, 2*np.pi-2*np.pi/n_coils, n_coils)))
+    if cp_weights:
+        if len(cp_weights) == b1_maps.shape[-1]:
+            if np.isclose(np.linalg.norm(cp_weights), 1):
+                weights_init = complex_to_vector(cp_weights)
+            else:
+                warnings.warn("Normalizing the CP mode weights.")
+                weights_init = complex_to_vector(cp_weights/np.linalg.norm(cp_weights))
+        else:
+            raise ValueError("CP mode and maps dimensions not matching.")
+    else:
+        # TODO: add function to compute CP coefficients
+        # Approximation of a circular polarisation to initialize the optimization
+        weights_init = np.concatenate((np.ones(n_coils) / np.linalg.norm(np.ones(n_coils)),
+                                       np.linspace(0, 2 * np.pi - 2 * np.pi / n_coils, n_coils)))
 
     print(f"Coefficient of variation before shimming: {cov(combine_maps(b1_roi, vector_to_complex(weights_init)))}")
 
-    bounds = np.concatenate((n_coils*[(0, 1)], n_coils*[(-np.pi, np.pi)]))
+    bounds = np.concatenate((n_coils * [(0, 1)], n_coils * [(-np.pi, np.pi)]))
 
     def cost(weights):
         return cov(combine_maps(b1_roi, vector_to_complex(weights)))
 
     shim_weights = vector_to_complex(scipy.optimize.minimize(cost, weights_init, bounds=bounds).x)
-    shim_weights = shim_weights/np.linalg.norm(shim_weights)
+    shim_weights = shim_weights / np.linalg.norm(shim_weights)
     print(f"Shim coefficient: {shim_weights}")
     print(f"Coefficient of variation after shimming: {cov(combine_maps(b1_roi, shim_weights))}")
     return shim_weights
@@ -63,7 +74,7 @@ def combine_maps(b1_maps, weights):
     else:
         raise ValueError("The number of shim weights does not match the number of coils.")
 
-    return abs(np.sum(np.multiply(b1_maps, weights), b1_maps.ndim-1))
+    return abs(np.sum(np.multiply(b1_maps, weights), b1_maps.ndim - 1))
 
 
 def cov(array):
@@ -77,12 +88,12 @@ def cov(array):
         float: Coefficient of variation of the input array (standard deviation/mean).
 
     """
-    return np.std(array)/np.mean(array)
+    return np.std(array) / np.mean(array)
 
 
 def vector_to_complex(weights):
     """
-    Combines real and imaginary values contained in a vector into a half long complex vector.
+    Combines magnitude and phase values contained in a vector into a half long complex vector.
 
     Args:
         weights (numpy.ndarray): 1D array of length 2*n_coils. First/second half: real/imaginary part of shim weights.
@@ -95,4 +106,18 @@ def vector_to_complex(weights):
         pass
     else:
         raise ValueError("The vector must have an even number of elements.")
-    return weights[:len(weights)//2] * np.exp(1j * weights[len(weights)//2:])
+    return weights[:len(weights) // 2] * np.exp(1j * weights[len(weights) // 2:])
+
+
+def complex_to_vector(weights):
+    """
+    Combines separates magnitude and phase values contained in a complex vector into a twice as long vector.
+
+    Args:
+        weights (numpy.ndarray): 1D complex array of length n_coils.
+
+    Returns:
+        numpy.ndarray: 1D array of length 2*n_coils. First/second half: real/imaginary part of shim weights.
+
+    """
+    return np.concatenate((np.abs(weights), np.angle(weights)))
--- test/b1/test_b1_shim.py
+++ test/b1/test_b1_shim.py
@@ -5,17 +5,19 @@ import numpy as np
 import os
 import pytest
 from shimmingtoolbox import __dir_testing__
-from shimmingtoolbox.b1.b1_shim import b1_shim, combine_maps, cov, vector_to_complex
+from shimmingtoolbox.b1.b1_shim import b1_shim, combine_maps, cov, vector_to_complex, complex_to_vector
 from shimmingtoolbox.load_nifti import read_nii
 
 fname_b1 = os.path.join(__dir_testing__, 'b1_maps', 'nifti', 'sub-01_run-10_TB1map.nii.gz')
 _, _, b1_maps = read_nii(fname_b1)
 mask = b1_maps[:, :, :, 0] != 0
+cp_weights = [0.3536, -0.3527+0.0247j, 0.2748-0.2225j, -0.1926-0.2965j, -0.3535+0.0062j, 0.2931+0.1977j, 0.3381+0.1034j,
+              -0.1494+0.3204j]
 
 
 def test_b1_shim():
     shim_weights = b1_shim(b1_maps, mask)
-    assert np.linalg.norm(shim_weights) == 1, 'The shim weights are not normalized'
+    assert np.linalg.norm(shim_weights) == 1, "The shim weights are not normalized"
     assert len(shim_weights) == b1_maps.shape[3], "The number of shim weights does not match the number of coils"
 
 
@@ -29,6 +31,25 @@ def test_b1_shim_wrong_mask_shape():
         b1_shim(b1_maps, mask[:-1, :, :])
 
 
+def test_b1_shim_cp_mode():
+    shim_weights = b1_shim(b1_maps, mask, cp_weights)
+    assert np.linalg.norm(shim_weights) == 1, "The shim weights are not normalized"
+    assert len(shim_weights) == b1_maps.shape[3], "The number of shim weights does not match the number of coils"
+
+
+def test_b1_shim_cp_mode_not_normalized():
+    cp_weights_not_normalized = [2*cp_weights[i] for i in range(len(cp_weights))]
+    with pytest.warns(UserWarning, match=r"Normalizing the CP mode weights."):
+        shim_weights = b1_shim(b1_maps, mask, cp_weights_not_normalized)
+    assert np.linalg.norm(shim_weights) == 1, "The shim weights are not normalized"
+    assert len(shim_weights) == b1_maps.shape[3], "The number of shim weights does not match the number of coils"
+
+
+def test_b1_shim_cp_mode_wrong_length():
+    with pytest.raises(ValueError, match=r"CP mode and maps dimensions not matching."):
+        b1_shim(b1_maps, mask, cp_weights[:-1])
+
+
 def test_vector_to_complex():
     assert np.isclose(vector_to_complex(np.asarray([1, 1, 1, 0, np.pi/2, np.pi])), np.asarray([1,  1j, -1])).all(),\
         "The function vector_to_complex returns unexpected results"
@@ -37,6 +58,11 @@ def test_vector_to_complex():
 def test_vector_to_complex_wrong_length():
     with pytest.raises(ValueError, match=r"The vector must have an even number of elements."):
         vector_to_complex(np.asarray([1, 1, 1, 0, np.pi/2])), np.asarray([1,  1j, -1])
+
+
+def test_complex_to_vector():
+    assert np.isclose(complex_to_vector(np.asarray([1,  1j, -1])), np.asarray([1, 1, 1, 0, np.pi/2, np.pi])).all(),\
+        "The function complex_to_vector returns unexpected results"
 
 
 def test_combine_maps():
