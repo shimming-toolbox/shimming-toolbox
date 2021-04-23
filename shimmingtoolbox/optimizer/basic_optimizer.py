2ae95d82963dce737955bfa8408b155a44183d6d
Alexandre D'Astous <po09i@hotmail.com>
2021-04-22 23:21:33 -0400

    Rafactor to accept Coil class

--- shimmingtoolbox/optimizer/basic_optimizer.py
+++ shimmingtoolbox/optimizer/basic_optimizer.py
@@ -5,6 +5,8 @@ import numpy as np
 import scipy.linalg
 import logging
 
+from shimmingtoolbox.coils.coil import Coil
+
 
 class Optimizer(object):
     """
@@ -13,110 +15,66 @@ class Optimizer(object):
     For basic optimizer, uses unbounded pseudo-inverse.
 
     Attributes:
-        X (int): Amount of pixels in the X direction
-        Y (int): Amount of pixels in the Y direction
-        Z (int): Amount of pixels in the Z direction
-        N (int): Amount of channels in the coil profile
-        coils (numpy.ndarray): (X, Y, Z, N) 4d array of N 3d coil profiles
+        coil (Coil): Coil object containing the coil profiles and related constraints
     """
 
-    def __init__(self, coil_profiles=None):
+    def __init__(self, coil: Coil):
         """
         Initializes X, Y, Z, N and coils according to input coil_profiles
 
         Args:
-            coil_profiles (numpy.ndarray): (X, Y, Z, N) 4d array of N 3d coil profiles
+            coil (Coil): Coil object containing the coil profiles and related constraints
         """
+        # TODO: Change to a list of coils
         # Logging
         self.logger = logging.getLogger()
         logging.basicConfig(filename='test_optimizer.log', filemode='w', level=logging.DEBUG)
 
-        # Load coil profiles (X, Y, Z, N) if given
-        if coil_profiles is None:
-            self.X = 0
-            self.Y = 0
-            self.Z = 0
-            self.N = 0
-            self.coils = None
-        else:
-            self.load_coil_profiles(coil_profiles)
+        self.coil = coil
 
-    # Load coil profiles and check dimensions
-    def load_coil_profiles(self, coil_profiles):
-        """
-        Load new coil profiles into Optimizer
-
-        Args:
-            coil_profiles (numpy.ndarray): (X, Y, Z, N) 4d array of N 3d coil profiles
-        """
-        self._error_if(coil_profiles.ndim != 4,
-                       f"Coil profile has {coil_profiles.ndim} dimensions, expected 4 (X, Y, Z, N)")
-        self.X, self.Y, self.Z, self.N = coil_profiles.shape
-        self.coils = coil_profiles
-
-    def optimize(self, unshimmed, mask, mask_origin=(0, 0, 0), bounds=None):
+    def optimize(self, unshimmed, mask):
         """
         Optimize unshimmed volume by varying current to each channel
 
         Args:
             unshimmed (numpy.ndarray): (X, Y, Z) 3d array of unshimmed volume
             mask (numpy.ndarray): (X, Y, Z) 3d array of integers marking volume for optimization -- 0 indicates unused
-            mask_origin (tuple): Origin of mask if mask volume does not cover unshimmed volume
-            bounds (list): List of ``(min, max)`` pairs for each coil channels. UNUSED in pseudo-inverse
         """
         # Check for sizing errors
-        self._check_sizing(unshimmed, mask, mask_origin=mask_origin, bounds=bounds)
+        self._check_sizing(unshimmed, mask)
 
         # Set up output currents and optimize
-        output = np.zeros(self.N)
+        # output = np.zeros(self.coil.n)
 
-        mask_range = tuple([slice(mask_origin[i], mask_origin[i] + mask.shape[i]) for i in range(3)])
         mask_vec = mask.reshape((-1,))
 
         # Simple pseudo-inverse optimization
         # Reshape coil profile: X, Y, Z, N --> [mask.shape], N
         #   --> N, [mask.shape] --> N, mask.size --> mask.size, N --> masked points, N
-        coil_mat = np.reshape(np.transpose(self.coils[mask_range], axes=(3, 0, 1, 2)),
-                                (self.N, -1)).T[mask_vec != 0, :]  # masked points x N
-        unshimmed_vec = np.reshape(unshimmed[mask_range], (-1,))[mask_vec != 0] # mV'
+        coil_mat = np.reshape(np.transpose(self.coil.profiles, axes=(3, 0, 1, 2)),
+                              (self.coil.n, -1)).T[mask_vec != 0, :]  # masked points x N
+        unshimmed_vec = np.reshape(unshimmed, (-1,))[mask_vec != 0]  # mV'
 
         output = -1 * scipy.linalg.pinv(coil_mat) @ unshimmed_vec  # N x mV' @ mV'
 
         return output
 
-    def _check_sizing(self, unshimmed, mask, mask_origin=(0, 0, 0), bounds=None):
+    def _check_sizing(self, unshimmed, mask):
         """
         Helper function to check array sizing
 
         Args:
             unshimmed (numpy.ndarray): (X, Y, Z) 3d array of unshimmed volume
             mask (numpy.ndarray): (X, Y, Z) 3d array of integers marking volume for optimization -- 0 indicates unused
-            mask_origin (tuple): Origin of mask if mask volume does not cover unshimmed volume
-            bounds (list): List of ``(min, max)`` pairs for each coil channels. None
-               is used to specify no bound.
         """
-        self._error_if(self.coils is None, "No loaded coil profiles!")
-        self._error_if(unshimmed.ndim != 3,
-                       f"Unshimmed profile has {unshimmed.ndim} dimensions, expected 3 (X, Y, Z)")
-        self._error_if(mask.ndim != 3, f"Mask has {mask.ndim} dimensions, expected 3 (X, Y, Z)")
-        self._error_if(unshimmed.shape != (self.X, self.Y, self.Z),
-                       f"XYZ mismatch -- Coils: {self.coils.shape}, Unshimmed: {unshimmed.shape}")
-        for i in range(3):
-            self._error_if(mask.shape[i] + mask_origin[i] > (self.X, self.Y, self.Z)[i],
-                           f"Mask (shape: {mask.shape}, origin: {mask_origin}) goes out of bounds (coil shape: "
-                           f"{(self.X, self.Y, self.Z)}")
-        if bounds is not None:
-            self._error_if(len(bounds) != self.N, f"Bounds should have the same number of (min, max)"
-                                                                     f" tuples as coil channels")
 
-    def _error_if(self, err_condition, message):
-        """
-        Helper function throwing errors
+        if unshimmed.ndim != 3:
+            raise ValueError(f"Unshimmed profile has {unshimmed.ndim} dimensions, expected 3 (X, Y, Z)")
+        if mask.ndim != 3:
+            raise ValueError(f"Mask has {mask.ndim} dimensions, expected 3 (X, Y, Z)")
+        if unshimmed.shape != (self.coil.x, self.coil.y, self.coil.z):
+            raise ValueError("XYZ mismatch -- Coils: {self.coil.profiles.shape}, Unshimmed: {unshimmed.shape}")
 
-        Args:
-            err_condition (bool): Condition to throw error on
-            message (string): Message to log and throw
-        """
-        if err_condition:
-            self.logger.error(message)
-            raise RuntimeError(message)
+        if mask.shape != (self.coil.x, self.coil.y, self.coil.z):
+            raise ValueError(f"Mask with shape: {mask.shape} expected to have the same shape as the coil profiles with"
+                             f" shape: {self.coil.profiles.shape}")
--- shimmingtoolbox/optimizer/lsq_optimizer.py
+++ shimmingtoolbox/optimizer/lsq_optimizer.py
@@ -7,7 +7,7 @@ import scipy.optimize as opt
 from shimmingtoolbox.optimizer.basic_optimizer import Optimizer
 
 
-class LSQ_Optimizer(Optimizer):
+class LsqOptimizer(Optimizer):
 
     def _residuals(self, coef, unshimmed_vec, coil_mat):
         """
@@ -20,46 +20,41 @@ class LSQ_Optimizer(Optimizer):
 
         Returns:
             numpy.ndarray: Residuals for least squares optimization -- equivalent to flattened shimmed vector
-
         """
-        self._error_if(unshimmed_vec.shape[0] != coil_mat.shape[0],
-                       (f'Unshimmed ({unshimmed_vec.shape}) and coil ({coil_mat.shape})'
-                        ' arrays do not align on axis 0'))
+        if unshimmed_vec.shape[0] != coil_mat.shape[0]:
+            ValueError(f"Unshimmed ({unshimmed_vec.shape}) and coil ({coil_mat.shape} arrays do not align on axis 0")
+
         return unshimmed_vec + np.sum(coil_mat * coef, axis=1, keepdims=False)
 
-    def optimize(self, unshimmed, mask, mask_origin=(0, 0, 0), bounds=None):
+    def optimize(self, unshimmed, mask):
         """
         Optimize unshimmed volume by varying current to each channel
 
         Args:
             unshimmed (numpy.ndarray): 3D B0 map
             mask (numpy.ndarray): 3D integer mask used for the optimizer (only consider voxels with non-zero values).
-            mask_origin (tuple): Mask origin if mask volume does not cover unshimmed volume
-            bounds (list): List of ``(min, max)`` pairs for each coil channels. None
-               is used to specify no bound.
-
         Returns:
             numpy.ndarray: Coefficients corresponding to the coil profiles that minimize the objective function
                            (coils.size)
         """
 
         # Check for sizing errors
-        self._check_sizing(unshimmed, mask, mask_origin=mask_origin, bounds=bounds)
+        self._check_sizing(unshimmed, mask)
 
-        mask_range = tuple([slice(mask_origin[i], mask_origin[i] + mask.shape[i]) for i in range(3)])
         mask_vec = mask.reshape((-1,))
 
         # Simple pseudo-inverse optimization
         # Reshape coil profile: X, Y, Z, N --> [mask.shape], N
         #   --> N, [mask.shape] --> N, mask.size --> mask.size, N --> masked points, N
-        coil_mat = np.reshape(np.transpose(self.coils[mask_range], axes=(3, 0, 1, 2)),
-                                (self.N, -1)).T[mask_vec != 0, :]  # masked points x N
-        unshimmed_vec = np.reshape(unshimmed[mask_range], (-1,))[mask_vec != 0]  # mV'
+        coil_mat = np.reshape(np.transpose(self.coil.profiles, axes=(3, 0, 1, 2)),
+                              (self.coil.n, -1)).T[mask_vec != 0, :]  # masked points x N
+        unshimmed_vec = np.reshape(unshimmed, (-1,))[mask_vec != 0]  # mV'
 
         # Set up output currents and optimize
-        currents_0 = np.zeros(self.N)
+        currents_0 = np.zeros(self.coil.n)
         currents_sp = opt.least_squares(self._residuals, currents_0,
-                                        args=(unshimmed_vec, coil_mat), bounds=np.array(bounds).T)
+                                        args=(unshimmed_vec, coil_mat),
+                                        bounds=np.array(self.coil.coef_channel_minmax).T)
 
         currents = currents_sp.x
 
