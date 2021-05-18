cdd45806f49bf9a7915b63c299a529b468a4171c
Alexandre D'Astous <po09i@hotmail.com>
2021-05-18 12:33:12 -0400

    Include .rst

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -51,6 +51,11 @@ autodoc_mock_imports = [
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
 
+# The suffix(es) of source filenames.
+# You can specify multiple suffix as a list of string:
+#
+source_suffix = ['.rst', '.md']
+
 # The master toctree document.
 master_doc = "index"
 
