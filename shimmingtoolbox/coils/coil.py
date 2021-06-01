085de4aa329a0aa4d4c51a77da1688401fa82f90
Alexandre D'Astous <po09i@hotmail.com>
2021-06-01 01:02:12 -0400

    More docstring

--- shimmingtoolbox/coils/coil.py
+++ shimmingtoolbox/coils/coil.py
@@ -30,14 +30,16 @@ class Coil(object):
                 * coef_channel_max (list): List of ``(min, max)`` pairs for each coil channels. None is used to specify
                   no bound.
 
-                ::
+        Examples:
 
-                    # Example
-                    constraints = {
-                        'coef_sum_max': 40,
-                        # 8 channel coil
-                        'coef_channel_minmax': [(-2, 2), (-2, 2), (-2, 2), (-2, 2), (-3, 3), (-3, 3), (-3, 3), (-3, 3)]
-                    }
+            ::
+
+                # Example of constraints
+                constraints = {
+                    'coef_sum_max': 40,
+                    # 8 channel coil
+                    'coef_channel_minmax': [(-2, 2), (-2, 2), (-2, 2), (-2, 2), (-3, 3), (-3, 3), (-3, 3), (-3, 3)]
+                }
         """
 
         self.dim = (np.nan,) * 4
