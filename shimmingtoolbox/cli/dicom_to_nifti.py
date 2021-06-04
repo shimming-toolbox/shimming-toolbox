5a48943531a1e3201ca97526fa62db38192a6236
Alexandre D'Astous <po09i@hotmail.com>
2021-06-04 14:01:07 -0400

    Update option names with _ to - to seperate words

--- shimmingtoolbox/cli/dicom_to_nifti.py
+++ shimmingtoolbox/cli/dicom_to_nifti.py
@@ -18,7 +18,7 @@ CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 @click.option('--subject', required=True, help="Name of the patient")
 @click.option('--config', 'fname_config', type=click.Path(), default=__dir_config_dcm2bids__,
               help="Full file path and name of the bids config file")
-@click.option('--remove_tmp/--dont_remove_tmp', default=False,
+@click.option('--remove-tmp/--dont-remove-tmp', default=False,
               help="Specifies if tmp folder will be deleted after processing")
 def dicom_to_nifti_cli(path_dicoms, path_nifti, subject, fname_config, remove_tmp):
     """Converts dicom files into nifti files by calling ``dcm2bids``."""
