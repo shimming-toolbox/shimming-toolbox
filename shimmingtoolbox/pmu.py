Fix bug in pmu where the calculation of the number of index was off by 1, fix tests and added test to verify

shimmingtoolbox/pmu.py
@@ -135,10 +135,10 @@ class PmuResp(object):
         Returns:
             numpy.ndarray: 1D array with interpolated times
         """
-        if np.any(self.start_time_mdh > acquisition_times) or np.all(self.stop_time_mdh < acquisition_times):
+        if np.any(self.start_time_mdh > acquisition_times) or np.any(self.stop_time_mdh < acquisition_times):
             raise RuntimeError("acquisition_times don't fit within time limits for resp trace")
 
-        raster = float(self.stop_time_mdh - self.start_time_mdh) / len(self.data-1)
+        raster = float(self.stop_time_mdh - self.start_time_mdh) / (len(self.data)-1)
         times = (self.start_time_mdh + raster * np.arange(len(self.data)))  # ms
 
         interp_data = np.interp(acquisition_times, times, self.data)

test/shim/test_realtime_shim.py
@@ -71,9 +71,9 @@ class TestRealtimeShim(object):
                                                  self.json)
 
         assert np.isclose(static_zcorrection[0], 0.1291926595061463,)
-        assert np.isclose(riro_zcorrection[0], -0.00802980555042238)
-        assert np.isclose(mean_p, 1326.3179660207873)
-        assert np.isclose(pressure_rms, 1493.9468284155396)
+        assert np.isclose(riro_zcorrection[0], -0.008084002566817211)
+        assert np.isclose(mean_p, 1326.7410085020922)
+        assert np.isclose(pressure_rms, 1494.6380477845253)
 
     def test_mask(self):
         """Test realtime_shim mask parameter"""
@@ -87,9 +87,9 @@ class TestRealtimeShim(object):
                                                  nii_mask_anat_riro=self.nii_mask_riro)
 
         assert np.isclose(static_zcorrection[0], 0.2766538103967352)
-        assert np.isclose(riro_zcorrection[0], -0.051144561917725075)
-        assert np.isclose(mean_p, 1326.318)
-        assert np.isclose(pressure_rms, 1493.9468284155396)
+        assert np.isclose(riro_zcorrection[0], -0.05118665738437744)
+        assert np.isclose(mean_p, 1326.7410085020922)
+        assert np.isclose(pressure_rms, 1494.6380477845253)
 
     def test_output_figure(self):
         """Test realtime_shim output figures parameter"""