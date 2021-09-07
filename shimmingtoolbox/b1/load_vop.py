fb89429b0c4997e729b7d74e6b4a952a63350ff7
gaspardcereza <gaspard.cereza@gmail.com>
2021-09-07 14:12:12 -0400

    added code to load VOP from matlab files + corresponding tests

--- shimmingtoolbox/b1/load_vop.py
+++ shimmingtoolbox/b1/load_vop.py
@@ -6,12 +6,26 @@ import numpy as np
 import os
 
 
-def load_vop(path_sar_file):
+def load_siemens_vop(path_sar_file):
+    """
+
+    Args:
+        path_sar_file: Path to the 'SarDataUser.mat' file containing the scanner's VOPs. This file should be available
+        at the scanner in 'C:/Medcom/MriProduct/PhysConfig'
+
+    Returns:
+        numpy.ndarray: VOP matrices (n_coils, n_coils, n_VOPs)
+
+    """
     if os.path.exists(path_sar_file):
         sar_data = scipy.io.loadmat(path_sar_file)
     else:
         raise FileNotFoundError('The SarDataUser.mat file could not be found.')
     # Assert file exists
 
+    if 'ZZ' in sar_data:
+        return sar_data['ZZ']
+    else:
+        raise ValueError('The SAR data does not contain the expected VOP values.')
     # Only return VOPs corresponding to 6 (body parts) and 8 (allowed forward power by channel)
-    return sar_data['ZZ'][:, :, np.argwhere(np.logical_or(sar_data['ZZtype'] == 6, sar_data['ZZtype'] == 8))[:, 1]]
+    # return sar_data['ZZ'][:, :, np.argwhere(np.logical_or(sar_data['ZZtype'] == 6, sar_data['ZZtype'] == 8))[:, 1]]
