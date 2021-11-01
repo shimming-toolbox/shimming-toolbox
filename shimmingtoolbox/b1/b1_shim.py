3cf8dd4def3aa4239ca5b838a2ea95f7a3f3923f
gaspardcereza <gaspard.cereza@gmail.com>
2021-11-01 13:04:35 -0400

    Different thresholding for B1 and B0 maps

--- shimmingtoolbox/b1/b1_shim.py
+++ shimmingtoolbox/b1/b1_shim.py
@@ -47,7 +47,7 @@ def b1_shim(b1_maps, mask, cp_weights=None, algorithm=1, target=None,  q_matrix=
 
     if mask is None:
         # If no mask provided, mask = 1 for every pixel where b1_maps values are non-zero
-        mask = threshold(b1_maps.sum(axis=-1), thr=0)
+        mask = threshold(b1_maps.sum(axis=-1), thr=0, b1map=True)
 
     if b1_maps.shape[:-1] == mask.shape:
         # b1_roi will be a (n_pixels, n_channels) numpy array with all zero-valued pixel removed
--- shimmingtoolbox/masking/threshold.py
+++ shimmingtoolbox/masking/threshold.py
@@ -5,15 +5,19 @@ Image thresholding API
 """
 
 
-def threshold(data, thr=30):
+def threshold(data, thr=30, b1map=False):
     """
     Threshold an image
 
     Args:
         data (numpy.ndarray): Data to be masked
-        thr: Value to threshold the data: voxels will be set to zero if their value is equal or less than this threshold
+        thr (float): Value to threshold the data: voxels will be set to zero if their value is equal or less than this threshold
+        b1map(boolean): Specifies if the data are b1 maps (True) or not. Thresholding will be applied accordingly.
 
     Returns:
         numpy.ndarray: Boolean mask with same dimensions as data
     """
-    return abs(data) > thr
+    if b1map:
+        return abs(data) > thr
+    else:
+        return data > thr
