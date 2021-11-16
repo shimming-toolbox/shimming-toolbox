a3cff40b5ad032ea7daf66a845a8a05c19dfae96
Alex Dastous <47249340+po09i@users.noreply.github.com>
2021-11-16 11:19:51 -0500

    gradient realtime shim output coordinate system (#310)
    * Convert voxel x, y, z to freq, phase and slice encode direction
    
    * Add inversion of freq and phase axes
    
    * Add test with manual input
    
    * Change output convention depending on phase encode direction
    
    * Add tests to improve code coverage
    
    * Improve docstring
    
    * Update shell script with proper options
    
    * Apply suggestions from code review
    
    Co-authored-by: Julien Cohen-Adad <jcohen@polymtl.ca>
    
    * More comments from code review
    
    * Remove not necessary test
    
    Co-authored-by: Julien Cohen-Adad <jcohen@polymtl.ca>

--- shimmingtoolbox/cli/realtime_shim.py
+++ shimmingtoolbox/cli/realtime_shim.py
@@ -18,7 +18,7 @@ CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
     context_settings=CONTEXT_SETTINGS,
 )
 @click.option('--fmap', 'fname_fmap', required=True, type=click.Path(),
-              help="B0 fieldmap. This should be a 4d file (4th dimension being time")
+              help="B0 fieldmap in Hertz. This should be a 4d file (4th dimension being time")
 @click.option('--anat', 'fname_anat', type=click.Path(), required=True,
               help="Filename of the anatomical image to apply the correction.")
 @click.option('--resp', 'fname_resp', type=click.Path(), required=True,
@@ -30,14 +30,16 @@ CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
               help="3D nifti file used to define the time varying (i.e. RIRO, Respiration-Induced Resonance Offset) "
                    "spatial region to shim. "
                    "The coordinate system should be the same as ``anat``'s coordinate system.")
-@click.option('-o', '--output', 'fname_output', type=click.Path(), default=os.curdir,
+@click.option('-o', '--output', 'fname_output', type=click.Path(), default=os.path.abspath(os.curdir),
+              show_default=True,
               help="Directory to output gradient text file and figures.")
 def realtime_shim_cli(fname_fmap, fname_mask_anat_static, fname_mask_anat_riro, fname_resp, fname_anat, fname_output):
-    """ Perform realtime z-shimming. This function will generate textfile containing static and dynamic (due to
-    respiration) Gz components based on a fieldmap time series and respiratory trace information obtained from Siemens
-    bellows (PMUresp_signal.resp). An additional multi-gradient echo (MGRE) magnitiude image is used to resample the
-    static and dynamic Gz component maps to match the MGRE image. Lastly the mean Gz values within the ROI are computed
-    for each slice. The mean pressure is also generated in the text file to be used to shim.
+    """ Perform realtime xyz-shimming. This function will generate text files containing static and dynamic (due to
+    respiration) Gx, Gy, Gz components based on a fieldmap time series and respiratory trace information obtained from
+    Siemens bellows (PMUresp_signal.resp). An additional multi-gradient echo (MGRE) magnitude image is used to
+    resample the static and dynamic Gx, Gy, Gz component maps to match the MGRE image. Lastly the mean Gx, Gy, Gz
+    values within the ROI are computed for each slice. The mean pressure is also generated in the text file to be used
+    to shim.
     """
 
     # Load fieldmap
@@ -45,6 +47,11 @@ def realtime_shim_cli(fname_fmap, fname_mask_anat_static, fname_mask_anat_riro, 
 
     # Load anat
     nii_anat = nib.load(fname_anat)
+    dim_info = nii_anat.header.get_dim_info()
+    if dim_info[2] != 2:
+        # Slice must be the 3rd dimension of the file
+        # TODO: Reorient nifti so that the slice is the 3rd dim
+        raise RuntimeError("Slice encode direction must be the 3rd dimension of the NIfTI file.")
 
     # Load static anatomical mask
     if fname_mask_anat_static is not None:
@@ -74,29 +81,86 @@ def realtime_shim_cli(fname_fmap, fname_mask_anat_static, fname_mask_anat_riro, 
                                              nii_mask_anat_riro=nii_mask_anat_riro,
                                              path_output=fname_output)
 
+    # Reorient according to freq_encode, phase_encode and slice encode direction
+    # static
+    corr_static_vox = (static_xcorrection, static_ycorrection, static_zcorrection)
+    freq_static_corr, phase_static_corr, slice_static_corr = [corr_static_vox[dim] for dim in dim_info]
+    # Riro
+    corr_riro_vox = (riro_xcorrection, riro_ycorrection, riro_zcorrection)
+    freq_riro_corr, phase_riro_corr, slice_riro_corr = [corr_riro_vox[dim] for dim in dim_info]
+
+    # To output to the gradient coord system, axes need some inversions. The gradient coordinate system is defined by
+    # the frequency, phase and slice encode directions.
+    # TODO: More thorough tests
+    phase_encode_is_positive = _get_phase_encode_direction_sign(fname_anat)
+    if not phase_encode_is_positive:
+        freq_static_corr = -freq_static_corr
+        phase_static_corr = -phase_static_corr
+        freq_riro_corr = -freq_riro_corr
+        phase_riro_corr = -phase_riro_corr
+
     # Write to a text file
     fname_zcorrections = os.path.join(fname_output, 'zshim_gradients.txt')
     file_gradients = open(fname_zcorrections, 'w')
-    for i_slice in range(static_zcorrection.shape[-1]):
-        file_gradients.write(f'corr_vec[0][{i_slice}]= {static_zcorrection[i_slice]:.6f}\n')
-        file_gradients.write(f'corr_vec[1][{i_slice}]= {riro_zcorrection[i_slice] / pressure_rms:.12f}\n')
+    for i_slice in range(slice_static_corr.shape[-1]):
+        file_gradients.write(f'corr_vec[0][{i_slice}]= {slice_static_corr[i_slice]:.6f}\n')
+        file_gradients.write(f'corr_vec[1][{i_slice}]= {slice_riro_corr[i_slice] / pressure_rms:.12f}\n')
         file_gradients.write(f'corr_vec[2][{i_slice}]= {mean_p:.3f}\n')
     file_gradients.close()
 
     fname_ycorrections = os.path.join(fname_output, 'yshim_gradients.txt')
     file_gradients = open(fname_ycorrections, 'w')
-    for i_slice in range(static_ycorrection.shape[-1]):
-        file_gradients.write(f'corr_vec[0][{i_slice}]= {static_ycorrection[i_slice]:.6f}\n')
-        file_gradients.write(f'corr_vec[1][{i_slice}]= {riro_ycorrection[i_slice] / pressure_rms:.12f}\n')
+    for i_slice in range(phase_static_corr.shape[-1]):
+        file_gradients.write(f'corr_vec[0][{i_slice}]= {phase_static_corr[i_slice]:.6f}\n')
+        file_gradients.write(f'corr_vec[1][{i_slice}]= {phase_riro_corr[i_slice] / pressure_rms:.12f}\n')
         file_gradients.write(f'corr_vec[2][{i_slice}]= {mean_p:.3f}\n')
     file_gradients.close()
 
     fname_xcorrections = os.path.join(fname_output, 'xshim_gradients.txt')
     file_gradients = open(fname_xcorrections, 'w')
-    for i_slice in range(static_xcorrection.shape[-1]):
-        file_gradients.write(f'corr_vec[0][{i_slice}]= {static_xcorrection[i_slice]:.6f}\n')
-        file_gradients.write(f'corr_vec[1][{i_slice}]= {riro_xcorrection[i_slice] / pressure_rms:.12f}\n')
+    for i_slice in range(freq_static_corr.shape[-1]):
+        file_gradients.write(f'corr_vec[0][{i_slice}]= {freq_static_corr[i_slice]:.6f}\n')
+        file_gradients.write(f'corr_vec[1][{i_slice}]= {freq_riro_corr[i_slice] / pressure_rms:.12f}\n')
         file_gradients.write(f'corr_vec[2][{i_slice}]= {mean_p:.3f}\n')
     file_gradients.close()
 
     return fname_xcorrections, fname_ycorrections, fname_zcorrections
+
+
+def _get_phase_encode_direction_sign(fname_nii):
+    """ Returns the phase encode direction sign
+
+    Args:
+        fname_nii (str): Filename to a NIfTI file with its corresponding json file.
+
+    Returns:
+        bool: Returns whether the encoding direction is positive (True) or negative (False)
+    """
+
+    # Load nibabel
+    nii = nib.load(fname_nii)
+    dim_info = nii.header.get_dim_info()
+
+    # Load json
+    fname_json = fname_nii.rsplit('.nii', 1)[0] + '.json'
+    with open(fname_json) as json_file:
+        json_data = json.load(json_file)
+
+    # json_data['PhaseEncodingDirection'] contains i, j or k then a '-' if the direction is reversed
+    phase_en_dir = json_data['PhaseEncodingDirection']
+
+    # Check that dim_info is consistent with PhaseEncodingDirection tag i --> 0, j --> 1, k --> 2
+    if (phase_en_dir[0] == 'i' and dim_info[1] != 0) \
+            or (phase_en_dir[0] == 'j' and dim_info[1] != 1) \
+            or (phase_en_dir[0] == 'k' and dim_info[1] != 2):
+        raise RuntimeError("Inconsistency between dim_info of fieldmap and PhaseEncodeDirection tag in the json")
+
+    # Find if the phase encode direction is negative or positive
+    if len(phase_en_dir) == 2 and phase_en_dir[1] == '-':
+        en_is_positive = False
+    elif len(phase_en_dir) == 1:
+        en_is_positive = True
+    else:
+        raise ValueError(f"Unexpected value for PhaseEncodingDirection: {phase_en_dir}")
+
+    return en_is_positive
--- test/cli/test_cli_realtime_shim.py
+++ test/cli/test_cli_realtime_shim.py
@@ -1,16 +1,20 @@
 #!usr/bin/env python3
 # coding: utf-8
-
+import copy
 import os
 import pathlib
 import tempfile
 import nibabel as nib
 import numpy as np
+import pytest
+import json
+
 
 from click.testing import CliRunner
 from shimmingtoolbox.cli.realtime_shim import realtime_shim_cli
 from shimmingtoolbox.masking.shapes import shapes
 from shimmingtoolbox import __dir_testing__
+from shimmingtoolbox.cli.realtime_shim import _get_phase_encode_direction_sign
 
 
 def test_cli_realtime_shim():
@@ -18,7 +22,6 @@ def test_cli_realtime_shim():
     with tempfile.TemporaryDirectory(prefix='st_' + pathlib.Path(__file__).stem) as tmp:
         runner = CliRunner()
 
-        # Local
         fname_fieldmap = os.path.join(__dir_testing__, 'realtime_zshimming_data', 'nifti', 'sub-example', 'fmap',
                                       'sub-example_fieldmap.nii.gz')
 
@@ -67,3 +70,86 @@ def test_cli_realtime_shim():
                                catch_exceptions=False)
         assert len(os.listdir(path_output)) != 0
         assert result.exit_code == 0
+
+
+def test_cli_realtime_shim_no_mask():
+    runner = CliRunner()
+    with tempfile.TemporaryDirectory(prefix='st_' + pathlib.Path(__file__).stem) as tmp:
+
+        # Path for fieldmap
+        fname_fieldmap = os.path.join(__dir_testing__, 'realtime_zshimming_data', 'nifti', 'sub-example', 'fmap',
+                                      'sub-example_fieldmap.nii.gz')
+        # Path for mag anat image
+        fname_anat = os.path.join(__dir_testing__, 'realtime_zshimming_data', 'nifti', 'sub-example', 'anat',
+                                  'sub-example_unshimmed_e1.nii.gz')
+        # Path for resp data
+        fname_resp = os.path.join(__dir_testing__, 'realtime_zshimming_data', 'PMUresp_signal.resp')
+        # Specify output for text file and figures
+        path_output = os.path.join(tmp, 'test_realtime_shim')
+
+        # Run the CLI
+        result = runner.invoke(realtime_shim_cli, ['--fmap', fname_fieldmap,
+                                                   '--output', path_output,
+                                                   '--resp', fname_resp,
+                                                   '--anat', fname_anat],
+                               catch_exceptions=False)
+
+        assert len(os.listdir(path_output)) != 0
+        assert result.exit_code == 0
+
+
+def test_phase_encode_sign():
+    # Using this acquisition because it has a positive phase encode direction
+    fname_nii = os.path.join(__dir_testing__, 'realtime_zshimming_data', 'nifti', 'sub-example', 'fmap',
+                             'sub-example_phasediff.nii.gz')
+    phase_encode_is_positive = _get_phase_encode_direction_sign(fname_nii)
+
+    assert phase_encode_is_positive is True
+
+
+def test_phase_encode_wrong_dim():
+    with tempfile.TemporaryDirectory(prefix='st_' + pathlib.Path(__file__).stem) as tmp:
+        fname_anat = os.path.join(__dir_testing__, 'realtime_zshimming_data', 'nifti', 'sub-example', 'anat',
+                                  'sub-example_unshimmed_e1.nii.gz')
+        fname_json = os.path.join(__dir_testing__, 'realtime_zshimming_data', 'nifti', 'sub-example', 'anat',
+                                  'sub-example_unshimmed_e1.json')
+
+        nii = nib.load(fname_anat)
+        with open(fname_json) as json_file:
+            json_data = json.load(json_file)
+
+        json_data['PhaseEncodingDirection'] = "i"
+        fname_new_json = os.path.join(tmp, "anat.json")
+        with open(fname_new_json, 'w', encoding='utf-8') as f:
+            json.dump(json_data, f, indent=4)
+
+        fname_nii = os.path.join(tmp, 'anat.nii.gz')
+        nib.save(nii, fname_nii)
+
+        with pytest.raises(RuntimeError,
+                           match="Inconsistency between dim_info of fieldmap and PhaseEncodeDirection tag in the json"):
+            _get_phase_encode_direction_sign(fname_nii)
+
+
+def test_phase_encode_wrong_tag_value():
+    with tempfile.TemporaryDirectory(prefix='st_' + pathlib.Path(__file__).stem) as tmp:
+        fname_anat = os.path.join(__dir_testing__, 'realtime_zshimming_data', 'nifti', 'sub-example', 'anat',
+                                  'sub-example_unshimmed_e1.nii.gz')
+        fname_json = os.path.join(__dir_testing__, 'realtime_zshimming_data', 'nifti', 'sub-example', 'anat',
+                                  'sub-example_unshimmed_e1.json')
+
+        nii = nib.load(fname_anat)
+        with open(fname_json) as json_file:
+            json_data = json.load(json_file)
+
+        json_data['PhaseEncodingDirection'] = "j----"
+        fname_new_json = os.path.join(tmp, "anat.json")
+        with open(fname_new_json, 'w', encoding='utf-8') as f:
+            json.dump(json_data, f, indent=4)
+
+        fname_nii = os.path.join(tmp, 'anat.nii.gz')
+        nib.save(nii, fname_nii)
+
+        with pytest.raises(ValueError,
+                           match="Unexpected value for PhaseEncodingDirection:"):
+            _get_phase_encode_direction_sign(fname_nii)
