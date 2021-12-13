aece4c663ca152742b4dc43939aef46ff2e9b213
Alexandre D'Astous <po09i@hotmail.com>
2021-12-13 15:55:55 -0500

    Created function to convert from physical x, y, z to gradient CS

--- shimmingtoolbox/shim/shim_utils.py
+++ shimmingtoolbox/shim/shim_utils.py
@@ -6,6 +6,8 @@ This file includes utility functions useful for the shimming module
 import nibabel as nib
 import json
 
+from shimmingtoolbox.coils.coordinates import phys_to_vox_coefs, get_main_orientation
+
 
 def get_phase_encode_direction_sign(fname_nii):
     """ Returns the phase encode direction sign
@@ -44,3 +46,65 @@ def get_phase_encode_direction_sign(fname_nii):
         raise ValueError(f"Unexpected value for PhaseEncodingDirection: {phase_en_dir}")
 
     return en_is_positive
+
+
+def phys_to_gradient_cs(coefs_x, coefs_y, coefs_z, fname_anat):
+    """ Converts physical coefficients (x, y, z from RAS Coordinate System) to Siemens Gradient Coordinate System
+
+    Args:
+        coefs_x (numpy.ndarray): Array containing x coefficients in the physical coordinate system RAS
+        coefs_y (numpy.ndarray): Array containing y coefficients in the physical coordinate system RAS
+        coefs_z (numpy.ndarray): Array containing z coefficients in the physical coordinate system RAS
+        fname_anat (str): Filename of the NIfTI file to convert the data to that Gradient CS
+
+    Returns:
+        (tuple): tuple containing:
+            * numpy.ndarray: Array containing the data in the gradient CS (frequency/readout)
+            * numpy.ndarray: Array containing the data in the gradient CS (phase)
+            * numpy.ndarray: Array containing the data in the gradient CS (slice)
+
+    """
+    # Load anat
+    nii_anat = nib.load(fname_anat)
+
+    # Convert from patient coordinates to image coordinates
+    scanner_coil_coef_vox = phys_to_vox_coefs(coefs_x, coefs_y, coefs_z, nii_anat.affine)
+    # scanner_coil_coef_vox[0]  # NIfTI dim1, etc
+
+    # Convert from image to freq, phase, slice encoding direction
+    dim_info = nii_anat.header.get_dim_info()
+    coefs_freq, coefs_phase, coefs_slice = [scanner_coil_coef_vox[dim] for dim in dim_info]
+
+    # To output to the gradient coord system, axes need some inversions. The gradient coordinate system is
+    # defined by the frequency, phase and slice encode directions.
+    # TODO: More tests, validated for TRA, SAG, COR, no-flip/flipped PE, no rotation
+
+    # Load anat json
+    fname_anat_json = fname_anat.rsplit('.nii', 1)[0] + '.json'
+    with open(fname_anat_json) as json_file:
+        json_anat_data = json.load(json_file)
+
+    if 'ImageOrientationText' in json_anat_data:
+        # Tag in private dicom header (0051,100E) indicates the slice orientation, if it exists, it will appear
+        # in the json under 'ImageOrientationText' tag
+        orientation_text = json_anat_data['ImageOrientationText']
+        orientation = orientation_text[:3].upper()
+    else:
+        # Find orientation with the ImageOrientationPatientDICOM tag, this is less reliable since it can fail
+        # if there are 2 highest cosines. It will raise an exception if there is a problem
+        orientation = get_main_orientation(json_anat_data['ImageOrientationPatientDICOM'])
+
+    if orientation == 'SAG':
+        coefs_slice = -coefs_slice
+    elif orientation == 'COR':
+        coefs_freq = -coefs_freq
+    else:
+        # TRA
+        pass
+
+    phase_encode_is_positive = get_phase_encode_direction_sign(fname_anat)
+    if not phase_encode_is_positive:
+        coefs_freq = -coefs_freq
+        coefs_phase = -coefs_phase
+
+    return coefs_freq, coefs_phase, coefs_slice
