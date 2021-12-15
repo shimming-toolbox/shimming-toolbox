4a8dae630a8195dbd87abb274c89a57ca586ccb5
gaspardcereza <gaspard.cereza@gmail.com>
2021-12-15 15:33:18 -0500

    Clean up B1 shim CLI help

--- shimmingtoolbox/cli/b1shim.py
+++ shimmingtoolbox/cli/b1shim.py
@@ -16,25 +16,23 @@ CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
 @click.command(context_settings=CONTEXT_SETTINGS)
 @click.option('--b1map', 'fname_b1_map', required=True, type=click.Path(exists=True),
-              help="Path to B1 nifti as returned by st_dicom_to_nifti.")
-@click.option('--mask', 'fname_mask', type=click.Path(exists=True), required=False,
-              help="3D nifti file used to define the static spatial region to shim. The coordinate system should be the"
-                   " same as ``b1map``'s coordinate system.")
+              help="Complex 3D B1+ map.")
+@click.option('--mask', 'fname_mask', type=click.Path(exists=True), required=False, help="3D boolean mask.")
 @click.option('--algo', 'algorithm', type=int, default=1, show_default=True,
-              help="Number from 1 to 3 specifying which algorithm to use for B1 optimization.\n"
-                   "1 - Optimization aiming to reduce the coefficient of variation (CoV) of the resulting B1+ field.\n"
-                   "2 - Magnitude least square optimization targeting a specific B1+ value. Target value required.\n"
-                   "3 - Maximizes the minimum B1+ value for better efficiency.")
-@click.option('--target', 'target', type=float, required=False, help="Target B1+ value used by algorithm 2 in nT/V.")
+              help="Number specifying the B1+ shimming algorithm:\n"
+                   "1 - Reduce B1+ coefficient of variation.\n"
+                   "2 - Target a specified B1+ value. Target value required.\n"
+                   "3 - Maximize minimum B1+ for higher signal.")
+@click.option('--target', 'target', type=float, required=False, help="B1+ value (nT/V) targeted by algorithm 2.")
 @click.option('--vop', 'fname_vop', type=click.Path(exists=True), required=False,
-              help="Path to the SarDataUser.mat file containing the VOP used for SAR constraint. This file can be found"
-                   " on the scanner in C:/MedcomMriProduct/PhysConfig.")
+              help="SarDataUser.mat file containing VOP matrices used for SAR constraint. Found on the scanner in "
+                   "C:/Medcom/MriProduct/PhysConfig.")
 @click.option('--sed', 'sed', type=float, required=False, default=1.5,
-              help="Factor (=> 1) to which the local SAR after optimization can exceed the CP mode local SAR."
-                   "SED between 1 and 1.5 usually work with Siemens scanners. Higher SED allows more liberty for RF"
-                   "shimming but might result in SAR excess at the scanner.")
+              help="Factor (=> 1) to which the shimmed max local SAR can exceed the phase-only shimming max local SAR."
+                   "SED between 1 and 1.5 usually work with Siemens scanners. High SED allows more RF shimming liberty "
+                   "but is more likely to result in SAR excess at the scanner.")
 @click.option('-o', '--output', 'path_output', type=click.Path(), default=os.path.join(os.curdir, 'b1_shim_results'),
-              show_default=True, help="Directory to output shim weights text file and figures.")
+              show_default=True, help="Output directory for shim weights, B1+ maps and figures.")
 def b1shim_cli(fname_b1_map, fname_mask, algorithm, target, fname_vop, sed, path_output):
     """ Perform static RF shimming over the volume defined by the mask. This function will generate a text file
     containing shim weights for each transmit element.
