a6fa292de74cac65dc3e23b75ece597edfaab9d3
Alex Dastous <47249340+po09i@users.noreply.github.com>
2022-01-27 15:25:20 -0500

    Correct for possible 2*pi offsets across time points when unwrapping (#330)
    * Update to fstrings
    
    * Add function to remove n*2pi offsets, make mag a required argument
    
    * Update tests according to mag being required and add default threshold
    
    * Add option to change verbose
    
    * Fix typos
    
    * Add standalone test for the correction of 2pi offset function

--- test/test_prepare_fieldmap.py
+++ test/test_prepare_fieldmap.py
@@ -9,6 +9,8 @@ import numpy as np
 
 from shimmingtoolbox import __dir_testing__
 from shimmingtoolbox.prepare_fieldmap import prepare_fieldmap
+from shimmingtoolbox.prepare_fieldmap import correct_2pi_offset
+from shimmingtoolbox.masking.threshold import threshold
 
 
 @pytest.mark.prelude
@@ -25,18 +27,12 @@ class TestPrepareFieldmap(object):
 
     def test_prepare_fieldmap_1_echo(self):
         """Test default works."""
-        fieldmap = prepare_fieldmap([self.nii_phase], self.echo_times)
+        fieldmap = prepare_fieldmap([self.nii_phase], self.echo_times, self.mag)
 
         assert fieldmap.shape == self.nii_phase.shape
         # If the behaviour of the called function is modified, this assertion below should capture it:
         assert np.all(np.isclose(fieldmap[30:35, 40, 0, 0],
                                  np.array([18.51407573, 13.85066883,  9.47872498,  5.11298149,  0.64801652])))
-
-    def test_prepare_fieldmap_with_mag(self):
-        """Test mag works."""
-        fieldmap = prepare_fieldmap([self.nii_phase], self.echo_times, mag=self.mag)
-
-        assert fieldmap.shape == self.nii_phase.shape
 
     def test_prepare_fieldmap_2_echoes(self):
         """Test 2 echoes works."""
@@ -49,7 +45,7 @@ class TestPrepareFieldmap(object):
         fname_phase2 = os.path.join(__dir_testing__, 'ds_b0', 'sub-fieldmap', 'fmap', 'sub-fieldmap_phase2.nii.gz')
         nii_phase2 = nib.load(fname_phase2)
         phase2 = (nii_phase2.get_fdata() * 2 * math.pi / 4095) - math.pi
-        nii_phase1_re = nib.Nifti1Image(phase2, nii_phase2.affine, header=nii_phase2.header)
+        nii_phase2_re = nib.Nifti1Image(phase2, nii_phase2.affine, header=nii_phase2.header)
 
         # Load mag data to speed it prelude
         fname_mag = os.path.join(__dir_testing__, 'ds_b0', 'sub-fieldmap', 'fmap', 'sub-fieldmap_magnitude1.nii.gz')
@@ -57,7 +53,7 @@ class TestPrepareFieldmap(object):
 
         echo_times = [0.0025, 0.0055]
 
-        fieldmap = prepare_fieldmap([nii_phase1_re, nii_phase1_re], echo_times, mag=mag)
+        fieldmap = prepare_fieldmap([nii_phase1_re, nii_phase2_re], echo_times, mag=mag)
 
         assert fieldmap.shape == phase1.shape
 
@@ -66,7 +62,7 @@ class TestPrepareFieldmap(object):
         """Test error when range is not between -pi and pi."""
         nii = nib.Nifti1Image(self.nii_phase.get_fdata() - math.pi, self.nii_phase.affine, header=self.nii_phase.header)
         with pytest.raises(ValueError, match="Values must range from -pi to pi."):
-            fieldmap = prepare_fieldmap([nii], self.echo_times)
+            prepare_fieldmap([nii], self.echo_times, self.mag)
 
     def test_prepare_fieldmap_wrong_echo_times(self):
         """Wrong number of echo times."""
@@ -74,7 +70,7 @@ class TestPrepareFieldmap(object):
         echo_times = [0.001, 0.002, 0.003]
         with pytest.raises(ValueError, match="The number of echoes must match the number of echo times unless there is "
                                              "1 echo"):
-            prepare_fieldmap([self.nii_phase], echo_times)
+            prepare_fieldmap([self.nii_phase], echo_times, self.mag)
 
     def test_prepare_fieldmap_mag_wrong_shape(self):
         """Mag has the wrong shape."""
@@ -86,14 +82,14 @@ class TestPrepareFieldmap(object):
         """Mask has the wrong shape."""
 
         with pytest.raises(ValueError, match="Shape of mask and phase must match."):
-            prepare_fieldmap([self.nii_phase], self.echo_times, mask=np.zeros_like([5, 5]))
+            prepare_fieldmap([self.nii_phase], self.echo_times, self.mag, mask=np.zeros_like([5, 5]))
 
     def test_prepare_fieldmap_phasediff_1_echotime(self):
         """EchoTime of length one for phasediff should fail."""
 
         with pytest.raises(ValueError, match="The number of echoes must match the number of echo times unless there is "
                                              "1 echo"):
-            prepare_fieldmap([self.nii_phase], [self.echo_times[0]])
+            prepare_fieldmap([self.nii_phase], [self.echo_times[0]], self.mag)
 
     def test_prepare_fieldmap_3_echoes(self):
         """3 echoes are not implemented so the test should fail."""
@@ -101,14 +97,36 @@ class TestPrepareFieldmap(object):
         echo_times = [0.001, 0.002, 0.003]
 
         with pytest.raises(NotImplementedError, match="This number of phase input is not supported:"):
-            prepare_fieldmap([self.nii_phase, self.nii_phase, self.nii_phase], echo_times)
+            prepare_fieldmap([self.nii_phase, self.nii_phase, self.nii_phase], echo_times, self.mag)
+
+    def test_prepare_fieldmap_wrong_threshold(self):
+        """EchoTime of length one for phasediff should fail."""
+
+        with pytest.raises(ValueError, match="Threshold should range from 0 to 1. Input value was:"):
+            prepare_fieldmap([self.nii_phase], self.echo_times, self.mag, threshold=2)
 
     def test_prepare_fieldmap_gaussian_filter(self):
-        """ Test output of gaussian filter optional argument"""
+        """Test output of gaussian filter optional argument"""
 
-        fieldmap = prepare_fieldmap([self.nii_phase], self.echo_times, gaussian_filter=True, sigma=1)
+        fieldmap = prepare_fieldmap([self.nii_phase], self.echo_times, self.mag, gaussian_filter=True, sigma=1)
 
         assert fieldmap.shape == self.nii_phase.shape
         # If the behaviour of the called function is modified, this assertion below should capture it:
         assert np.all(np.isclose(fieldmap[30:35, 40, 0, 0],
-                                 np.array([19.46321364, 15.46275223, 11.0505227 ,  6.28134902,  1.30906534])))
+                                 np.array([19.46321364, 15.46275223, 11.0505227,  6.28134902,  1.30906534])))
+
+
+def test_correct_2pi_offset():
+    """Test if it corrects a 2pi offset"""
+    fname_mag = os.path.join(__dir_testing__, 'ds_b0', 'sub-realtime', 'fmap', 'sub-realtime_magnitude1.nii.gz')
+    mag = nib.load(fname_mag).get_fdata()
+    fmap = np.zeros_like(mag)
+
+    # Add 2pi to one timepoint
+    mask = threshold(mag)
+    fmap_rad_with_offset = fmap
+    fmap_rad_with_offset[..., 2] += mask[..., 2] * 2 * np.pi
+
+    fmap_corrected = correct_2pi_offset(fmap, mag, mask, validity_threshold=0.2)
+
+    assert np.all(np.isclose(fmap_corrected, fmap))
