6134f8ecc9caba9aea560f952c478aa40e1e68f4
Alexandre D'Astous <po09i@hotmail.com>
2022-02-04 13:37:47 -0500

    Fix bug, add logging

--- shimmingtoolbox/cli/create_coil_profiles.py
+++ shimmingtoolbox/cli/create_coil_profiles.py
@@ -10,11 +10,12 @@ import numpy as np
 
 from shimmingtoolbox.coils.create_coil_profiles import create_coil_profiles
 from shimmingtoolbox.cli.prepare_fieldmap import prepare_fieldmap_uncli
-from shimmingtoolbox.utils import create_output_dir, save_nii_json
+from shimmingtoolbox.utils import create_output_dir, save_nii_json, set_all_loggers
 from shimmingtoolbox.masking.threshold import threshold as mask_threshold
 
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
+logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
@@ -35,10 +36,13 @@ logger = logging.getLogger(__name__)
 @click.option('-o', '--output', 'fname_output', type=click.Path(), required=False,
               default=os.path.join(os.path.curdir, 'coil_profiles.nii.gz'),
               help="Output path filename of coil profile nifti file. Supported types : '.nii', '.nii.gz'")
-def create_coil_profiles_cli(fname_json, autoscale, unwrapper, threshold, gaussian_filter, sigma, fname_output):
+@click.option('-v', '--verbose', type=click.Choice(['info', 'debug']), default='info', help="Be more verbose")
+def create_coil_profiles_cli(fname_json, autoscale, unwrapper, threshold, gaussian_filter, sigma, fname_output,
+                             verbose):
     """Create b0 coil profiles from acquisitions defined in the input json file"""
 
-    # Todo: Set logger level according to --log level. Remove line about basic config
+    # Set logger level
+    set_all_loggers(verbose)
 
     # Config file is set up:
     # phase[i_channel][min_max][i_echo]
@@ -73,8 +77,8 @@ def create_coil_profiles_cli(fname_json, autoscale, unwrapper, threshold, gaussi
     mask = np.full_like(nii_mag.get_fdata(), True, bool)
     for i_channel in range(n_channels):
         # Calculate the average mag image for a channel using all echoes for both min and max currents
-        mag_min_mean = np.zeros_like(nii_mag.get_fdata)
-        mag_max_mean = np.zeros_like(nii_mag.get_fdata)
+        mag_min_mean = np.zeros_like(nii_mag.get_fdata())
+        mag_max_mean = np.zeros_like(nii_mag.get_fdata())
         for i_echo in range(n_echoes):
             # min
             fname_mag = mags[i_channel][0][i_echo]
@@ -85,8 +89,8 @@ def create_coil_profiles_cli(fname_json, autoscale, unwrapper, threshold, gaussi
             mag = nib.load(fname_mag).get_fdata()
             mag_max_mean += mag
 
-        mag_min_mean /= n_channels
-        mag_max_mean /= n_channels
+        mag_min_mean /= n_echoes
+        mag_max_mean /= n_echoes
 
         # Calculate threshold for min current
         thresh_mask = mask_threshold(mag_min_mean, threshold)
