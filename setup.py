cdc277a1063af56f21cb0853d93bd17881253646
Alex Dastous <47249340+po09i@users.noreply.github.com>
2022-02-10 13:15:32 -0500

    Update nibabel to 3.2.1 (#337)

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
         'importlib-metadata ~= 1.0 ; python_version < "3.8"',
         "numpy>=1.21",
         "phantominator~=0.6.4",
-        "nibabel~=3.1.1",
+        "nibabel~=3.2.1",
         "requests",
         "scipy~=1.6.0",
         "tqdm",
