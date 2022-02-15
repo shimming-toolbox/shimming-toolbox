8a3436ce02a32e9d46870364ecc573edac7cedef
Gaspard Cereza <54723104+gaspardcereza@users.noreply.github.com>
2022-02-15 15:28:56 -0500

    Update st_dicom_to_nifti help (#338)

--- shimmingtoolbox/cli/dicom_to_nifti.py
+++ shimmingtoolbox/cli/dicom_to_nifti.py
@@ -10,18 +10,15 @@ from shimmingtoolbox import __dir_config_dcm2bids__
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
 
-@click.command(
-    context_settings=CONTEXT_SETTINGS,
-)
-@click.option('-i', '--input', 'path_dicom', type=click.Path(), required=True, help="Input path of dicom folder")
-@click.option('-o', '--output', 'path_nifti', type=click.Path(), default=os.curdir, help="Output path for niftis")
-@click.option('--subject', required=True, help="Name of the patient")
-@click.option('--config', 'fname_config', type=click.Path(), default=__dir_config_dcm2bids__,
-              help="Full file path and name of the bids config file")
-@click.option('--remove-tmp/--dont-remove-tmp', default=False,
-              help="Specifies if tmp folder will be deleted after processing")
-def dicom_to_nifti_cli(path_dicom, path_nifti, subject, fname_config, remove_tmp):
-    """Converts dicom files into nifti files by calling ``dcm2bids``."""
+@click.command(context_settings=CONTEXT_SETTINGS)
+@click.option('-i', '--input', 'path_dicom', type=click.Path(), required=True, help="Input path to DICOM folder")
+@click.option('--subject', required=True, help="Name of the imaged subject")
+@click.option('-o', '--output', 'path_nifti', type=click.Path(), default=os.curdir, help="Output path to BIDS folder")
+@click.option('--config', 'fname_config', type=click.Path(), default=__dir_config_dcm2bids__, show_default=True,
+              help="Path to dcm2bids config file")
+@click.option('--rm-tmp', 'remove_tmp', is_flag=True, help="Remove tmp folder")
+def dicom_to_nifti_cli(path_dicom, subject, path_nifti, fname_config, remove_tmp):
+    """Converts DICOM files into NIfTI files by calling ``dcm2bids``."""
 
     dicom_to_nifti(path_dicom=path_dicom,
                    path_nifti=path_nifti,
