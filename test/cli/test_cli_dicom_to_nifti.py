6a111e6bb0e00f9a8750602e493e7c8ae72538f3
gaspardcereza <gaspard.cereza@gmail.com>
2022-02-15 15:12:14 -0500

    Add catch exception false to cli test

--- test/cli/test_cli_dicom_to_nifti.py
+++ test/cli/test_cli_dicom_to_nifti.py
@@ -19,7 +19,8 @@ def test_cli_dicom_to_nifti(test_dcm2niix_installation):
         path_dicoms = os.path.join(__dir_testing__, 'dicom_unsorted')
         path_nifti = os.path.join(tmp, 'nifti')
         subject_id = 'sub-test'
-        result = runner.invoke(dicom_to_nifti_cli, ['-i', path_dicoms, '-o', path_nifti, '--subject', subject_id])
+        result = runner.invoke(dicom_to_nifti_cli, ['-i', path_dicoms, '-o', path_nifti, '--subject', subject_id],
+                               catch_exceptions=False)
 
         assert result.exit_code == 0
         # Check that dicom_to_nifti was invoked, not if files were actually created (API test already looks for that)
