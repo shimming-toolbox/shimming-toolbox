f382ef31a1028374731a05a1fe9a800624d291c5
Alexandre D'Astous <po09i@hotmail.com>
2023-01-05 18:19:42 -0500

    Implement a way to correct for 2pi offset between currents + improvements

--- shimmingtoolbox/cli/create_coil_profiles.py
+++ shimmingtoolbox/cli/create_coil_profiles.py
@@ -45,21 +45,13 @@ def create_coil_profiles_cli(fname_json, autoscale, unwrapper, threshold, gaussi
     set_all_loggers(verbose)
 
     # Config file is set up:
-    # phase[i_channel][min_max][i_echo]
-    # mag[i_channel][min_max][i_echo]
-    #     "setup_currents": [
-    #     [-0.5, 0.5],
-    #     [-0.5, 0.5],
-    #     [-0.5, 0.5],
-    #     [-0.5, 0.5],
-    #     [-0.5, 0.5],
-    #     [-0.5, 0.5],
-    #     [-0.5, 0.5]
-    #   ],
-    #   "name": "greg_coil",
+    #     "phase": [i_channel][i_currents][i_echo],
+    #     "mag": [i_channel][i_currents][i_echo],
+    #     "setup_currents": [i_channel][i_currents],
+    #   "name": "Awesome_coil",
     #   "n_channels": 7,
     #   "units": "A",
-    #   "coef_channel_minmax": [i_channel][min_max]
+    #   "coef_channel_minmax": [i_channel][2]
     #   "coef_sum_max": null
 
     # Get directory
@@ -76,15 +68,22 @@ def create_coil_profiles_cli(fname_json, autoscale, unwrapper, threshold, gaussi
     list_setup_currents = json_data["setup_currents"]
     n_channels = json_data["n_channels"]
 
+    # Find dead channels (mag or phase is not filled)
     dead_channels = []
     for i_channel in range(n_channels):
-        if not phases[i_channel][0]:
+        if not phases[i_channel][0] or not mags[i_channel][0]:
             dead_channels.append(i_channel)
             continue
 
+    if len(dead_channels) == n_channels:
+        raise ValueError("All channels are empty. Verify input.")
+
     # Create a mask containing the threshold of all channels and currents
     fname_mask = os.path.join(path_output, 'mask.nii.gz')
-    fname_mag = mags[0][0][0]
+    for i_channel in range(n_channels):
+        if i_channel not in dead_channels:
+            fname_mag = mags[i_channel][0][0]
+            break
     nii_mag = nib.load(fname_mag)
     mask = np.full_like(nii_mag.get_fdata(), True, bool)
 
@@ -119,40 +118,109 @@ def create_coil_profiles_cli(fname_json, autoscale, unwrapper, threshold, gaussi
     nii_mask = nib.Nifti1Image(mask.astype(int), nii_mag.affine, header=nii_mag.header)
     nib.save(nii_mask, fname_mask)
 
-    # In 4d
-    nii_mask_4d = nib.Nifti1Image(np.repeat(mask.astype(int)[..., np.newaxis], n_currents, -1), nii_mag.affine,
-                                  header=nii_mag.header)
-    fname_mask_4d = os.path.join(path_output, f"mask_4d.nii.gz")
-    nib.save(nii_mask_4d, fname_mask_4d)
-
     if not dead_channels:
-        logger.warning(f"Channels: {dead_channels} do not have phase data. They will be set to 0.")
+        logger.warning(f"Channels: {dead_channels} do(es) not have phase or magnitude data. They will be set to 0.")
 
+    # Calculate fieldmaps
     fnames_fmap = []
     index_channel = -1
-    # For each channel
     for i_channel in range(n_channels):
         if i_channel in dead_channels:
             continue
-
         # Keeps track of the index since there could be dead channels
         index_channel += 1
 
+        # Unwrapping each current individually leads to n*2pi offset between currents. This induces an error in the
+        # linear regression in the next steps. To avoid this, we can correct the 2npi offset by assuming that the
+        # difference in current does not induce more than 2pi dephasing on average in the mask (ROI). In the dataset
+        # tested, the maximum offset for 0.75 amps was observed to be n=0.03. This leaves plenty of room for more
+        # difference in current.
+        # Note: The choice of reference fieldmap is not important since we are only interested in the slope and we
+        # disregard the intercept and assume it is 0. (0 current should produce no field).
+        # To implement the above mentioned method, we feed un fieldmapping pipeine the currents as if they were
+        # timepoints along the 4th dimension.
+
+        # Repeat the mask along 4th dimension
         n_currents = len(phases[i_channel])
-        # Method 1: Unwrap each current individually
-        # for each current
+        nii_mask_4d = nib.Nifti1Image(np.repeat(mask.astype(int)[..., np.newaxis], n_currents, -1), nii_mag.affine,
+                                      header=nii_mag.header)
+        fname_mask_4d = os.path.join(path_output, f"mask_4d.nii.gz")
+        nib.save(nii_mask_4d, fname_mask_4d)
+
+        # Merge the currents along the 4th dimension for the phase and magnitude of each echo
+        n_echoes = len(phases[i_channel][0])
+        list_fname_phase_4d = []
+        for i_echo in range(n_echoes):
+            phase_tmp = np.zeros(nii_mag.shape + (n_currents,))
+            for i_current in range(n_currents):
+                nii_phase_tmp = nib.load(phases[i_channel][i_current][i_echo])
+                phase_tmp[..., i_current] = nii_phase_tmp.get_fdata()
+
+            nii_phase_4d = nib.Nifti1Image(phase_tmp, nii_phase_tmp.affine, header=nii_phase_tmp.header)
+            fname_4d = os.path.join(path_output, f"phase_channel{i_channel}_echo{i_echo}_4d.nii.gz")
+            list_fname_phase_4d.append(fname_4d)
+            nib.save(nii_phase_4d, fname_4d)
+
+            # Save the json file of the 4d phase
+            fname_json_4d = fname_4d.rsplit('.nii', 1)[0] + '.json'
+            fname_json_3d = phases[i_channel][0][i_echo].rsplit('.nii', 1)[0] + '.json'
+            # Open and save as new name
+            with open(fname_json_3d) as json_file:
+                json_data_3d = json.load(json_file)
+            with open(fname_json_4d, mode='w') as f:
+                json.dump(json_data_3d, f, indent=4)
+
+        # Mag
+        mag_tmp = np.zeros(nii_mag.shape + (n_currents,))
+        for i_current in range(n_currents):
+            nii_mag_tmp = nib.load(mags[i_channel][i_current][0])
+            mag_tmp[..., i_current] = nii_mag_tmp.get_fdata()
+
+        nii_mag_4d = nib.Nifti1Image(mag_tmp, nii_mag_tmp.affine, header=nii_mag_tmp.header)
+        fname_4d = os.path.join(path_output, f"mag_channel{i_channel}_echo0_4d.nii.gz")
+        nib.save(nii_mag_4d, fname_4d)
+
+        # Save the json file of the 4d mag
+        fname_json_4d = fname_4d.rsplit('.nii', 1)[0] + '.json'
+        fname_json_3d = mags[i_channel][0][0].rsplit('.nii', 1)[0] + '.json'
+        # Open and save as new name
+        with open(fname_json_3d) as json_file:
+            json_data_3d = json.load(json_file)
+        with open(fname_json_4d, mode='w') as f:
+            json.dump(json_data_3d, f, indent=4)
+
+        fname_fmap = os.path.join(path_output, f"channel{i_channel}_fieldmap.nii.gz")
+        prepare_fieldmap_uncli(list_fname_phase_4d, fname_4d, unwrapper, fname_fmap, autoscale,
+                               fname_mask=fname_mask_4d,
+                               gaussian_filter=gaussian_filter,
+                               sigma=sigma)
+
+        nii_fmap_4d = nib.load(fname_fmap)
         fnames_fmap.append([])
         for i_current in range(n_currents):
-            phase = phases[i_channel][i_current]
+            fname_fmap_3d = os.path.join(path_output, f"channel{i_channel}_{i_current}_fieldmap.nii.gz")
+            nii = nib.Nifti1Image(nii_fmap_4d.get_fdata()[..., i_current], nii_fmap_4d.affine,
+                                  header=nii_fmap_4d.header)
+            nib.save(nii, fname_fmap_3d)
+            fnames_fmap[index_channel].append(fname_fmap_3d)
 
-            # Calculate fieldmap and save to a file
-            fname_fmap = os.path.join(path_output, f"channel{i_channel}_{i_current}_fieldmap.nii.gz")
-            prepare_fieldmap_uncli(phase, fname_mag, unwrapper, fname_fmap, autoscale,
-                                   fname_mask=fname_mask,
-                                   gaussian_filter=gaussian_filter,
-                                   sigma=sigma)
+        # If not debug, remove junk output
+        if not logger.level <= getattr(logging, 'DEBUG'):
+            # Delete tmp fieldmaps
+            os.remove(fname_fmap)
+            fname = fname_fmap.rsplit('.nii', 1)[0] + '.json'
+            os.remove(fname)
 
-            fnames_fmap[index_channel].append(fname_fmap)
+            # Delete 4d tmp phase files
+            for fname in list_fname_phase_4d:
+                os.remove(fname)
+                fname = fname.rsplit('.nii', 1)[0] + '.json'
+                os.remove(fname)
+
+            # Delete 4d mag tmp files
+            os.remove(fname_4d)
+            fname = fname_4d.rsplit('.nii', 1)[0] + '.json'
+            os.remove(fname)
 
     # Remove dead channels from the list of currents
     for i_channel in dead_channels:
@@ -167,27 +235,32 @@ def create_coil_profiles_cli(fname_json, autoscale, unwrapper, threshold, gaussi
 
     # If not debug, remove junk output
     if not logger.level <= getattr(logging, 'DEBUG'):
+        # Delete tmp masks
         os.remove(fname_mask)
+        os.remove(fname_mask_4d)
+
         # For each channel
         for list_fnames in fnames_fmap:
             # For each fieldmap
             for fname_nifti in list_fnames:
-                fname_json_m = fname_nifti.rsplit('.nii', 1)[0] + '.json'
-                # Delete nifti
-                # os.remove(fname_nifti)
-                # Delete json
-                # os.remove(fname_json_m)
+                os.remove(fname_nifti)
 
     # Use header and json info from first file in the list
-    fname_json_phase = phases[0][0][0].rsplit('.nii', 1)[0] + '.json'
+    for i_channel in range(n_channels):
+        if i_channel not in dead_channels:
+            fname_json_phase = phases[i_channel][0][0].rsplit('.nii', 1)[0] + '.json'
+            fname_phase = phases[i_channel][0][0]
+            break
+
     with open(fname_json_phase) as json_file:
         json_data_phase = json.load(json_file)
-    nii_phase = nib.load(phases[0][0][0])
+
+    nii_phase = nib.load(fname_phase)
     nii_profiles = nib.Nifti1Image(profiles, nii_phase.affine, header=nii_phase.header)
 
     # Save nii and json
     save_nii_json(nii_profiles, json_data_phase, fname_output)
-    logger.info(f"\n\n Filename of the coil profiles is: {fname_output}")
+    logger.info(f"Filename of the coil profiles is: {fname_output}")
 
     # Create coil config file
     coil_name = json_data['name']
@@ -195,7 +268,8 @@ def create_coil_profiles_cli(fname_json, autoscale, unwrapper, threshold, gaussi
     config_coil = {
         'name': coil_name,
         'coef_channel_minmax': json_data['coef_channel_minmax'],
-        'coef_sum_max': json_data['coef_sum_max']
+        'coef_sum_max': json_data['coef_sum_max'],
+        'Units': json_data['units']
     }
 
     # write json
