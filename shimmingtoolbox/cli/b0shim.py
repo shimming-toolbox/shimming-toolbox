071d46c43c987196212dd886ab4e73b20358b6a9
Aurélien Pujol <63396133+Aurelienpujolm@users.noreply.github.com>
2023-06-22 11:06:33 -0400

    Address the comment on the PR

--- shimmingtoolbox/cli/b0shim.py
+++ shimmingtoolbox/cli/b0shim.py
@@ -1119,7 +1119,8 @@ def _add_sub_figure(fig, i_plot, n_plots, static_coefs, bounds, min_y, max_y, un
 
     # Add realtime component as an errorbar
     if rt_coefs is not None:
-        riro = np.abs([rt_coefs * -pres_probe_min, rt_coefs * pres_probe_max])
+        riro = np.abs([np.min(rt_coef_ishim * -pres_probe_min, rt_coef_ishim* pres_probe_max),
+                       np.max(rt_coef_ishim * -pres_probe_min, rt_coef_ishim* pres_probe_max))])
         ax.errorbar(range(n_channels), static_coefs, yerr=riro, fmt='o', elinewidth=4, capsize=6,
                     label='static-riro')
     # Add static component
