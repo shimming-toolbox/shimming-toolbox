b5b5cc4284d6d2f457d2060be5aa942bb785c2db
Aurélien Pujol <63396133+Aurelienpujolm@users.noreply.github.com>
2023-08-09 12:24:34 -0400

    Update version of read the docs theme (#468)
    * Changed version of Sphinx to be able to used style
    
    * Changed version of sphinx and sphinx theme

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ setup(
         "joblib",
     ],
     extras_require={
-        'docs': ["sphinx>=1.6", "sphinx_rtd_theme>=0.2.4", "sphinx-click"],
+        'docs': ["sphinx>=1.7", "sphinx_rtd_theme>=1.2.2", "sphinx-click"],
         'dev': ["pre-commit>=2.10.0"]
     },
 )
