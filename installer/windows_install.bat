b241b0e1a8ea53247667c8cb11c09ecdb464dba4
Alexandre D'Astous <po09i@hotmail.com>
2024-04-16 11:04:18 -0400

    Use python 3.10 for windows

--- installer/windows_install.bat
+++ installer/windows_install.bat
@@ -36,7 +36,7 @@ del "%UNIQUE_TMP_INSTALLER%"
 
 REM Installing dcm2niix and python
 echo Installing dcm2niix and python
-call "%ST_DIR%\%PYTHON_DIR%\condabin\mamba.bat" install -y -c conda-forge dcm2niix python=3.9 || goto error
+call "%ST_DIR%\%PYTHON_DIR%\condabin\mamba.bat" install -y -c conda-forge dcm2niix python=3.10 || goto error
 
 REM Installing Shimming Toolbox
 copy "%ST_SOURCE_FILES%\config\dcm2bids.json" "%ST_DIR%\dcm2bids.json" || goto error
