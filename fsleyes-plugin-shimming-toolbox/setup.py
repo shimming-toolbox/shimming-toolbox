e3585b45ae538c90b5f465c04bd4d4bc4795f1b8
Alex Dastous <47249340+po09i@users.noreply.github.com>
2024-11-08 14:18:47 -0500

    Create a workflow to automate updating CHANGES.md (#562)
    * Use text file to specify versions
    
    * Add workflow to automate changelog

--- fsleyes-plugin-shimming-toolbox/setup.py
+++ fsleyes-plugin-shimming-toolbox/setup.py
@@ -1,8 +1,16 @@
 from setuptools import setup, find_packages
+from os import path
+
+# Get the directory where this current file is saved
+here = path.abspath(path.dirname(__file__))
+
+path_version = path.join(here, 'fsleyes_plugin_shimming_toolbox', 'version.txt')
+with open(path_version) as f:
+    version = f.read().strip()
 
 setup(
     name='fsleyes-plugin-shimming-toolbox',
-    version="1.0.0",
+    version=version,
     install_requires=[
         "imageio",
         'pre-commit>=2.10.0'
