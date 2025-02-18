70d8a92c6329a6a8b4823199a1d05aeac91e0e32
Arnaud Breheret <arnaudbreheret14@gmail.com>
2025-02-18 14:37:08 -0500

    add requirement.txt to setup files in prreparation for release automatization

--- fsleyes-plugin-shimming-toolbox/setup.py
+++ fsleyes-plugin-shimming-toolbox/setup.py
@@ -8,13 +8,20 @@ path_version = path.join(here, 'fsleyes_plugin_shimming_toolbox', 'version.txt')
 with open(path_version) as f:
     version = f.read().strip()
 
+requirements_path = path.join(path.dirname(here), "requirements_fsleyes.txt")
+if path.exists(requirements_path):
+    with open(requirements_path) as f:
+        install_requires = [line.strip() for line in f if line.strip() and not line.startswith("#")]
+else:
+    install_requires = [
+        "imageio",
+        'pre-commit>=2.10.0'
+    ]
+
 setup(
     name='fsleyes-plugin-shimming-toolbox',
     version=version,
-    install_requires=[
-        "imageio",
-        'pre-commit>=2.10.0'
-    ],
+    install_requires=install_requires,
     packages=find_packages(exclude=['.git']),
     include_package_data=True,
     entry_points={
