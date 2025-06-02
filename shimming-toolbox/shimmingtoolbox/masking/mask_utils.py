38a957967dc9140ae2576738274307cd0127639b
AntoineGuenette <70295563+AntoineGuenette@users.noreply.github.com>
2025-06-02 14:02:14 -0400

    Add debugging fonctionnality

--- shimming-toolbox/shimmingtoolbox/masking/mask_utils.py
+++ shimming-toolbox/shimmingtoolbox/masking/mask_utils.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import logging
+import os
 import nibabel as nib
 import numpy as np
 
@@ -65,6 +66,12 @@ def resample_mask(nii_mask_from, nii_target, from_slices=None, dilation_kernel='
     mask_dilated_in_roi = np.zeros_like(mask_dilated)
     mask_dilated_in_roi[nii_full_mask_target.get_fdata() != 0] = mask_dilated[nii_full_mask_target.get_fdata() != 0]
     nii_mask_dilated = nib.Nifti1Image(mask_dilated_in_roi, nii_mask_target.affine, header=nii_mask_target.header)
+
+    # Save masks for debugging if necessary
+    if logger.getEffectiveLevel() <= logging.DEBUG and path_output is not None:
+        nib.save(nii_mask, os.path.join(path_output, f"fig_mask_{from_slices[0]}.nii.gz"))
+        nib.save(nii_mask_target, os.path.join(path_output, f"fig_mask_res{from_slices[0]}.nii.gz"))
+        nib.save(nii_mask_dilated, os.path.join(path_output, f"fig_mask_dilated{from_slices[0]}.nii.gz"))
 
     # Return non dilated mask if requested
     if return_non_dil_mask:
