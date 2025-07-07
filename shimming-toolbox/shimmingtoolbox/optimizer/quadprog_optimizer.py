c2c3f08e855935c045f1c1d4b3b31205728b7779
AntoineGuenette <anto.guenette@gmail.com>
2025-07-07 13:02:35 -0400

    No changes needed in PmuQuadProgOpt class

--- shimming-toolbox/shimmingtoolbox/optimizer/quadprog_optimizer.py
+++ shimming-toolbox/shimmingtoolbox/optimizer/quadprog_optimizer.py
@@ -179,7 +179,6 @@ class QuadProgOpt(OptimizerUtils):
 
         return cost_matrix, cost_vector
 
-# TODO : Realtime softmask B0 shimming needs to be implemented
 class PmuQuadProgOpt(QuadProgOpt):
     """ Optimizer for the realtime component (riro) for this optimization:
             field(i_vox) = riro(i_vox) * (acq_pressures - mean_p) + static(i_vox)
